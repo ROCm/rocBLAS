f38a12e9ea258eb6a70d5c6ce7bba73a69be9c9d
David Tanner <guacamoleo@gmail.com>
2015-10-08 15:50:09 -0500

    defining matrix and gemm
    [ROCm/rocblas commit: 9f7dcff581b8d031171656e341c7a9a8962c375a]

--- src/include/ablas_types.h
+++ src/include/ablas_types.h
@@ -15,200 +15,134 @@
  * ************************************************************************ */
 
 /*! \file
- * \brief clBLAS-types.h defines public types to be consummed by the library
+ * \brief aBLAS-types.h defines public types to be consummed by the library
  * The types are agnostic to the underlying runtime used by the library
  */
 
 #pragma once
-#ifndef _CL_BLAS_TYPES_H_
-#define _CL_BLAS_TYPES_H_
+#ifndef _ABLAS_TYPES_H_
+#define _ABLAS_TYPES_H_
 
 /*! \brief An enumeration to describe the precision of data pointed by a
  * particular instance of a struct
- * \remarks This impllies that clBLAS can support mixed precision operations
+ * \remarks This impllies that aBLAS can support mixed precision operations
  */
-typedef enum clblasPrecision_ {
-    clblasSingleReal,
-    clblasDoubleReal,
-    clblasSingleComplex,
-    clblasDoubleComplex,
-} clblasPrecision;
+typedef enum ablas_precision_ {
+    ablas_single_real,
+    ablas_double_real,
+    ablas_single_complex,
+    ablas_double_complex,
+} ablas_precision;
 
 /*! \brief Used by the Hermitian, symmetric and triangular matrix
  * routines to specify whether the upper or lower triangle is being referenced.
  */
-typedef enum clblasUplo_ {
-    clblasUpper,               /**< Upper triangle. */
-    clblasLower                /**< Lower triangle. */
-} clblasUplo;
+typedef enum ablas_uplo_ {
+    ablas_upper,               /**< Upper triangle. */
+    ablas_lower                /**< Lower triangle. */
+} ablas_uplo;
 
 /*! \brief It is used by the triangular matrix routines to specify whether the
  * matrix is unit triangular.
  */
-typedef enum clblasDiag_ {
-    clblasUnit,               /**< Unit triangular. */
-    clblasNonUnit             /**< Non-unit triangular. */
-} clblasDiag;
+typedef enum ablas_diag_ {
+    ablas_unit,               /**< Unit triangular. */
+    ablas_non_unit             /**< Non-unit triangular. */
+} ablas_diag;
 
 /*! \brief Indicates the side matrix A is located relative to matrix B during multiplication. */
-typedef enum clblasSide_ {
-    clblasLeft,        /**< Multiply general matrix by symmetric,
+typedef enum ablas_side_ {
+    ablas_left,        /**< Multiply general matrix by symmetric,
                                Hermitian or triangular matrix on the left. */
-    clblasRight        /**< Multiply general matrix by symmetric,
+    ablas_right        /**< Multiply general matrix by symmetric,
                                Hermitian or triangular matrix on the right. */
-} clblasSide;
+} ablas_side;
 
 
-/*! \brief Structure to encapsulate scalar data to clBLAS API
- * \details This stores data in a struct of arrays (SoA) model.  This should help performance
- * for batched operation, and gracefully become a 'normal' struct when batch_size == 1
- * \note It is the users responsibility to allocate/deallocate OpenCL buffers
- */
-typedef struct clblasScalar_
-{
-    /*! Polymorphic pointer for the library.  If clBLAS is compiled with BUILD_CLVERSION < 200,
-     * value will be will be treated as allocated with clCreateBuffer().  If
-     * BUILD_CLVERSION >= 200 then this will be treated as allocated with clSVMalloc()
-     */
-    void* device_scalar;
-
-    /*! This describes the precision of the data pointed to by value
-     */
-    clblasPrecision precision;
-
-    /*! This offset is added to the cl_mem locations on device to define beginning of the data in the cl_mem buffers
-     */
-    size_t offset;
-
-    /*! This is the number of scalar values stored in the value buffer
-     */
-    size_t batch_size;
-
-    /*! This is the distance between scalars in value; batch_stride >= 1
-     * Packed scalars would have a batch_stride of 1
-     */
-    size_t batch_stride;
-} clblasScalar;
-
-/*! \brief Structure to encapsulate dense vector data to clBLAS API
- * \details This stores data in a struct of arrays (SoA) model.  This should help performance
- * for batched operation, and gracefully become a 'normal' struct when batch_size == 1
- * \note It is the users responsibility to allocate/deallocate OpenCL buffers
- */
-typedef struct clblasVector_
-{
-    /*! Polymorphic pointer for the library.  If clBLAS is compiled with BUILD_CLVERSION < 200,
-     * value will be will be treated as a pointer allocated with clCreateBuffer().  If
-     * BUILD_CLVERSION >= 200 then this will be treated as a pointer allocated with clSVMalloc()
-     */
-    void* device_vector;
-
-    /*! This describes the precision of the data pointed to by value
-     */
-    clblasPrecision precision;
-
-    /*! \brief This offset is added to the cl_mem location on device to define beginning of the data in the cl_mem buffers
-     * \details Usually used to define the start a smaller subvector in larger vector allocation block.
-     * This same offset is applied to every vector in a batch
-     */
-    size_t offset;
-
-    size_t length;  /*!< Length of an individual vector */
-
-    /*! \brief Stride to consecutive elements in vector
-     * \details For packed vectors, stride == 1
-     */
-    size_t stride;
-
-    /*! This is the number of vectors stored in the values buffer; a single vector would have batch_size == 1
-     */
-    size_t batch_size;
-
-    /*! This is the distance between vectors in values; batch_stride >= num_values
-     * Packed vectors would have a batch_stride == num_values
-     */
-    size_t batch_stride;
-} clblasVector;
-
-/*! \brief Structure to encapsulate dense matrix data to clBLAS API
- * \details This stores data in a struct of arrays (SoA) model.  This should help performance
- * for batched operation, and gracefully become a 'normal' struct when batch_size == 1
+/*! \brief Structure to encapsulate dense matrix/vector/scalar data to aBLAS API.
+ * \details Able to store multiple matrices (or vectors, scalars)
+ * to facilitate high-performance batched oprations;
+ * gracefully becomes a 'normal' matrix when num_matrices == 1.
  * \verbatim
-    clBlas V2: Given a column major matrix with M, N, lda, nontranspose
-    This matrix is represented in clBlas V3 as:
+    clBLAS V2: Given a column major matrix with M, N, lda, nontranspose
+    This matrix is represented in aBLAS as:
     num_rows = M
     num_cols = N
     row_stride = 1
     col_stride = ldX
+    num_matrices = 1
    \endverbatim
-
- * \note It is the users responsibility to allocate/deallocate OpenCL buffers
+ * aBLAS API represents scalars as ablas_matrix with num_rows = 1 and num_cols = 1.
+ * aBLAS API represents vectors as ablas_matrix with num_rows = 1 or  num_cols = 1.
+ *
+ * \note It is the users responsibility to allocate/deallocate buffers
  * \note Traditional matrix fields not explicitely represented within this structure
  * \li \b transpose layout
  * \li \b row/column major layout
  * \attention There has been significant debate about changing the matrix meta data below from host scalar values
- * into batched scalar values by changing their types to clblasScalar.  The advantage is that we
+ * into batched scalar values by changing their types to ablasScalar.  The advantage is that we
  * could then process batched matricies of arbitrary row, column and stride values.  The problem is
  * that both host and device need access to this data, which would introduce mapping calls.  The host needs
  * the data to figure out how to form launch parameters, and the device needs access to be able to
  * handle matrix tail cases properly.  This may have reasonable performance on APU's, but the performance
  * impact on discrete devices could be significant.  For now, we keep the num_rows, num_cols and strides as a size_t on host
  */
-typedef struct clblasMatrix_
-{
-    /*! \brief An OpenCL buffer that holds the values of all the matrix data
-     * \details Polymorphic pointer for the library.  If clBLAS is compiled with BUILD_CLVERSION < 200,
+typedef struct ablas_matrix_ {
+
+    /*! \brief Buffer that holds the matrix data.
+     * \details Polymorphic pointer for the library.  If aBLAS is compiled with BUILD_CLVERSION < 200,
      * value will be will be treated as a pointer allocated with clCreateBuffer().  If
      * BUILD_CLVERSION >= 200 then this will be treated as a pointer allocated with clSVMalloc()
      * For batched matrices, this buffer contains the packed values of all the matrices.
      */
-    void* device_matrix;
+    void* data;
 
-    /*! This describes the precision of the data pointed to by value
+    /*! Precision of the data.
      */
-    clblasPrecision precision;
+    ablas_precision precision;
 
     /*! \brief This offset is added to the cl_mem location on device to define beginning of the data in the cl_mem buffers
      * \details Usually used to define the start a smaller submatrix in larger matrix allocation block.
      * This same offset is applied to every matrix in a batch
      */
-    size_t offset_values;
+    size_t offset;
 
-    /*! \brief Number of elements in a matrix row
+    /*! \brief Number of rows in each matrix.
      * \details For batched matrices, this is a constant property of each 'matrix', where each matrix has the same number
      *  of rows
      */
     size_t num_rows;
 
-    /*! \brief Number of elements in a matrix column
+    /*! \brief Number of columns in each matrix.
     * \details For batched matrices, this is a constant property of each 'matrix', where each matrix has the same number
     *  of columns
      */
     size_t num_cols;
 
-    /*! \brief Stride to consecutive elements in a matrix row
-     * \details For an example of a packed matrices, in 'C' family row major
-     * languages this is num_rows, for Fortran family languages this is 1
+     /*! Number of matrices stored in the buffer; a single matrix would have num_matrices == 1.
+     * \pre num_matrices > 0
+      */
+    size_t num_matrices;
+
+    /*! \brief Stride to consecutive rows in each matrix.
+     * \details ptr += row_stride would point to same column, same matrix, next row.
+     * For column-major matrix, row_stride = 1.
      */
     size_t row_stride;
 
-    /*! \brief Stride to consecutive elements in a matrix column
-     * \details For an example of a packed matrices, in 'C' family row major
-     * languages this is 1, for Fortran family languages this is num_rows
+    /*! \brief Stride to consecutive columns in each matrix.
+     * \details ptr += col_stride would point to same row, same matrix, next column.
+     * For row-major matrix, col_stride = 1.
      */
     size_t col_stride;
 
-     /*! This is the number of vectors stored in the values buffer; a single vector would have batch_size == 1
-     * \pre batch_size > 0
-      */
-    size_t batch_size;
-
-     /*! This is the distance between the start of matrices in values
+     /*! \bried Stride to consectutive matrices.
+      * \details ptr += matrix_stride would point to same row, same column, next matrix
       * \pre row_major: batch_stride >= num_rows * row_stride
       * \pre column_major: batch_stride >= num_cols * col_stride
       */
-    size_t batch_stride;
+    size_t matrix_stride;
 
-} clblasMatrix;
+} ablas_matrix;
 
 #endif
