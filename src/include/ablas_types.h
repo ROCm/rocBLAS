6d9a4e6978805babb981e98b75bbbb1c32ffc0ad
tim <tingxingdong@gmail.com>
2016-02-02 10:32:48 -0600

    first update: add netlib/batched blas api. elaborate ablas types

--- src/include/ablas_types.h
+++ src/include/ablas_types.h
@@ -14,7 +14,7 @@
  * limitations under the License.
  * ************************************************************************ */
 
-/*! \file
+/*!\file
  * \brief aBLAS-types.h defines public types to be consummed by the library
  * The types are agnostic to the underlying runtime used by the library
  */
@@ -23,61 +23,177 @@
 #ifndef _ABLAS_TYPES_H_
 #define _ABLAS_TYPES_H_
 
-/*! \brief An enumeration to describe the precision of data pointed by a
- * particular instance of a struct
- * \remarks This impllies that aBLAS can support mixed precision operations
+#include <stdint.h>
+#include <hip_runtime_api.h> 
+
+
+/*! \file
+ * \brief ablas_types.h defines data types used by ablas
  */
-typedef enum ablas_precision_ {
-    ablas_single_real,
-    ablas_double_real,
-    ablas_single_complex,
-    ablas_double_complex,
-} ablas_precision;
 
-/*! \brief Used by the Hermitian, symmetric and triangular matrix
- * routines to specify whether the upper or lower triangle is being referenced.
- */
-typedef enum ablas_uplo_ {
-    ablas_upper,               /**< Upper triangle. */
-    ablas_lower                /**< Lower triangle. */
-} ablas_uplo;
-
-/*! \brief It is used by the triangular matrix routines to specify whether the
- * matrix is unit triangular.
- */
-typedef enum ablas_diag_ {
-    ablas_unit,               /**< Unit triangular. */
-    ablas_non_unit             /**< Non-unit triangular. */
-} ablas_diag;
-
-/*! \brief Indicates the side matrix A is located relative to matrix B during multiplication. */
-typedef enum ablas_side_ {
-    ablas_left,        /**< Multiply general matrix by symmetric,
-                               Hermitian or triangular matrix on the left. */
-    ablas_right        /**< Multiply general matrix by symmetric,
-                               Hermitian or triangular matrix on the right. */
-} ablas_side;
+    /*
+     * ===========================================================================
+     *   READEME: ABLAS Wrapper of HIP data types and APIs
+     *   HIP is still under development. Developers of aBLAS are encouraged to use ablas APIs
+     *   in their code, in case HIP APIs would be changed in the future.
+     * ===========================================================================
+     */
 
 
-/*! \brief Indicate the data order of a matrix. This is only used by the help function clblas_2_ablas_gemm */
-typedef enum ablas_order_{
-    ablas_row_major,
-    ablas_column_major
-} ablas_order;
+typedef hipStream_t ablas_queue;
+typedef hipEvent_t  ablas_event;
+typedef ablas_queue ablas_handle;
 
-/*! \brief Indicate the transpose form of a matrix. This is only used by the help function clblas_2_ablas_gemm */
-typedef enum ablas_transpose_{
-    ablas_no_trans,
-    ablas_trans,
-    ablas_conjugate_trans
-}ablas_transpose;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* ============================================================================================ */
+
+    /*! parameter constants. 
+     *  numbering is consistent with CBLAS, ACML and most standard C BLAS libraries 
+     */
+
+    /*! \brief Used to specify whether the matrix is in row major or column major storage format. */
+    typedef enum ablas_order_{
+        ablas_row_major         = 101,
+        ablas_column_major      = 102
+    } ablas_order;
+
+
+    /*! \brief Used to specify whether the matrix is to be transposed or not. */
+    typedef enum ablas_trans_ {
+        ablas_no_trans   = 111,           /**< Operate with the matrix. */
+        ablas_trans      = 112,           /**< Operate with the transpose of the matrix. */
+        ablas_conj_trans = 113            /**< Operate with the conjugate transpose of
+                                         the matrix. */
+    } ablas_transpose;
+
+    /*! \brief Used by the Hermitian, symmetric and triangular matrix
+     * routines to specify whether the upper or lower triangle is being referenced.
+     */
+    typedef enum ablas_uplo_ {
+        ablas_upper = 121,               /**< Upper triangle. */
+        ablas_lower = 122,               /**< Lower triangle. */
+        ablas_full  = 123
+    } ablas_uplo;
+
+
+    /*! \brief It is used by the triangular matrix routines to specify whether the
+     * matrix is unit triangular.
+     */
+    typedef enum ablas_diag_ {
+        ablas_non_unit  = 131           /**< Non-unit triangular. */
+        ablas_unit      = 132,          /**< Unit triangular. */
+    } ablas_diag;
+
+
+    /*! \brief Indicates the side matrix A is located relative to matrix B during multiplication. */
+    typedef enum ablas_side_ {
+        ablas_left  = 141,        /**< Multiply general matrix by symmetric,
+                                   Hermitian or triangular matrix on the left. */
+        ablas_right = 142,        /**< Multiply general matrix by symmetric,
+                                   Hermitian or triangular matrix on the right. */
+        ablas_both  = 143
+    } ablas_side;
+
+
+    /* ============================================================================================ */
+    /**
+     *   @brief ablas error codes definition, incorporating HIP error
+     *   definitions.
+     *
+     *   This enumeration is a subset of the HIP error codes extended with some
+     *   additional extra codes.  For example, hipErrorMemoryAllocation, which is
+     *   defined in hip_runtime_api.h is aliased as ablas_error_memory_allocation.
+     */
+    typedef enum ablas_status_ {
+
+        ablas_success                       =    hipSuccess = 0,                  ///< Successful completion.
+        ablas_error_memory_allocation       =    hipErrorMemoryAllocation,        ///< Memory allocation error.
+        ablas_error_memory_free             =    hipErrorMemoryFree,              ///< Memory free error.
+        ablas_error_unknown_symbol          =    hipErrorUnknownSymbol,           ///< Unknown symbol
+        ablas_error_outof_resources         =    hipErrorOutOfResources          ///< Out of resources error
+        ablas_error_invalid_value           =    hipErrorInvalidValue            ///< One or more of the paramters passed to the API call is NULL or not in an acceptable range.
+        ablas_error_invalid_resource_handle =    hipErrorInvalidResourceHandle   ///< Resource handle (hipEvent_t or hipStream_t) invalid.
+        ablas_error_invalid_device          =    hipErrorInvalidDevice           ///< DeviceID must be in range 0...#compute-devices.
+        ablas_error_no_deive                =    hipErrorNoDevice                ///< Call to cudaGetDeviceCount returned 0 devices
+        ablas_error_not_ready               =    hipErrorNotReady                ///< indicates that asynchronous operations enqueued earlier are not ready.  
+                                                                                 /// This is not actually an error, but is used to distinguish from hipSuccess(which indicates completion).  
+                                                                                 /// APIs that return this error include hipEventQuery and hipStreamQuery.
+        /* Extended error codes */
+        ablas_not_implemented         = -1024, /**< Functionality is not implemented */
+        ablas_not_initialized,                 /**< ablas library is not initialized yet */
+        ablas_invalid_matA,                    /**< Matrix A is not a valid memory object */
+        ablas_invalid_matB,                    /**< Matrix B is not a valid memory object */
+        ablas_invalid_matC,                    /**< Matrix C is not a valid memory object */
+        ablas_invalid_vecX,                    /**< Vector X is not a valid memory object */
+        ablas_invalid_becY,                    /**< Vector Y is not a valid memory object */
+        ablas_invalid_dim,                     /**< An input dimension (M,N,K) is invalid */
+        ablas_invalid_leadDimA,                /**< Leading dimension A must not be less than the size of the first dimension */
+        ablas_invalid_leadDimB,                /**< Leading dimension B must not be less than the size of the second dimension */
+        ablas_invalid_leadDimC,                /**< Leading dimension C must not be less than the size of the third dimension */
+        ablas_invalid_incx,                    /**< The increment for a vector X must not be 0 */
+        ablas_invalid_incy,                    /**< The increment for a vector Y must not be 0 */
+    } ablas_status;
+
+
+
+    /* ============================================================================================ */
+
+    /*! \brief To specify whether int32 or int64 is used
+     */
+    #if defined(ABLAS_ILP64)
+    typedef int64_t ablas_int;
+    #else
+    typedef int32_t ablas_int;
+    #endif
+
+    /*! \brief  HIP & CUDA both use float2/double2 to define complex number  
+     */
+    typedef float2 ablas_float_complex
+    typedef double2 ablas_double_complex
+
+    #define ABLAS_ONE 1
+    #define ABLAS_NEG_ONE -1
+    #define ABLAS_ZERO 0
+
+    #define ABLAS_Z_MAKE(r,i)     
+    #define ABLAS_Z_REAL(a)       (a).x
+    #define ABLAS_Z_IMAG(a)       (a).y
+    #define ABLAS_Z_ADD(a, b)     
+    #define ABLAS_Z_SUB(a, b)     
+    #define ABLAS_Z_MUL(a, b)     
+    #define ABLAS_Z_DIV(a, b)     
+    #define ABLAS_Z_ABS(a)        
+    #define ABLAS_Z_ABS1(a)       (fabs((a).x) + fabs((a).y))
+    #define ABLAS_Z_CONJ(a)       
+        
+    #define ABLAS_C_MAKE(r,i)     
+    #define ABLAS_C_REAL(a)       (a).x
+    #define ABLAS_C_IMAG(a)       (a).y
+    #define ABLAS_C_ADD(a, b)     
+    #define ABLAS_C_SUB(a, b)     
+    #define ABLAS_C_MUL(a, b)     
+    #define ABLAS_C_DIV(a, b)     
+    #define ABLAS_C_ABS(a)        
+    #define ABLAS_C_ABS1(a)       (fabsf((a).x) + fabsf((a).y))
+    #define ABLAS_C_CONJ(a)       
+
+#ifdef __cplusplus
+}
+#endif
+
+
+/* ============================================================================================ */
 
 /*! \brief Structure to encapsulate dense matrix/vector/scalar data to aBLAS API.
  * \details Able to store multiple matrices (or vectors, scalars)
  * to facilitate high-performance batched oprations;
  * gracefully becomes a 'normal' matrix when num_matrices == 1.
  * \verbatim
-    clBLAS V2: Given a column major matrix with M, N, lda, nontranspose
+    ablas V2: Given a column major matrix with M, N, lda, nontranspose
     This matrix is represented in aBLAS as:
     num_rows = M
     num_cols = N
@@ -157,5 +273,7 @@ typedef struct ablas_matrix_ {
     size_t matrix_stride;
 
 } ablas_matrix;
+
+
 
 #endif
