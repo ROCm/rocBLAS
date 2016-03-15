fc0caba775da8996d9cbbd3ee205a8640a61fa78
Kent Knox <kent.knox@amd>
2016-03-15 15:39:47 -0500

    Unit tester now building with boost, gtest and cblas
    The build environment works to the point that we can now build our
    unit test framework with all of its required dependencies.
    
    
    [ROCm/rocblas commit: 058a71d43a552eb8fe85214a7168d8e407b001de]

--- clients/cmake/external-lapack.cmake
+++ clients/cmake/external-lapack.cmake
@@ -8,19 +8,29 @@ include( ExternalProject )
 set( lapack_git_repository "https://github.com/live-clones/lapack.git" CACHE STRING "URL to download lapack from" )
 set( lapack_git_tag "master" CACHE STRING "git branch" )
 
-# Create a workspace to house the src and buildfiles for googleMock
-set_directory_properties( PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/extern/lapack )
-
-set( lapack_cmake_args -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>/package -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} )
+set( lapack_cmake_args -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>/package -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} )
 
 # message( STATUS "lapack_make ( " ${lapack_make} " ) " )
 # message( STATUS "lapack_cmake_args ( " ${lapack_cmake_args} " ) " )
 
+include( GNUInstallDirs )
+
+# lapack cmake exports has a bug on debian architectures, they do not take into account the
+# lib/<machine> paths
+# if CMAKE_INSTALL_LIBDIR is of the form above, strip the machine
+# Match against a '/' in CMAKE_INSTALL_LIBDIR, i.e. lib/x86_64-linux-gnu
+if( ${CMAKE_INSTALL_LIBDIR} MATCHES "lib/.*" )
+  list( APPEND lapack_cmake_args "-DCMAKE_INSTALL_LIBDIR=lib" )
+endif( )
+
 # Master branch has a new structure that combines googletest with googlemock
 ExternalProject_Add(
   lapack
+  PREFIX ${CMAKE_BINARY_DIR}/extern/lapack
   DOWNLOAD_COMMAND git clone --depth 1 --branch ${lapack_git_tag} ${lapack_git_repository}
-  CMAKE_ARGS ${lapack_cmake_args} -DCBLAS=ON -DLAPACKE=ON -DBUILD_TESTING=OFF
+  CMAKE_ARGS ${lapack_cmake_args} -DCBLAS=ON -DLAPACKE=OFF -DBUILD_TESTING=OFF
+  LOG_BUILD 1
+  LOG_INSTALL 1
 )
 
 ExternalProject_Get_Property( lapack source_dir )
--- clients/tests/main.cpp
+++ clients/tests/main.cpp
@@ -7,34 +7,34 @@
 ///
 
 #include <iostream>
+#include <cblas.h>
 #include <gtest/gtest.h>
-// #include <boost/program_options.hpp>
-// #include <cblas.h>
+#include <boost/program_options.hpp>
 
-// namespace po = boost::program_options;
+namespace po = boost::program_options;
 
 int main( int argc, char* argv[] )
 {
-	// // Declare the supported options.
-	// po::options_description desc( "rocblas-test command line options" );
-	// desc.add_options()
-	// 	( "help,h",		"print help message" )
-	// 	;
-	//
-	// //	Parse the command line options, ignore unrecognized options and collect them into a vector of strings
-	// po::variables_map vm;
-	// po::parsed_options parsed = po::command_line_parser( argc, argv ).options( desc ).allow_unregistered( ).run( );
-	// po::store( parsed, vm );
-	// po::notify( vm );
-	// std::vector< std::string > to_pass_further = po::collect_unrecognized( parsed.options, po::include_positional );
-	//
-	// std::cout << std::endl;
-	//
-	// if( vm.count( "help" ) )
-	// {
-	// 	std::cout << desc << std::endl;
-	// 	return 0;
-	// }
+	// Declare the supported options.
+	po::options_description desc( "rocblas-test command line options" );
+	desc.add_options()
+		( "help,h",		"print help message" )
+		;
+
+	//	Parse the command line options, ignore unrecognized options and collect them into a vector of strings
+	po::variables_map vm;
+	po::parsed_options parsed = po::command_line_parser( argc, argv ).options( desc ).allow_unregistered( ).run( );
+	po::store( parsed, vm );
+	po::notify( vm );
+	std::vector< std::string > to_pass_further = po::collect_unrecognized( parsed.options, po::include_positional );
+
+	std::cout << std::endl;
+
+	if( vm.count( "help" ) )
+	{
+		std::cout << desc << std::endl;
+		return 0;
+	}
 
 	::testing::InitGoogleTest( &argc, argv );
 
