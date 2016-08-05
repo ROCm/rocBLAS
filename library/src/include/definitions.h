c45107fb665376e9c4703bfdfd3fa660ce1e64e2
David Tanner <david.tanner@amd.com>
2016-08-05 10:38:49 -0500

    fixed status_check functions to not double execute

--- library/src/include/definitions.h
+++ library/src/include/definitions.h
@@ -6,32 +6,38 @@
  * thereby it can include top-level definitions included by all
  ******************************************************************************/
 
-#define RETURN_IF_COBALT_ERROR(STATUS) \
-  if (STATUS != cobaltStatusSuccess) { \
-    cobaltStatusCheck( STATUS ); \
-    return get_rocblas_status_for_cobalt_status(STATUS); \
+#define RETURN_IF_COBALT_ERROR(INPUT_STATUS_FOR_CHECK) \
+  cobaltStatus TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK; \
+  if (TMP_STATUS_FOR_CHECK != cobaltStatusSuccess) { \
+    cobaltStatusCheck( TMP_STATUS_FOR_CHECK ); \
+    return get_rocblas_status_for_cobalt_status(TMP_STATUS_FOR_CHECK); \
   }
-#define RETURN_IF_HIP_ERROR(STATUS) \
-  if (STATUS != hipSuccess ) { \
-    return get_rocblas_status_for_hip_status(STATUS); \
+#define RETURN_IF_HIP_ERROR(INPUT_STATUS_FOR_CHECK) \
+  hipError_t TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK; \
+  if (TMP_STATUS_FOR_CHECK != hipSuccess ) { \
+    return get_rocblas_status_for_hip_status(TMP_STATUS_FOR_CHECK); \
   }
-#define RETURN_IF_ROCBLAS_ERROR(STATUS) \
-  if (STATUS != rocblas_status_success) { \
-    return STATUS; \
+#define RETURN_IF_ROCBLAS_ERROR(INPUT_STATUS_FOR_CHECK) \
+  rocblas_status TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK; \
+  if (TMP_STATUS_FOR_CHECK != rocblas_status_success) { \
+    return TMP_STATUS_FOR_CHECK; \
   }
 
-#define THROW_IF_COBALT_ERROR(STATUS) \
-  if (STATUS != cobaltStatusSuccess) { \
-    cobaltStatusCheck( STATUS ); \
-    throw get_rocblas_status_for_cobalt_status(STATUS); \
+#define THROW_IF_COBALT_ERROR(INPUT_STATUS_FOR_CHECK) \
+  cobaltStatus TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK; \
+  if (TMP_STATUS_FOR_CHECK != cobaltStatusSuccess) { \
+    cobaltStatusCheck( TMP_STATUS_FOR_CHECK ); \
+    throw get_rocblas_status_for_cobalt_status(TMP_STATUS_FOR_CHECK); \
   }
-#define THROW_IF_HIP_ERROR(STATUS) \
-  if (STATUS != hipSuccess ) { \
-    throw get_rocblas_status_for_hip_status(STATUS); \
+#define THROW_IF_HIP_ERROR(INPUT_STATUS_FOR_CHECK) \
+  hipError_t TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK; \
+  if (TMP_STATUS_FOR_CHECK != hipSuccess ) { \
+    throw get_rocblas_status_for_hip_status(TMP_STATUS_FOR_CHECK); \
   }
-#define THROW_IF_ROCBLAS_ERROR(STATUS) \
-  if (STATUS != rocblas_status_success) { \
-    throw STATUS; \
+#define THROW_IF_ROCBLAS_ERROR(INPUT_STATUS_FOR_CHECK) \
+  rocblas_status TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK; \
+  if (TMP_STATUS_FOR_CHECK != rocblas_status_success) { \
+    throw TMP_STATUS_FOR_CHECK; \
   }
 
 #endif
