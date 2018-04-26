5b07135c792fd206a7db496a272110795d0dbe1e
amcamd <andrew.chapman@amd.com>
2018-04-26 15:24:24 -0500

    scripts to bump master and develop versions

---
+++ bump_develop_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script needs to be edited to bump new master version to new develop for new release.
+# - run this script after running bump_master_version.sh and merging develop into master
+# - run this script in master branch 
+# - after running this script merge master into develop 
+
+OLD_ROCBLAS_VERSION="12.3.0"
+NEW_ROCBLAS_VERSION="13.3.0"
+
+OLD_TENSILE_VERSION="tensile_tag 4.1.0"
+NEW_TENSILE_VERSION="tensile_tag \"develop\""
+
+sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
+sed -i "s/${OLD_TENSILE_VERSION}/${NEW_TENSILE_VERSION}/g" CMakeLists.txt
+
