329774a8f96206692259e046c774d3b8658001cb
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-05-04 16:50:15 -0500

    Jenkins unearthed that trtri also needs to have A marked const for the previous change to compile in all configs.
    This goes deeper down the stack than the previous change. I also want to
    note that the trtri interface in rocBLAS previously did only say A[in],
    which was practically true (as the overwriting code path was never
    executed - hence deleted) but technically wrong. It does however either
    way not *not* represent the actual trtri LAPACK reference interface where
    A is [in,out]:
    http://www.netlib.org/lapack/explore-html/de/da7/dtrsm_8f_source.html
    
    Hence, reference trtri is supposed to be an in-place inversion and our
    implementation may need to be changed again.
    
    
    [ROCm/rocblas commit: e549b8cb45e27ca65ab1436d11a13bc726fded55]

--- library/src/blas3/trtri_trsm.hpp
+++ library/src/blas3/trtri_trsm.hpp
@@ -28,7 +28,7 @@
 */
 template <typename T, rocblas_int NB>
 __global__ void trtri_trsm_kernel(
-    rocblas_fill uplo, rocblas_diagonal diag, rocblas_int n, T* A, rocblas_int lda, T* invA)
+    rocblas_fill uplo, rocblas_diagonal diag, rocblas_int n, const T* A, rocblas_int lda, T* invA)
 {
     // get the individual matrix which is processed by device function
     // device function only see one matrix
@@ -101,7 +101,7 @@ rocblas_status rocblas_trtri_trsm_template(rocblas_handle handle,
                                            rocblas_fill uplo,
                                            rocblas_diagonal diag,
                                            rocblas_int n,
-                                           T* A,
+                                           const T* A,
                                            rocblas_int lda,
                                            T* invA)
 {
