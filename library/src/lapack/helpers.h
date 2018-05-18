7d4421b6d1bf8445b7fd6be695faaa22aa4af231
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-05-18 10:07:30 -0500

    Pacify clang-format.

--- library/src/lapack/helpers.h
+++ library/src/lapack/helpers.h
@@ -4,9 +4,6 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-inline size_t idx2D(const size_t i, const size_t j, const size_t lda){
-    return j*lda+i;
-}
+inline size_t idx2D(const size_t i, const size_t j, const size_t lda) { return j * lda + i; }
 
 #endif /* HELPERS_H */
-
