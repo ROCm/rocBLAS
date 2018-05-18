26f2f3b42c855c39eba43befd372d6a3660cdcf3
Johannes M Dieterich <Johannes.Dieterich@amd.com>
2018-05-18 10:07:49 -0500

    Pacify clang-format.

--- library/src/lapack/roclapack_potf2.cpp
+++ library/src/lapack/roclapack_potf2.cpp
@@ -14,22 +14,14 @@
  * ===========================================================================
  */
 
-extern "C" rocblas_status roclapack_spotf2(rocblas_handle handle,
-                                           rocblas_fill uplo,
-                                           rocblas_int n,
-                                           float* A,
-                                           rocblas_int lda)
+extern "C" rocblas_status
+roclapack_spotf2(rocblas_handle handle, rocblas_fill uplo, rocblas_int n, float* A, rocblas_int lda)
 {
-    return rocblas_potf2_template<float>(
-        handle, uplo, n, A, lda);
+    return rocblas_potf2_template<float>(handle, uplo, n, A, lda);
 }
 
-extern "C" rocblas_status roclapack_dpotf2(rocblas_handle handle,
-                                           rocblas_fill uplo,
-                                           rocblas_int n,
-                                           double* A,
-                                           rocblas_int lda)
+extern "C" rocblas_status roclapack_dpotf2(
+    rocblas_handle handle, rocblas_fill uplo, rocblas_int n, double* A, rocblas_int lda)
 {
-    return rocblas_potf2_template<double>(
-        handle, uplo, n, A, lda);
+    return rocblas_potf2_template<double>(handle, uplo, n, A, lda);
 }
