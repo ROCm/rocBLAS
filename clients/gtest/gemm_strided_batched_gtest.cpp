2c99b8de05b38e285dadba96df85f48b8ca8c0f7
amcamd <andrew.chapman@amd.com>
2018-06-04 15:16:14 -0500

    known_bug rocblas-test for stride_a = 0

--- clients/gtest/gemm_strided_batched_gtest.cpp
+++ clients/gtest/gemm_strided_batched_gtest.cpp
@@ -37,29 +37,53 @@ Representative sampling is sufficient, endless brute-force sampling is not neces
 
 // vector of vector, each vector is a {M, N, K, lda, ldb, ldc, stride_a, stride_b, stride_c};
 // add/delete as a group, in batched gemm, the matrix is much smaller than standard gemm
+// clang-format off
 const vector<vector<int>> matrix_size_range = {
-    {-1, -1, -1, -1, 1, 1, 1, 1, 1},
-    {31, 33, 35, 101, 102, 103, 3605, 3605, 3605},
-    {59, 61, 63, 129, 131, 137, 8631, 8631, 8631},
-    {129, 130, 131, 132, 133, 134, 17554, 17554, 17554},
+    { -1,  -1,  -1,  -1,   1,   1,      1,      1,     1},
+    { 31,  33,  35, 101, 102, 103,   3605,   3605,   3605},
+    { 59,  61,  63, 129, 131, 137,   8631,   8631,   8631},
+    {129, 130, 131, 132, 133, 134,  17554,  17554,  17554},
     {501, 502, 103, 504, 605, 506, 340010, 340010, 340010},
 };
+
+const vector<vector<int>> matrix_size_stride_a_range = {
+    {  3,   3,   3,   3,   3,   3, 9,     9,      9},
+    {  3,   3,   3,   3,   3,   3, 0,     9,      9},
+    { 15,  15,  15,  15,  15,  15, 0,   225,    225},
+    { 16,  16,  16,  16,  16,  16, 0,   256,    256},
+    { 17,  17,  17,  17,  17,  17, 0,   289,    289},
+    { 63,  63,  63,  63,  63,  63, 0,  3969,   3969},
+    { 64,  64,  64,  64,  64,  64, 0,  4096,   4096},
+    { 65,  65,  65,  65,  65,  65, 0,  4225,   4225},
+    {127, 127, 127, 127, 127, 127, 0, 16129,  16129},
+    {128, 128, 128, 128, 128, 128, 0, 16384,  16384},
+    {129, 129, 129, 129, 129, 129, 0, 16641,  16641},
+    {255, 255, 255, 255, 255, 255, 0, 65025,  65025},
+    {256, 256, 256, 256, 256, 256, 0, 65536,  65536},
+    {257, 257, 257, 257, 257, 257, 0, 66049,  66049},
+};
+// clang-format on
 
 // vector of vector, each pair is a {alpha, beta};
 // add/delete this list in pairs, like {2.0, 4.0}
 const vector<vector<double>> alpha_beta_range = {
     {1.0, 0.0}, {-1.0, -1.0},
 };
+const vector<vector<double>> alpha_beta_stride_a_range = {{2.0, 3.0}};
 
 // vector of vector, each pair is a {transA, transB};
 // add/delete this list in pairs, like {'N', 'T'}
 // for single/double precision, 'C'(conjTranspose) will downgraded to 'T' (transpose) internally in
 // sgemm_strided_batched/dgemm_strided_batched,
 const vector<vector<char>> transA_transB_range = {{'N', 'N'}, {'N', 'T'}, {'C', 'N'}, {'T', 'C'}};
+const vector<vector<char>> transA_transB_stride_a_range = {{'N', 'N'}};
 
 // number of gemms in batched gemm
 const vector<int> batch_count_range = {
     -1, 0, 1, 3,
+};
+const vector<int> batch_count_stride_a_range = {
+    1, 3,
 };
 
 /* ===============Google Unit Test==================================================== */
@@ -204,6 +228,13 @@ TEST_P(gemm_strided_batched, double)
 // ValuesIn take each element (a vector) and combine them and feed them to test_p
 // The combinations are  { {M, N, K, lda, ldb, ldc}, {alpha, beta}, {transA, transB}, {batch_count}
 // }
+
+INSTANTIATE_TEST_CASE_P(known_bug_blas3,
+                        gemm_strided_batched,
+                        Combine(ValuesIn(matrix_size_stride_a_range),
+                                ValuesIn(alpha_beta_stride_a_range),
+                                ValuesIn(transA_transB_stride_a_range),
+                                ValuesIn(batch_count_stride_a_range)));
 
 INSTANTIATE_TEST_CASE_P(checkin_blas3,
                         gemm_strided_batched,
