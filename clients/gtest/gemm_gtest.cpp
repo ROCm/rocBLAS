bd9e71d83c07eade9e6d35acce85a38db65ce9b0
amcamd <andrew.chapman@amd.com>
2018-08-14 10:03:37 -0500

    add deepbench gemm_strided_batched tests
    [ROCm/rocblas commit: 86058ec4fb6099c1ce6a94f5cc69adad223285fb]

--- clients/gtest/gemm_gtest.cpp
+++ clients/gtest/gemm_gtest.cpp
@@ -117,6 +117,163 @@ const vector<vector<double>> full_alpha_beta_range = {
 // sgemm/dgemm,
 const vector<vector<char>> transA_transB_range = {{'N', 'N'}, {'N', 'T'}, {'C', 'N'}, {'T', 'C'}};
 
+// clang-format off
+gemm_tuple deepbench0{{192, 64, 784, 784, 784, 192}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench1{{12544, 128, 256, 12544, 256, 12544}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench2{{12544, 256, 64, 12544, 64, 12544}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench3{{12544, 64, 147, 12544, 147, 12544}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench4{{1568, 1024, 256, 1568, 256, 1568}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench5{{1568, 1024, 512, 1568, 512, 1568}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench6{{1568, 256, 1024, 1568, 1024, 1568}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench7{{1568, 256, 256, 1568, 256, 1568}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench8{{1568, 256, 512, 1568, 512, 1568}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench9{{1568, 512, 128, 1568, 128, 1568}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench10{{1653, 256, 3200, 1653, 3200, 1653}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench11{{196, 48, 12800, 196, 12800, 196}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench12{{23040, 16, 9, 23040, 9, 23040}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench13{{26939, 32, 100, 26939, 100, 26939}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench14{{27920, 64, 25, 27920, 25, 27920}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench15{{2916, 64, 27, 2916, 27, 2916}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench16{{3136, 1024, 256, 3136, 256, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench17{{3136, 1024, 512, 3136, 512, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench18{{3136, 192, 512, 3136, 512, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench19{{3136, 256, 1024, 3136, 1024, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench20{{3136, 256, 256, 3136, 256, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench21{{3136, 256, 512, 3136, 512, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench22{{3136, 512, 128, 3136, 128, 3136}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench23{{369, 512, 6400, 369, 6400, 369}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench24{{392, 1024, 256, 392, 256, 392}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench25{{392, 2048, 1024, 392, 1024, 392}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench26{{392, 2048, 512, 392, 512, 392}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench27{{392, 512, 1024, 392, 1024, 392}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench28{{392, 512, 2048, 392, 2048, 392}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench29{{392, 512, 512, 392, 512, 392}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench30{{49, 128, 20800, 49, 20800, 49}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench31{{49, 512, 2048, 49, 2048, 49}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench32{{50176, 64, 27, 50176, 27, 50176}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench33{{5760, 32, 144, 5760, 144, 5760}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench34{{6272, 128, 256, 6272, 256, 6272}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench35{{6272, 256, 64, 6272, 64, 6272}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench36{{6308, 32, 1600, 6308, 1600, 6308}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench37{{6786, 128, 1600, 6786, 1600, 6786}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench38{{784, 1024, 256, 784, 256, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench39{{784, 2048, 1024, 784, 1024, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench40{{784, 2048, 512, 784, 512, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench41{{784, 256, 832, 784, 832, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench42{{784, 32, 4800, 784, 4800, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench43{{784, 512, 1024, 784, 1024, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench44{{784, 512, 2048, 784, 2048, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench45{{784, 512, 512, 784, 512, 784}, {1, 0}, {'N', 'N'}};
+gemm_tuple deepbench46{{12544, 147, 64, 12544, 147, 12544}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench47{{12544, 256, 128, 12544, 256, 12544}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench48{{12544, 64, 256, 12544, 64, 12544}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench49{{1568, 128, 512, 1568, 128, 1568}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench50{{1568, 512, 1024, 1568, 512, 1568}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench51{{1568, 512, 256, 1568, 512, 1568}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench52{{1653, 3200, 256, 1653, 3200, 1653}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench53{{196, 12800, 48, 196, 12800, 196}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench54{{23040, 9, 16, 23040, 9, 23040}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench55{{26939, 100, 32, 26939, 100, 26939}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench56{{27920, 25, 64, 27920, 25, 27920}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench57{{2916, 27, 64, 2916, 27, 2916}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench58{{3136, 128, 512, 3136, 128, 3136}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench59{{3136, 512, 1024, 3136, 512, 3136}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench60{{3136, 512, 256, 3136, 512, 3136}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench61{{369, 6400, 512, 369, 6400, 369}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench62{{392, 1024, 2048, 392, 1024, 392}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench63{{392, 1024, 512, 392, 1024, 392}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench64{{392, 256, 1024, 392, 256, 392}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench65{{49, 2048, 512, 49, 2048, 49}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench66{{49, 20800, 128, 49, 20800, 49}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench67{{6272, 256, 128, 6272, 256, 6272}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench68{{6272, 64, 256, 6272, 64, 6272}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench69{{6308, 1600, 32, 6308, 1600, 6308}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench70{{6786, 1600, 128, 6786, 1600, 6786}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench71{{784, 1024, 2048, 784, 1024, 784}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench72{{784, 1024, 512, 784, 1024, 784}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench73{{784, 256, 1024, 784, 256, 784}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench74{{784, 4800, 32, 784, 4800, 784}, {1, 0}, {'N', 'T'}};
+gemm_tuple deepbench75{{100, 32, 26939, 26939, 26939, 100}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench76{{1024, 2048, 49, 49, 49, 1024}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench77{{1024, 256, 196, 196, 196, 1024}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench78{{1024, 512, 49, 49, 49, 1024}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench79{{1152, 128, 7000, 7000, 7000, 1152}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench80{{1152, 128, 729, 729, 729, 1152}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench81{{1152, 128, 784, 784, 784, 1152}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench82{{1152, 256, 196, 196, 196, 1152}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench83{{1152, 256, 3136, 3136, 3136, 1152}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench84{{12800, 48, 196, 196, 196, 12800}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench85{{128, 512, 196, 196, 196, 128}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench86{{128, 512, 784, 784, 784, 128}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench87{{144, 32, 5760, 5760, 5760, 144}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench88{{147, 64, 12544, 12544, 12544, 147}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench89{{1600, 128, 6786, 6786, 6786, 1600}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench90{{1600, 32, 6308, 6308, 6308, 1600}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench91{{192, 64, 784, 784, 784, 192}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench92{{2048, 512, 49, 49, 49, 2048}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench93{{20800, 128, 49, 49, 49, 20800}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench94{{2304, 256, 1680, 1680, 1680, 2304}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench95{{2304, 256, 196, 196, 196, 2304}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench96{{2304, 512, 49, 49, 49, 2304}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench97{{2304, 512, 784, 784, 784, 2304}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench98{{256, 1024, 196, 196, 196, 256}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench99{{256, 1024, 49, 49, 49, 256}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench100{{256, 128, 784, 784, 784, 256}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench101{{256, 256, 196, 196, 196, 256}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench102{{256, 64, 3136, 3136, 3136, 256}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench103{{25, 64, 27920, 27920, 27920, 25}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench104{{27, 64, 2916, 2916, 2916, 27}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench105{{27, 64, 50176, 50176, 50176, 27}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench106{{288, 64, 1440, 1440, 1440, 288}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench107{{3200, 256, 1653, 1653, 1653, 3200}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench108{{4608, 512, 196, 196, 196, 4608}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench109{{4608, 512, 420, 420, 420, 4608}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench110{{4608, 512, 49, 49, 49, 4608}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench111{{4800, 32, 784, 784, 784, 4800}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench112{{512, 1024, 196, 196, 196, 512}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench113{{512, 128, 784, 784, 784, 512}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench114{{512, 192, 196, 196, 196, 512}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench115{{512, 2048, 49, 49, 49, 512}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench116{{512, 256, 196, 196, 196, 512}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench117{{512, 512, 49, 49, 49, 512}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench118{{576, 128, 12544, 12544, 12544, 576}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench119{{576, 128, 360, 360, 360, 576}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench120{{576, 64, 28000, 28000, 28000, 576}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench121{{576, 64, 2916, 2916, 2916, 576}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench122{{576, 64, 3136, 3136, 3136, 576}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench123{{6400, 512, 369, 369, 369, 6400}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench124{{64, 256, 3136, 3136, 3136, 64}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench125{{64, 256, 784, 784, 784, 64}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench126{{64, 64, 12544, 12544, 12544, 64}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench127{{832, 256, 49, 49, 49, 832}, {1, 1}, {'T', 'N'}};
+gemm_tuple deepbench128{{9, 16, 23040, 23040, 23040, 9}, {1, 1}, {'T', 'N'}};
+
+const vector<gemm_tuple> deepbench_vec = {
+    deepbench0,   deepbench1,   deepbench2,   deepbench3,   deepbench4,   deepbench5,
+    deepbench6,   deepbench7,   deepbench8,   deepbench9,   deepbench10,  deepbench11,
+    deepbench12,  deepbench13,  deepbench14,  deepbench15,  deepbench16,  deepbench17,
+    deepbench18,  deepbench19,  deepbench20,  deepbench21,  deepbench22,  deepbench23,
+    deepbench24,  deepbench25,  deepbench26,  deepbench27,  deepbench28,  deepbench29,
+    deepbench30,  deepbench31,  deepbench32,  deepbench33,  deepbench34,  deepbench35,
+    deepbench36,  deepbench37,  deepbench38,  deepbench39,  deepbench40,  deepbench41,
+    deepbench42,  deepbench43,  deepbench44,  deepbench45,  deepbench46,  deepbench47,
+    deepbench48,  deepbench49,  deepbench50,  deepbench51,  deepbench52,  deepbench53,
+    deepbench54,  deepbench55,  deepbench56,  deepbench57,  deepbench58,  deepbench59,
+    deepbench60,  deepbench61,  deepbench62,  deepbench63,  deepbench64,  deepbench65,
+    deepbench66,  deepbench67,  deepbench68,  deepbench69,  deepbench70,  deepbench71,
+    deepbench72,  deepbench73,  deepbench74,  deepbench75,  deepbench76,  deepbench77,
+    deepbench78,  deepbench79,  deepbench80,  deepbench81,  deepbench82,  deepbench83,
+    deepbench84,  deepbench85,  deepbench86,  deepbench87,  deepbench88,  deepbench89,
+    deepbench90,  deepbench91,  deepbench92,  deepbench93,  deepbench94,  deepbench95,
+    deepbench96,  deepbench97,  deepbench98,  deepbench99,  deepbench100, deepbench101,
+    deepbench102, deepbench103, deepbench104, deepbench105, deepbench106, deepbench107,
+    deepbench108, deepbench109, deepbench110, deepbench111, deepbench112, deepbench113,
+    deepbench114, deepbench115, deepbench116, deepbench117, deepbench118, deepbench119,
+    deepbench120, deepbench121, deepbench122, deepbench123, deepbench124, deepbench125,
+    deepbench126, deepbench127, deepbench128,
+};
+// clang-format on
+
 /* ===============Google Unit Test==================================================== */
 
 /* =====================================================================
@@ -413,3 +570,9 @@ INSTANTIATE_TEST_CASE_P(daily_blas3_chunk,
                         Combine(ValuesIn(chunk_matrix_size_range),
                                 ValuesIn(alpha_beta_2_3_range),
                                 ValuesIn(transA_transB_range)));
+
+INSTANTIATE_TEST_CASE_P(daily_blas3_deepbench_sizes, parameterized_gemm, ValuesIn(deepbench_vec));
+
+INSTANTIATE_TEST_CASE_P(daily_blas3_deepbench_sizes,
+                        parameterized_half_gemm,
+                        ValuesIn(deepbench_vec));
