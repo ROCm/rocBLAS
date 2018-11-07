efd7a67d0b3eaa33b53b8cbb2d4498adc0199867
Yoichi Yoshida <yoichi.yoshida@amd.com>
2018-11-06 19:32:47 -0500

    fixing restrictions for Int8

--- library/src/blas_ex/rocblas_gemm_ex.cpp
+++ library/src/blas_ex/rocblas_gemm_ex.cpp
@@ -438,9 +438,16 @@ extern "C" rocblas_status rocblas_gemm_ex(rocblas_handle handle,
             c_type == rocblas_datatype_i32_r && d_type == rocblas_datatype_i32_r &&
             compute_type == rocblas_datatype_i32_r)
     {
+<<<<<<< 956c5380ed992f429360e65a88c65343336248a2
         // For now, K must be a multiple of 4, and/or LDA/LDB based on transpose mode
         if(k % 4 != 0 || (trans_a != rocblas_operation_none && lda % 4 != 0) ||
            (trans_b == rocblas_operation_none && ldb % 4 != 0))
+=======
+        // For now, K must be a multiple of 4
+        if (k % 4 != 0 ||
+            ((trans_a != rocblas_operation_none) && (lda % 4 != 0)) ||
+            ((trans_b == rocblas_operation_none) && (ldb % 4 != 0)))
+>>>>>>> fixing restrictions for Int8
         {
             rb_status = rocblas_status_invalid_size;
         }
@@ -449,8 +456,8 @@ extern "C" rocblas_status rocblas_gemm_ex(rocblas_handle handle,
             // adjust by 4 for Tensile
             lda      = (trans_a == rocblas_operation_none) ? lda : lda / 4;
             ldb      = (trans_b == rocblas_operation_none) ? ldb / 4 : ldb;
-            stride_a = (trans_a == rocblas_operation_none) ? stride_a / 4 : stride_a;
-            stride_b = (trans_b == rocblas_operation_none) ? stride_b : stride_b / 4;
+            stride_a = stride_a / 4;
+            stride_b = stride_b / 4;
             k = k / 4;
 
             rb_status = gemm_ex_typecasting<TensileInt8x4, TensileInt32, TensileInt32>(handle,
@@ -951,7 +958,11 @@ extern "C" rocblas_status rocblas_gemm_strided_batched_ex(rocblas_handle handle,
             compute_type == rocblas_datatype_i32_r)
     {
         // For now, K must be a multiple of 4
-        if (k % 4 != 0)
+        if (k % 4 != 0 ||
+            ((trans_a != rocblas_operation_none) && (lda % 4 != 0)) ||
+            ((trans_b == rocblas_operation_none) && (ldb % 4 != 0)) ||
+            stride_a % 4 != 0 ||
+            stride_b % 4 != 0)
         {
             rb_status = rocblas_status_invalid_size;
         }
@@ -960,8 +971,8 @@ extern "C" rocblas_status rocblas_gemm_strided_batched_ex(rocblas_handle handle,
             // adjust by 4 for Tensile
             lda      = (trans_a == rocblas_operation_none) ? lda : lda / 4;
             ldb      = (trans_b == rocblas_operation_none) ? ldb / 4 : ldb;
-            stride_a = (trans_a == rocblas_operation_none) ? stride_a / 4 : stride_a;
-            stride_b = (trans_b == rocblas_operation_none) ? stride_b : stride_b / 4;
+            stride_a = stride_a / 4;
+            stride_b = stride_b / 4;
             k = k / 4;
 
             rb_status = gemm_ex_typecasting<TensileInt8x4, TensileInt32, TensileInt32>(handle,
