d1201853b8377eef565487d6936895fbad7a070d
William Gilmartin <wgilmart@wgilmart-ubuntu-0.amd.com>
2018-11-08 10:51:49 -0800

    remove the 2 gemmv entries

--- scripts/performance/benchmark_p1b3.sh
+++ scripts/performance/benchmark_p1b3.sh
@@ -10,8 +10,6 @@ fi
 ${bench} -f gemm -r s --transposeA N --transposeB N -m 500 -n 200 -k 1000 --alpha 1 --lda 500 --ldb 1000 --beta 0 --ldc 500
 ${bench} -f gemm -r s --transposeA N --transposeB N -m 100 -n 200 -k 500 --alpha 1 --lda 100 --ldb 500 --beta 0 --ldc 100
 ${bench} -f gemm -r s --transposeA N --transposeB N -m 50 -n 200 -k 100 --alpha 1 --lda 50 --ldb 100 --beta 0 --ldc 50
-${bench} -f gemv -r s --transposeA T -m 50 -n 200 --alpha 1 --lda 50 --incx 1 --beta 0 --incy 1
-${bench} -f gemv -r s --transposeA N -m 50 -n 200 --alpha 1 --lda 50 --incx 1 --beta 0 --incy 1
 ${bench} -f gemm -r s --transposeA T --transposeB N -m 50 -n 200 -k 1 --alpha 1 --lda 1 --ldb 1 --beta 0 --ldc 50
 ${bench} -f gemm -r s --transposeA N --transposeB T -m 50 -n 100 -k 200 --alpha 1 --lda 50 --ldb 100 --beta 0 --ldc 50
 ${bench} -f gemm -r s --transposeA T --transposeB N -m 100 -n 200 -k 50 --alpha 1 --lda 50 --ldb 50 --beta 0 --ldc 100
