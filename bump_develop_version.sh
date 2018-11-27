4f0518ea4918903f53ea03cd8ba6e57dd4238529
Alex Liu <35415350+zaliu@users.noreply.github.com>
2018-11-26 18:38:17 -0600

    version for periodic master branch release

--- bump_develop_version.sh
+++ bump_develop_version.sh
@@ -5,11 +5,11 @@
 # - run this script in master branch 
 # - after running this script merge master into develop 
 
-OLD_ROCBLAS_VERSION="14.3.4"
-NEW_ROCBLAS_VERSION="15.3.4"
+OLD_ROCBLAS_VERSION="14.3.5"
+NEW_ROCBLAS_VERSION="15.3.5"
 
-OLD_TENSILE_VERSION="tensile_tag e747c75b7ebc8bf9f005f5881be18122fc94f7f7"
-NEW_TENSILE_VERSION="tensile_tag \"develop\""
+OLD_TENSILE_VERSION="tensile_tag b0e0ce85f8b77ce384296850f85715c39d123108"
+NEW_TENSILE_VERSION="tensile_tag \"develop-rocm20\""
 
 sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
 sed -i "s/${OLD_TENSILE_VERSION}/${NEW_TENSILE_VERSION}/g" CMakeLists.txt
