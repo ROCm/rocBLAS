c2b43e9fce2430afa932fc6cd342132aa12d7c98
Saad Rahim <saad.rahim@amd.com>
2018-12-10 13:42:14 -0800

    Adding missing stuff

--- docs/run_doxygen.sh
+++ docs/run_doxygen.sh
@@ -4,6 +4,6 @@ if [ -d docBin ]; then
     rm -rf docBin
 fi
 
-sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocfft.h > rocfft.h
+sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas.h > rocblas.h
 doxygen Doxyfile
-rm rocfft.h
+rm rocblas.h
