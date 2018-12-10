ce0f9be1697d57c6ff40cbe8a6b1b0f51f5bad75
Saad Rahim <saad.rahim@amd.com>
2018-12-10 13:38:32 -0800

    Changing library name

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -4,7 +4,7 @@ if [ -d docBin ]; then
     rm -rf docBin
 fi
 
-sed -e 's/ROCFFT_EXPORT //g' ../library/include/rocfft.h > rocfft.h
+sed -e 's/ROCFFT_EXPORT //g' ../library/include/rocblas.h > rocblas.h
 doxygen Doxyfile
 
 cd source
@@ -12,5 +12,4 @@ make clean
 make html
 cd ..
 
-rm rocfft.h
-
+rm rocblas.h
