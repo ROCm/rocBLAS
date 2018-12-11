55a0c07137906c1d4803e7c2d659192dce445cb7
Saad Rahim <saad.rahim@amd.com>
2018-12-11 13:53:36 -0800

    Fixing copyright

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -6,6 +6,7 @@ fi
 
 sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-functions.h > rocblas-functions.h
 sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-types.h > rocblas-types.h
+sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-auxilliary.h > rocblas-auxilliary.h
 
 
 doxygen Doxyfile
