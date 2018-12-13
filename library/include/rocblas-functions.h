20e23398838c7acff75c58b235fb847b80086db1
Saad Rahim <saad.rahim@amd.com>
2018-12-13 14:01:15 -0800

    Build info

--- library/include/rocblas-functions.h
+++ library/include/rocblas-functions.h
@@ -1477,6 +1477,17 @@ ROCBLAS_EXPORT rocblas_status rocblas_gemm_strided_batched_ex(rocblas_handle han
  *    build information
  * ===========================================================================
  */
+ 
+ /*******************************************************************************
+ *! \brief   loads char* buf with the rocblas library version. size_t len
+     is the maximum length of char* buf.
+     
+     @param[in, out]
+     buf    pointer to buffer for version string
+     
+     @param[in]
+     len    lenght of buf
+ ******************************************************************************/
 ROCBLAS_EXPORT rocblas_status rocblas_get_version_string(char* buf, size_t len);
 
 #ifdef __cplusplus
