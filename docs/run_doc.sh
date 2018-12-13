96300760c96470822bfdc1a4dff02d6273d580c6
Saad Rahim <saad.rahim@amd.com>
2018-12-13 11:43:33 -0800

    File name wrong

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -8,7 +8,7 @@ sh run_doxygen.sh
 
 #sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-functions.h > rocblas-functions.h
 #sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-types.h > rocblas-types.h
-#sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-auxilliary.h > rocblas-auxilliary.h
+#sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-auxiliary.h > rocblas-auxiliary.h
 
 
 #doxygen Doxyfile
