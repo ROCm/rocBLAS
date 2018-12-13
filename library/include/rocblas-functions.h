a6950b3b2de9521fa4a482f8ea2f63c21785183f
Saad Rahim <saad.rahim@amd.com>
2018-12-13 14:15:58 -0800

    Build info

--- library/include/rocblas-functions.h
+++ library/include/rocblas-functions.h
@@ -1486,7 +1486,7 @@ ROCBLAS_EXPORT rocblas_status rocblas_gemm_strided_batched_ex(rocblas_handle han
     buf             pointer to buffer for version string
 
     @param[in]
-    len             lenght of buf
+    len             length of buf
 
  ******************************************************************************/
 ROCBLAS_EXPORT rocblas_status rocblas_get_version_string(char* buf, size_t len);
