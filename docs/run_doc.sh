0442404597085c2919050de5ff0f322dbf7b3658
Saad Rahim <saad.rahim@amd.com>
2018-12-13 14:45:30 -0800

    Deleting commented out lines

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -6,16 +6,9 @@ fi
 
 sh run_doxygen.sh
 
-#sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-functions.h > rocblas-functions.h
-#sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-types.h > rocblas-types.h
-#sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-auxiliary.h > rocblas-auxiliary.h
-
-
-#doxygen Doxyfile
-
 cd source
 make clean
 make html
 cd ..
 
-rm rocblas.h
+rm *.h
