2b4333d4446dcb1644778bfc1c11bbb32070a7aa
amcamd <andrew.chapman@amd.com>
2019-01-28 14:05:10 -0600

    version for master branch release

--- bump_develop_version.sh
+++ bump_develop_version.sh
@@ -5,10 +5,10 @@
 # - run this script in master branch 
 # - after running this script merge master into develop 
 
-OLD_ROCBLAS_VERSION="2.0.0"
-NEW_ROCBLAS_VERSION="2.1.0"
+OLD_ROCBLAS_VERSION="2.0.2"
+NEW_ROCBLAS_VERSION="2.1.2"
 
-OLD_TENSILE_VERSION="tensile_tag 7e50085d9e310349152f686275d0ad98cc58b130"
+OLD_TENSILE_VERSION="tensile_tag ad79c035bf59d236982f024c1494cd82234c085b"
 NEW_TENSILE_VERSION="tensile_tag \"develop\""
 
 sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
