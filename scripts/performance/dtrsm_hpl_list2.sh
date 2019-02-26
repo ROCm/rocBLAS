41da5dcbb49b236cb3d7e3bfbcb60b9aa98ea855
Yoichi Yoshida <44450773+yoichiyoshida@users.noreply.github.com>
2019-02-26 11:51:15 -0700

    adding performance scripts for dtrsm hpl (#492)
    [ROCm/rocblas commit: 3e2a412fe4eeb678250600e3e5d55a19e2de94e8]

---
+++ scripts/performance/dtrsm_hpl_list2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+bench=./rocblas-bench
+if [ ! -f ${bench} ]; then
+	echo ${bench} not found, exit...
+	exit 1
+else
+	echo ">>" $(realpath $(ldd ${bench} | grep rocblas | awk '{print $3;}'))
+fi
+
+for i in {256..27648..256}; do
+	${bench} -f trsm -r d --side R --uplo L --transposeA T --diag U \
+	-m ${i} -n 256 --lda 256 --ldb ${i} \
+	--alpha 1 -i 1 2>&1 | egrep '[L,R],[U,L],[N,T],[U,N]|,fault'
+done
+
+for i in {256..27648..256}; do
+	${bench} -f trsm -r d --side R --uplo L --transposeA T --diag U \
+	-m ${i} -n 256 --lda 44800 --ldb ${i} \
+	--alpha 1 -i 1 2>&1 | egrep '[L,R],[U,L],[N,T],[U,N]|,fault'
+done
+
+for i in {1..27393..256}; do
+	${bench} -f trsm -r d --side R --uplo L --transposeA T --diag U \
+	-m ${i} -n 256 --lda 256 --ldb ${i} \
+	--alpha 1 -i 1 2>&1 | egrep '[L,R],[U,L],[N,T],[U,N]|,fault'
+done
+
+for i in {1..27393..256}; do
+	${bench} -f trsm -r d --side R --uplo L --transposeA T --diag U \
+	-m ${i} -n 256 --lda 44800 --ldb ${i} \
+	--alpha 1 -i 1 2>&1 | egrep '[L,R],[U,L],[N,T],[U,N]|,fault'
+done
+
+for i in {384..27648..384}; do
+	${bench} -f trsm -r d --side R --uplo L --transposeA T --diag U \
+	-m ${i} -n 384 --lda 384 --ldb ${i} \
+	--alpha 1 -i 1 2>&1 | egrep '[L,R],[U,L],[N,T],[U,N]|,fault'
+done
+
+for i in {1..27265..384}; do
+	${bench} -f trsm -r d --side R --uplo L --transposeA T --diag U \
+	-m ${i} -n 384 --lda 384 --ldb ${i} \
+	--alpha 1 -i 1 2>&1 | egrep '[L,R],[U,L],[N,T],[U,N]|,fault'
+done
