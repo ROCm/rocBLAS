50d628967fc6d0beef71d1a22b1bb565dc8f5cc2
amcamd <andrew.chapman@amd.com>
2019-05-31 11:48:05 -0500

    add bfloat16 documentation
    [ROCm/rocblas commit: 31d13155f6af485f7ae44aff076c8292fd00ff7e]

--- docs/run_doxygen.sh
+++ docs/run_doxygen.sh
@@ -7,6 +7,7 @@ fi
 sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas.h > rocblas.h
 sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-functions.h > rocblas-functions.h
 sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-types.h > rocblas-types.h
+sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas_bfloat16.h > rocblas_bfloat16.h
 sed -e 's/ROCBLAS_EXPORT //g' ../library/include/rocblas-auxiliary.h > rocblas-auxiliary.h
 
 
