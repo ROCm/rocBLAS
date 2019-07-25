ec3655bae473b0bbc3c23a537b66919585687600
Lee Killough <15950023+leekillough@users.noreply.github.com>
2019-07-25 16:18:19 -0700

    Adding level 1 complex and gemv complex (#616)
    * Added functionality for complex BLAS1 functions, including:
     1. caxpy/zaxpy
     2. cdotu/zdotu
     3. cdotc/zdotc
     4. scnrm2/dznrm2
     5. cscal/zscal
     6. csscal/zdscal
     7. icamax/izamax
     8. icamin/izamin
     9. scasum/dzasum
    
    Made changes to include testing these in unit tests.
    
    * Added functionality for additional BLAS1 functions:
      1. cswap/zswap
      2. ccopy/zcopy
    
    Fixed issues with complex type struct, fixed isues with complex unit tests.
    
    * Adding support for performance testing of complex blas1 functions.
    
    * Fixed some complex number logging issues.
    
    * Simplified scaling functions, performance testing should work for complex calls now as well
    
    * Added single precision complex functionality to gemv. Formatting changes.
    
    * Added support for double precision gemv
    
    * Removed c++17 references to if constexpr, other cleanup.
    
    * Small updates
    
    * merge from develop; refactor; reimplement rocblas-complex-types.h
    
    * bug fixing
    
    * Scal type ordering fixes, small gemv fixes.
    
    * Changed asum and nrm2 testing organization
    
    * Change spacing, generalize script
    
    * Complex alpha logging for rocblas-bench added
    
    * Switched order of template variables for scal
    
    * Changed complex testing behaviour to be the same as real testing.
    
    * Changed call to std::conj to our own conj.
    
    * Addressed logging test failures
    
    * Change conj(z) to default to returning z; return std::conj(z) only if is_complex<T>

--- clients/include/testing_dot.hpp
+++ clients/include/testing_dot.hpp
@@ -4,6 +4,7 @@
 
 #include "cblas_interface.hpp"
 #include "flops.hpp"
+#include "near.hpp"
 #include "norm.hpp"
 #include "rocblas.hpp"
 #include "rocblas_init.hpp"
@@ -14,7 +15,7 @@
 #include "unit.hpp"
 #include "utility.hpp"
 
-template <typename T>
+template <typename T, bool CONJ = false>
 void testing_dot_bad_arg(const Arguments& arg)
 {
     rocblas_int         N         = 100;
@@ -34,17 +35,29 @@ void testing_dot_bad_arg(const Arguments& arg)
 
     CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_device));
 
-    EXPECT_ROCBLAS_STATUS(rocblas_dot<T>(handle, N, nullptr, incx, dy, incy, d_rocblas_result),
-                          rocblas_status_invalid_pointer);
-    EXPECT_ROCBLAS_STATUS(rocblas_dot<T>(handle, N, dx, incx, nullptr, incy, d_rocblas_result),
-                          rocblas_status_invalid_pointer);
-    EXPECT_ROCBLAS_STATUS(rocblas_dot<T>(handle, N, dx, incx, dy, incy, nullptr),
-                          rocblas_status_invalid_pointer);
-    EXPECT_ROCBLAS_STATUS(rocblas_dot<T>(nullptr, N, dx, incx, dy, incy, d_rocblas_result),
-                          rocblas_status_invalid_handle);
+    EXPECT_ROCBLAS_STATUS(
+        (CONJ ? rocblas_dotc<T>
+              : rocblas_dot<T>)(handle, N, nullptr, incx, dy, incy, d_rocblas_result),
+        rocblas_status_invalid_pointer);
+    EXPECT_ROCBLAS_STATUS(
+        (CONJ ? rocblas_dotc<T>
+              : rocblas_dot<T>)(handle, N, dx, incx, nullptr, incy, d_rocblas_result),
+        rocblas_status_invalid_pointer);
+    EXPECT_ROCBLAS_STATUS(
+        (CONJ ? rocblas_dotc<T> : rocblas_dot<T>)(handle, N, dx, incx, dy, incy, nullptr),
+        rocblas_status_invalid_pointer);
+    EXPECT_ROCBLAS_STATUS(
+        (CONJ ? rocblas_dotc<T> : rocblas_dot<T>)(nullptr, N, dx, incx, dy, incy, d_rocblas_result),
+        rocblas_status_invalid_handle);
 }
 
 template <typename T>
+void testing_dotc_bad_arg(const Arguments& arg)
+{
+    testing_dot_bad_arg<T, true>(arg);
+}
+
+template <typename T, bool CONJ = false>
 void testing_dot(const Arguments& arg)
 {
     rocblas_int N    = arg.N;
@@ -73,14 +86,16 @@ void testing_dot(const Arguments& arg)
         }
 
         CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_device));
-        CHECK_ROCBLAS_ERROR(rocblas_dot<T>(handle, N, dx, incx, dy, incy, d_rocblas_result));
+        CHECK_ROCBLAS_ERROR(
+            (CONJ ? rocblas_dotc<T>
+                  : rocblas_dot<T>)(handle, N, dx, incx, dy, incy, d_rocblas_result));
         return;
     }
 
     rocblas_int abs_incx = incx >= 0 ? incx : -incx;
     rocblas_int abs_incy = incy >= 0 ? incy : -incy;
-    size_t      size_x   = N * static_cast<size_t>(abs_incx);
-    size_t      size_y   = N * static_cast<size_t>(abs_incy);
+    size_t      size_x   = N * size_t(abs_incx);
+    size_t      size_y   = N * size_t(abs_incy);
 
     // allocate memory on device
     device_vector<T> dx(size_x);
@@ -112,19 +127,23 @@ void testing_dot(const Arguments& arg)
     {
         // GPU BLAS, rocblas_pointer_mode_host
         CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_host));
-        CHECK_ROCBLAS_ERROR(rocblas_dot<T>(handle, N, dx, incx, dy, incy, &rocblas_result_1));
+        CHECK_ROCBLAS_ERROR(
+            (CONJ ? rocblas_dotc<T>
+                  : rocblas_dot<T>)(handle, N, dx, incx, dy, incy, &rocblas_result_1));
 
         // GPU BLAS, rocblas_pointer_mode_device
         CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_device));
-        CHECK_ROCBLAS_ERROR(rocblas_dot<T>(handle, N, dx, incx, dy, incy, d_rocblas_result_2));
+        CHECK_ROCBLAS_ERROR(
+            (CONJ ? rocblas_dotc<T>
+                  : rocblas_dot<T>)(handle, N, dx, incx, dy, incy, d_rocblas_result_2));
         CHECK_HIP_ERROR(
             hipMemcpy(&rocblas_result_2, d_rocblas_result_2, sizeof(T), hipMemcpyDeviceToHost));
 
         // CPU BLAS
         cpu_time_used = get_time_us();
-        cblas_dot<T>(N, hx, incx, hy, incy, &cpu_result);
+        (CONJ ? cblas_dotc<T> : cblas_dot<T>)(N, hx, incx, hy, incy, &cpu_result);
         cpu_time_used = get_time_us() - cpu_time_used;
-        cblas_gflops  = axpy_gflop_count<T>(N) / cpu_time_used * 1e6 * 1;
+        cblas_gflops  = dot_gflop_count<T>(N) / cpu_time_used * 1e6 * 1;
 
         if(arg.unit_check)
         {
@@ -134,12 +153,11 @@ void testing_dot(const Arguments& arg)
 
         if(arg.norm_check)
         {
-            printf("cpu=%f, gpu_host_ptr=%f, gpu_device_ptr=%f\n",
-                   cpu_result,
-                   rocblas_result_1,
-                   rocblas_result_2);
-            rocblas_error_1 = fabs((cpu_result - rocblas_result_1) / cpu_result);
-            rocblas_error_2 = fabs((cpu_result - rocblas_result_2) / cpu_result);
+            std::cout << "cpu=" << cpu_result << ", gpu_host_ptr=" << rocblas_result_1
+                      << ", gpu_device_ptr=" << rocblas_result_2 << "\n";
+
+            rocblas_error_1 = std::abs((cpu_result - rocblas_result_1) / cpu_result);
+            rocblas_error_2 = std::abs((cpu_result - rocblas_result_2) / cpu_result);
         }
     }
 
@@ -151,14 +169,16 @@ void testing_dot(const Arguments& arg)
 
         for(int iter = 0; iter < number_cold_calls; iter++)
         {
-            rocblas_dot<T>(handle, N, dx, incx, dy, incy, &rocblas_result_1);
+            (CONJ ? rocblas_dotc<T>
+                  : rocblas_dot<T>)(handle, N, dx, incx, dy, incy, &rocblas_result_1);
         }
 
         gpu_time_used = get_time_us(); // in microseconds
 
         for(int iter = 0; iter < number_hot_calls; iter++)
         {
-            rocblas_dot<T>(handle, N, dx, incx, dy, incy, &rocblas_result_1);
+            (CONJ ? rocblas_dotc<T>
+                  : rocblas_dot<T>)(handle, N, dx, incx, dy, incy, &rocblas_result_1);
         }
 
         gpu_time_used     = (get_time_us() - gpu_time_used) / number_hot_calls;
@@ -179,4 +199,10 @@ void testing_dot(const Arguments& arg)
 
         std::cout << std::endl;
     }
+}
+
+template <typename T>
+void testing_dotc(const Arguments& arg)
+{
+    testing_dot<T, true>(arg);
 }
