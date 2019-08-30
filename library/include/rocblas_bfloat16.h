de55d7fe6d11abe7879d345eced3411e94afe0c2
Lee Killough <killough@leekillough.com>
2019-08-30 13:03:05 -0400

    Replace explicit conversion of bloat16 to float and double with implicit conversion of bfloat16 to float

--- library/include/rocblas_bfloat16.h
+++ library/include/rocblas_bfloat16.h
@@ -63,7 +63,7 @@ struct rocblas_bfloat16
     }
 
     // zero extend lower 16 bits of bfloat16 to convert to IEEE float
-    explicit constexpr __host__ __device__ operator float() const
+    constexpr __host__ __device__ operator float() const
     {
         union
         {
@@ -71,11 +71,6 @@ struct rocblas_bfloat16
             float    fp32;
         } u = {uint32_t(data) << 16};
         return u.fp32;
-    }
-
-    explicit constexpr __host__ __device__ operator double() const
-    {
-        return double(float(*this));
     }
 
 private:
