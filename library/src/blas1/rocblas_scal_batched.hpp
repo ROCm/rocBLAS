3d8139bb849d949b26f67ccae69974bcc4b31082
First Last <daine.mcniven@amd.com>
2019-09-06 15:13:46 -0600

    Exposing impl functions in hpp file so they can be used from within.

---
+++ library/src/blas1/rocblas_scal_batched.hpp
@@ -0,0 +1,87 @@
+/* ************************************************************************
+ * Copyright 2016-2019 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+#include "handle.h"
+#include "logging.h"
+#include "rocblas.h"
+#include "scal_host.hpp"
+#include "utility.h"
+
+template <typename T, typename = T>
+constexpr char rocblas_scal_name[] = "unknown";
+template <>
+constexpr char rocblas_scal_name<float>[] = "rocblas_sscal_batched";
+template <>
+constexpr char rocblas_scal_name<double>[] = "rocblas_dscal_batched";
+template <>
+constexpr char rocblas_scal_name<rocblas_float_complex>[] = "rocblas_cscal_batched";
+template <>
+constexpr char rocblas_scal_name<rocblas_double_complex>[] = "rocblas_zscal_batched";
+template <>
+constexpr char rocblas_scal_name<rocblas_float_complex, float>[] = "rocblas_csscal_batched";
+template <>
+constexpr char rocblas_scal_name<rocblas_double_complex, double>[] = "rocblas_zdscal_batched";
+
+template <typename T, typename U>
+rocblas_status rocblas_scal_batched_impl(rocblas_handle handle,
+                                            rocblas_int    n,
+                                            const U*       alpha,
+                                            T*             x[],
+                                            rocblas_int    offsetx,
+                                            rocblas_int    incx,
+                                            rocblas_int    batch_count)
+{
+    if(!handle)
+        return rocblas_status_invalid_handle;
+    if(!alpha)
+        return rocblas_status_invalid_pointer;
+
+    auto layer_mode = handle->layer_mode;
+    if(handle->pointer_mode == rocblas_pointer_mode_host)
+    {
+        if(layer_mode & rocblas_layer_mode_log_trace)
+            log_trace(handle, rocblas_scal_name<T, U>, n, *alpha, x, incx, batch_count);
+
+        // there are an extra 2 scal functions, thus
+        // the -r mode will not work correctly. Substitute
+        // with --a_type and --b_type (?)
+        // ANSWER: -r is syntatic sugar; the types can be specified separately
+        if(layer_mode & rocblas_layer_mode_log_bench)
+        {
+            std::stringstream alphass;
+            alphass << "--alpha " << std::real(*alpha)
+                    << (std::imag(*alpha) != 0
+                            ? (" --alphai " + std::to_string(std::imag(*alpha)))
+                            : "");
+            log_bench(handle,
+                        "./rocblas-bench -f scal_batched --a_type",
+                        rocblas_precision_string<T>,
+                        "--b_type",
+                        rocblas_precision_string<U>,
+                        "-n",
+                        n,
+                        "--incx",
+                        incx,
+                        alphass.str(),
+                        "--batch",
+                        batch_count);
+        }
+    }
+    else
+    {
+        if(layer_mode & rocblas_layer_mode_log_trace)
+            log_trace(handle, rocblas_scal_name<T, U>, n, alpha, x, incx, batch_count);
+    }
+    if(layer_mode & rocblas_layer_mode_log_profile)
+        log_profile(
+            handle, rocblas_scal_name<T, U>, "N", n, "incx", incx, "batch", batch_count);
+
+    if(!x)
+        return rocblas_status_invalid_pointer;
+    if(batch_count < 0)
+        return rocblas_status_invalid_size;
+
+    RETURN_ZERO_DEVICE_MEMORY_SIZE_IF_QUERIED(handle);
+
+    return rocblas_scal_batched_template(handle, n, alpha, x, offsetx, incx, batch_count);
+}
