f2f44a4a53ecff559750776f4291cdbae4e2da4e
Yvan Mokwinski <Yvan.Mokwinski@amd.com>
2019-09-25 10:27:38 -0700

    undo modification

--- clients/include/testing_scal.hpp
+++ clients/include/testing_scal.hpp
@@ -2,7 +2,6 @@
  * Copyright 2018-2019 Advanced Micro Devices, Inc.
  * ************************************************************************ */
 
-
 #include "cblas_interface.hpp"
 #include "flops.hpp"
 #include "norm.hpp"
