d7343b42eb2d7874a7422e1b336e8b5b5157a15b
Torre Zuk <Torre.Zuk@amd.com>
2019-09-25 14:40:51 -0600

    keep dependency build the same

--- install.sh
+++ install.sh
@@ -365,7 +365,7 @@ if [[ "${install_dependencies}" == true ]]; then
   pushd .
     printf "\033[32mBuilding \033[33mgoogletest & lapack\033[32m from source; installing into \033[33m/usr/local\033[0m\n"
     mkdir -p ${build_dir}/deps && cd ${build_dir}/deps
-    ${cmake_executable} -lpthread -DBUILD_WITH_TENSILE=OFF -DBUILD_CLIENTS_BENCHMARKS=OFF -DBUILD_BOOST=OFF ../../deps
+    ${cmake_executable} -lpthread -DBUILD_BOOST=OFF ../../deps
     make -j$(nproc)
     elevate_if_not_root make install
   popd
