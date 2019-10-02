2d3d66546addab7c8b2bb5691230c74fcf61f4ee
mahmoodw <44450175+mahmoodw@users.noreply.github.com>
2019-10-02 12:18:05 -0600

    Dot and dotc batch and strided_batch support (#713)
    Added batch support to dot and dotc

--- library/src/blas1/rocblas_copy_strided_batched.cpp
+++ library/src/blas1/rocblas_copy_strided_batched.cpp
@@ -87,7 +87,7 @@ namespace
                         "batch_count",
                         batch_count);
 
-        return rocblas_copy_template<NB, false, T>(
+        return rocblas_copy_template<NB, T>(
             handle, n, x, 0, incx, stridex, y, 0, incy, stridey, batch_count);
     }
 
