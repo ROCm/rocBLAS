972620b1d4926f614c09bd4be66a6229a8e188c3
amcamd <andrew.chapman@amd.com>
2019-10-09 09:34:27 -0500

    update Tensile package number
    [ROCm/rocblas commit: 72c3b969a042ad3ea7b0e6f54b5ed48a6b3d14bd]

--- bump_master_version.sh
+++ bump_master_version.sh
@@ -9,17 +9,21 @@
 OLD_ROCBLAS_VERSION="2.9.0"
 NEW_ROCBLAS_VERSION="2.10.0"
 
-OLD_TENSILE_VERSION="3ab0890743db4cca5244d0dab30a574fa34c89b8"
-NEW_TENSILE_VERSION="fe4f721886d07eef6251cea4225e027181022aa5"
+OLD_TENSILE_VERSION="Tensile 4.12.0"
+NEW_TENSILE_VERSION="Tensile 4.13.0"
+
+OLD_TENSILE_HASH="3ab0890743db4cca5244d0dab30a574fa34c89b8"
+NEW_TENSILE_HASH="fe4f721886d07eef6251cea4225e027181022aa5"
 
 OLD_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.6.0"
 NEW_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.7.1"
 
 sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
-sed -i "s/${OLD_TENSILE_VERSION}/${NEW_TENSILE_VERSION}/g" tensile_tag.txt
+sed -i "s/${OLD_TENSILE_VERSION}/${NEW_TENSILE_VERSION}/g" CMakeLists.txt
+sed -i "s/${OLD_TENSILE_HASH}/${NEW_TENSILE_HASH}/g" tensile_tag.txt
 
 #only update yaml files for a Tensile major version change
 #for FILE in library/src/blas3/Tensile/Logic/*/*yaml
 #do
 #  sed -i "s/${OLD_MINIMUM_REQUIRED_VERSION}/${NEW_MINIMUM_REQUIRED_VERSION}/" $FILE
-#done
+#a9379f4e42efb754c9a618047bfbf292d74dfd0fdone
