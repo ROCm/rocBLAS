a6343b0ca2884c22353ebf33ba460995052938ae
First Last <daine.mcniven@amd.com>
2019-10-15 14:38:43 -0600

    fix mistake in last commit

--- clients/include/testing_trsm_strided_batched.hpp
+++ clients/include/testing_trsm_strided_batched.hpp
@@ -41,8 +41,8 @@ void testing_trsm_strided_batched(const Arguments& arg)
     rocblas_diagonal  diag   = char2rocblas_diagonal(char_diag);
 
     rocblas_int K      = side == rocblas_side_left ? M : N;
-    size_t      size_A = lda * size_t(K) + stride_a * batch_count - 1;
-    size_t      size_B = ldb * size_t(N) + stride_b * batch_count - 1;
+    size_t      size_A = lda * size_t(K) + stride_a * (batch_count - 1);
+    size_t      size_B = ldb * size_t(N) + stride_b * (batch_count - 1);
 
     rocblas_local_handle handle;
 
