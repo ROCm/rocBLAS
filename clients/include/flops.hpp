192095ce939cba75848e1b2a00b43efba83653c8
First Last <daine.mcniven@amd.com>
2019-10-31 15:16:34 -0600

    Adding complex flops calcs.

--- clients/include/flops.hpp
+++ clients/include/flops.hpp
@@ -23,15 +23,67 @@ constexpr double axpy_gflop_count(rocblas_int n)
 {
     return (2.0 * n) / 1e9;
 }
-template <typename T>
+template <>
+constexpr double axpy_gflop_count<rocblas_float_complex>(rocblas_int n)
+{
+    return (8.0 * n) / 1e9; // 6 for complex-complex multiply, 2 for c-c add
+}
+template <>
+constexpr double axpy_gflop_count<rocblas_double_complex>(rocblas_int n)
+{
+    return (8.0 * n) / 1e9;
+}
+
+template <bool CONJ, typename T>
 constexpr double dot_gflop_count(rocblas_int n)
 {
     return (2.0 * n) / 1e9;
 }
-template <typename T>
+template <>
+constexpr double dot_gflop_count<false, rocblas_float_complex>(rocblas_int n)
+{
+    return (8.0 * n) / 1e9; // 6 for each c-c multiply, 2 for each c-c add
+}
+template <>
+constexpr double dot_gflop_count<false, rocblas_double_complex>(rocblas_int n)
+{
+    return (8.0 * n) / 1e9;
+}
+template <>
+constexpr double dot_gflop_count<true, rocblas_float_complex>(rocblas_int n)
+{
+    return (9.0 * n) / 1e9; // regular dot (8n) + 1n for complex conjugate
+}
+template <>
+constexpr double dot_gflop_count<true, rocblas_double_complex>(rocblas_int n)
+{
+    return (9.0 * n) / 1e9;
+}
+
+template <typename T, typename U>
 constexpr double scal_gflop_count(rocblas_int n)
 {
     return (1.0 * n) / 1e9;
+}
+template <>
+constexpr double scal_gflop_count<rocblas_float_complex, rocblas_float_complex>(rocblas_int n)
+{
+    return (6.0 * n) / 1e9; // 6 for c-c multiply
+}
+template <>
+constexpr double scal_gflop_count<rocblas_double_complex, rocblas_double_complex>(rocblas_int n)
+{
+    return (6.0 * n) / 1e9;
+}
+template <>
+constexpr double scal_gflop_count<rocblas_float_complex, float>(rocblas_int n)
+{
+    return (2.0 * n) / 1e9; // 2 for real-complex multiply
+}
+template <>
+constexpr double scal_gflop_count<rocblas_double_complex, double>(rocblas_int n)
+{
+    return (2.0 * n) / 1e9;
 }
 
 /*
