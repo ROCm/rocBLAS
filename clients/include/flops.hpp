6129e4821f4f0f164bc2349c111dfaf366167228
First Last <daine.mcniven@amd.com>
2019-11-01 12:27:21 -0600

    Updated BLAS2/3 flop calcs from netlib.

--- clients/include/flops.hpp
+++ clients/include/flops.hpp
@@ -10,7 +10,8 @@
 
 /*!\file
  * \brief provides Floating point counts of Basic Linear Algebra Subprograms (BLAS) of Level 1, 2,
- * 3.
+ * 3. Where possible we are using the values of NOP from the legacy BLAS files [sdcz]blas[23]time.f
+ * for flop count.
  */
 
 /*
@@ -96,46 +97,47 @@ constexpr double scal_gflop_count<rocblas_double_complex, double>(rocblas_int n)
 template <typename T>
 constexpr double gemv_gflop_count(rocblas_int m, rocblas_int n)
 {
-    return (2.0 * m * n) / 1e9;
+    return (2.0 * m * n + 2.0 * m * n) / 1e9;
 }
 template <>
 constexpr double gemv_gflop_count<rocblas_float_complex>(rocblas_int m, rocblas_int n)
 {
-    return (8.0 * m * n) / 1e9;
+    return (8.0 * m * n + 6.0 * m * n) / 1e9;
 }
 
 template <>
 constexpr double gemv_gflop_count<rocblas_double_complex>(rocblas_int m, rocblas_int n)
 {
-    return (8.0 * m * n) / 1e9;
+    return (8.0 * m * n + 6.0 * m * n) / 1e9;
 }
 
 /* \brief floating point counts of TRSV */
 template <typename T>
 constexpr double trsv_gflop_count(rocblas_int m)
 {
-    return (m * (m + 1.0)) / 1e9;
+    return (m * m) / 1e9;
 }
 
 /* \brief floating point counts of SY(HE)MV */
 template <typename T>
 constexpr double symv_gflop_count(rocblas_int n)
 {
-    return (2.0 * n * n) / 1e9;
+    return (2.0 * n * n + 2.0 * n) / 1e9;
 }
 
 /* \brief floating point counts of GER */
 template <typename T>
 constexpr double ger_gflop_count(rocblas_int m, rocblas_int n)
 {
-    return (2.0 * m * n) / 1e9;
+    rocblas_int min = (m < n) ? m : n;
+    return (2.0 * m * n + min) / 1e9;
 }
 
 /* \brief floating point counts of SYR */
 template <typename T>
 constexpr double syr_gflop_count(rocblas_int n)
 {
-    return ((2.0 * n * (n + 1)) / 2) / 1e9;
+    return (n * (n + 1) + n) / 1e9;
 }
 
 /*
@@ -176,21 +178,21 @@ constexpr double geam_gflop_count(rocblas_int m, rocblas_int n)
 template <typename T>
 constexpr double trsm_gflop_count(rocblas_int m, rocblas_int n, rocblas_int k)
 {
-    return (1.0 * m * n * (k + 1)) / 1e9;
+    return (m * n * k) / 1e9;
 }
 
 template <>
 constexpr double
     trsm_gflop_count<rocblas_float_complex>(rocblas_int m, rocblas_int n, rocblas_int k)
 {
-    return (4.0 * m * n * (k + 1)) / 1e9;
+    return (4.0 * m * n * k) / 1e9;
 }
 
 template <>
 constexpr double
     trsm_gflop_count<rocblas_double_complex>(rocblas_int m, rocblas_int n, rocblas_int k)
 {
-    return (4.0 * m * n * (k + 1)) / 1e9;
+    return (4.0 * m * n * k) / 1e9;
 }
 
 /* \brief floating point counts of TRTRI */
