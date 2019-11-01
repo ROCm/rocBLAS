0b0ce2c2eac022d55eeba9b787c4d240dcb95560
Yvan Mokwinski <yvan.mokwinski@amd.com>
2019-11-01 10:22:52 -0600

    introduce rocblas_reduction_template and rocblas_reduction_impl (#767)
    and generalize its usage.
    
    [ROCm/rocblas commit: 3b1a1b26ce07a0f2d3a49e9a8a3370620726a2c9]

--- library/src/blas1/rocblas_nrm2.hpp
+++ library/src/blas1/rocblas_nrm2.hpp
@@ -3,10 +3,7 @@
  * ************************************************************************ */
 #pragma once
 
-#include "fetch_template.h"
-#include "handle.h"
-#include "reduction_strided_batched.h"
-#include "rocblas.h"
+#include "rocblas_reduction_template.hpp"
 
 template <class To>
 struct rocblas_fetch_nrm2
@@ -35,23 +32,16 @@ rocblas_status rocblas_nrm2_template(rocblas_handle handle,
                                      rocblas_int    shiftx,
                                      rocblas_int    incx,
                                      To*            workspace,
-                                     To*            result)
+                                     To*            results)
 {
-    // Quick return if possible.
-    if(n <= 0 || incx <= 0)
-    {
-        if(handle->is_device_memory_size_query())
-            return rocblas_status_size_unchanged;
-        else if(rocblas_pointer_mode_device == handle->pointer_mode)
-            RETURN_IF_HIP_ERROR(hipMemset(result, 0, sizeof(*result)));
-        else
-            *result = 0;
-        return rocblas_status_success;
-    }
+    static constexpr bool           isbatched     = false;
+    static constexpr rocblas_stride stridex_0     = 0;
+    static constexpr rocblas_int    batch_count_1 = 1;
 
-    return rocblas_reduction_strided_batched_kernel<NB,
-                                                    rocblas_fetch_nrm2<To>,
-                                                    rocblas_reduce_sum,
-                                                    rocblas_finalize_nrm2>(
-        handle, n, x, shiftx, incx, 0, 1, workspace, result);
+    return rocblas_reduction_template<NB,
+                                      isbatched,
+                                      rocblas_fetch_nrm2<To>,
+                                      rocblas_reduce_sum,
+                                      rocblas_finalize_nrm2>(
+        handle, n, x, shiftx, incx, stridex_0, batch_count_1, results, workspace);
 }
