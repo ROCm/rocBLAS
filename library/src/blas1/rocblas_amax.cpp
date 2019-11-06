f4176768ed84bdbbba7ff3fd7d778b0e87d7fa17
Yvan Mokwinski <yvan.mokwinski@amd.com>
2019-11-05 20:45:04 -0700

    batched and strided batched version of iamax iamin (#776)
    * batched and strided batched version of iamax iamain
    
    * because clang format reorders included files ...
    
    * adressing Torre's comments about the PR
    
    * Fixing 'namespace or static' redundancy pointed by Lee.
    
    * Back to Lee's more intuitive name.
    
    * Symmetry on data as pointed by Lee.
    
    * remove unnecessary brackets as pointed by Lee.
    
    * - Fixing behavior specified by Lee for unsual parameters in the tests and in the rocblas library templates.
    - Removed dependencies of Fortran indices in the testing reduction templates
    - Renamed rocblas_cblas with rocblas_iamax_iamin_ref.
    
    * fixing test failures on nrm2 and asum due to the assumed behavior on parameters.
    
    if batch_count is negative then it returns a negative size error if N and incx are positive.
    otherwise it returns success.
    
    * clang format ...

--- library/src/blas1/rocblas_amax.cpp
+++
@@ -1,6 +0,0 @@
-/* ************************************************************************
- * Copyright 2018-2019 Advanced Micro Devices, Inc.
- * ************************************************************************ */
-#define MAX_MIN max
-#define AMAX_AMIN_REDUCTION rocblas_reduce_amax
-#include "rocblas_amax_amin.h"
