29a30fd9ea0c11b3ac2d33c6fc0440765431965d
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2019-11-11 14:19:27 -0700

    Dot batched fixes for host memory  (#806)
    * host memory case fixes, large N req. double, add bad_arg, fixes to allow for very large batch_count
    
    
    [ROCm/rocblas commit: ea949554948cfb38ac8c94a5ae1c6beaea41393e]

--- library/src/blas1/rocblas_dot.hpp
+++ library/src/blas1/rocblas_dot.hpp
@@ -127,12 +127,14 @@ rocblas_status rocblas_dot_template(rocblas_handle __restrict__ handle,
                            handle->rocblas_stream,
                            blocks,
                            workspace,
-                           workspace);
+                           (V*)(workspace + size_t(batch_count) * blocks));
 
-        V res_V[batch_count];
+        // result is in the beginning of workspace[0]+offset
+        size_t offset = size_t(batch_count) * blocks;
+        V      res_V[batch_count];
         RETURN_IF_HIP_ERROR(
-            hipMemcpy(res_V, workspace, sizeof(V) * batch_count, hipMemcpyDeviceToHost));
-        for(int i = 0; i < batch_count; i++)
+            hipMemcpy(res_V, workspace + offset, sizeof(V) * batch_count, hipMemcpyDeviceToHost));
+        for(rocblas_int i = 0; i < batch_count; i++)
             results[i] = T(res_V[i]);
     }
 
