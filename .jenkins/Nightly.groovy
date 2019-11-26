2a3b767e7725fdb8167f45ce456a067692335f93
Eiden Yoshida <eiden.yoshida@amd.com>
2019-11-26 14:32:41 -0800

    Split up Jenkinsfile into common, nightly, and precheckin files

---
+++ .jenkins/Nightly.groovy
@@ -0,0 +1,64 @@
+#!/usr/bin/env groovy
+// This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
+@Library('rocJenkins') _
+
+// This is file for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+import com.amd.project.*
+import com.amd.docker.*
+
+////////////////////////////////////////////////////////////////////////
+// Mostly generated from snippet generator 'properties; set job properties'
+// Time-based triggers added to execute nightly tests, eg '30 2 * * *' means 2:30 AM
+properties([
+    pipelineTriggers([cron('0 1 * * *'), [$class: 'PeriodicFolderTrigger', interval: '5m']]),
+    buildDiscarder(logRotator(
+      artifactDaysToKeepStr: '',
+      artifactNumToKeepStr: '',
+      daysToKeepStr: '',
+      numToKeepStr: '10')),
+    disableConcurrentBuilds(),
+    [$class: 'CopyArtifactPermissionProperty', projectNames: '*']
+   ])
+
+import java.nio.file.Path;
+
+rocBLASCI:
+{
+
+    def rocblas = new rocProject('rocBLAS')
+    // customize for project
+    rocblas.paths.build_command = './install.sh -lasm_ci -c'
+
+    // Define test architectures, optional rocm version argument is available
+    def nodes = new dockerNodes(['ubuntu && gfx900', 'centos7 && gfx900', 'centos7 && gfx906', 'sles && gfx906'], rocblas)
+
+    boolean formatCheck = true
+
+    def compileCommand =
+    {
+        platform, project->
+
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/Common.groovy"
+        commonGroovy.runCompileCommand(platform, project)
+    }
+
+    def testCommand =
+    {
+        platform, project->
+
+        def gfilter = "*nightly*"
+        commonGroovy.runTestCommand(platform, project, gfilter)
+    }
+
+    def packageCommand =
+    {
+        platform, project->
+        
+        commonGroovy.runPackageCommand(platform, project)
+    }
+
+    buildProject(rocblas, formatCheck, nodes.dockerArray, compileCommand, testCommand, packageCommand)
+
+}
