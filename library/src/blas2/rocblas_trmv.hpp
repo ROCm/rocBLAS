735c166b63c674725bede996be4ec3bd0522d73a
Yvan Mokwinski <Yvan.Mokwinski@amd.com>
2019-12-06 00:02:00 -0800

    just a detail.

--- library/src/blas2/rocblas_trmv.hpp
+++ library/src/blas2/rocblas_trmv.hpp
@@ -22,6 +22,7 @@ rocblas_status rocblas_trmv_template(rocblas_handle    handle,
                                      rocblas_int       incx,
                                      W                 w)
 {
+    static constexpr rocblas_int    NB          = 512;
     static constexpr rocblas_int    batch_count = 1;
     static constexpr ptrdiff_t      offseta     = 0;
     static constexpr ptrdiff_t      offsetx     = 0;
@@ -29,20 +30,20 @@ rocblas_status rocblas_trmv_template(rocblas_handle    handle,
     static constexpr rocblas_stride stridea     = 0;
     static constexpr rocblas_stride stridew     = 0;
 
-    return trmv_template<512>(handle,
-                              uplo,
-                              transa,
-                              diag,
-                              m,
-                              a,
-                              offseta,
-                              lda,
-                              stridea,
-                              x,
-                              offsetx,
-                              incx,
-                              stridex,
-                              w,
-                              stridew,
-                              batch_count);
+    return trmv_template<NB>(handle,
+                             uplo,
+                             transa,
+                             diag,
+                             m,
+                             a,
+                             offseta,
+                             lda,
+                             stridea,
+                             x,
+                             offsetx,
+                             incx,
+                             stridex,
+                             w,
+                             stridew,
+                             batch_count);
 }
