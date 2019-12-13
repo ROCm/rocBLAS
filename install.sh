7b46a907b56ee08fc329b72deeaa58a52d38c9b9
Saad Rahim <saad.rahim@amd.com>
2019-12-13 15:55:01 -0800

    Revert "Disabling build testing"
    This reverts commit 8f3a306db701a6d94e0184352f510330709f4b46.

--- install.sh
+++ install.sh
@@ -361,7 +361,7 @@ pushd .
   compiler="hcc"
   if [[ "${build_cuda}" == true || "${build_hip_clang}" == true ]]; then
     compiler="hipcc"
-    cmake_common_options="${cmake_common_options} -DTensile_COMPILER=hipcc -DBUILD_TESTING=OFF "
+    cmake_common_options="${cmake_common_options} -DTensile_COMPILER=hipcc"
   fi
 
   # Uncomment for cmake debugging
