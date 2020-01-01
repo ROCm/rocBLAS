9a5a9eabe28b4e648bedf575a79d6b55f5086d28
Lee Killough <killough@leekillough.com>
2020-01-01 18:43:02 -0500

    simplify code
    [ROCm/rocblas commit: ac5d9479e497d203e9181bbfb179ddbf6ca473b6]

--- clients/gtest/rocblas_gtest_main.cpp
+++ clients/gtest/rocblas_gtest_main.cpp
@@ -7,6 +7,7 @@
 #include "test_cleanup.hpp"
 #include "utility.hpp"
 #include <atomic>
+#include <cerrno>
 #include <csetjmp>
 #include <csignal>
 #include <cstdlib>
@@ -117,59 +118,45 @@ public:
 /*********************************************
  * Signal-handling for detecting test faults *
  *********************************************/
-
 // Id of the thread which is catching signals
 static volatile pthread_t rocblas_test_sighandler_tid;
 
 // sigjmp_buf describing stack frame to go back to
 static sigjmp_buf rocblas_sigjmp_buf;
 
-// Whether rocblas_sigjmp_buf is set
+// Whether rocblas_sigjmp_buf is set and catching signals is enabled
 static volatile sig_atomic_t rocblas_sighandler_enabled = false;
-
-// Whether the signal handler has been reached recursively
-static std::atomic_flag is_recursive = ATOMIC_FLAG_INIT;
 
 // Signal handler
 extern "C" void rocblas_test_signal_handler(int sig)
 {
+    int e = errno; // Save errno
+
     // If the signal handler is disabled, restore this signal's disposition
     // to default, and reraise the signal
     if(!rocblas_sighandler_enabled)
     {
         signal(sig, SIG_DFL);
         raise(sig);
+        errno = e; // Restore errno
         return;
     }
 
     // If the thread receiving the signal is different from the thread
     // catching signals, send the signal to the thread catching signals.
-    // If recursion is detected, print a fatal error message and abort.
     if(!pthread_equal(pthread_self(), rocblas_test_sighandler_tid))
     {
-        if(!is_recursive.test_and_set())
-        {
-            pthread_kill(rocblas_test_sighandler_tid, sig);
-            sleep(1);
-            return;
-        }
-        else
-        {
-            static constexpr char msg[] = "A thread other than the main thread"
-                                          " has received a fatal signal, and cannot be recovered\n";
-            write(STDERR_FILENO, msg, sizeof(msg) - 1);
-            signal(SIGABRT, SIG_DFL);
-            abort();
-        }
+        pthread_kill(rocblas_test_sighandler_tid, sig);
+        sleep(1);
+        errno = e; // Restore errno
+        return;
     }
-
-    // Clear the recursive flag, since we are finally handling the signal
-    is_recursive.clear();
 
     // Jump back to the handler code
     // Note: This bypasses stack unwinding, and may lead to memory leaks, but
     // it is better than crashing. Throwing exceptions from signal handlers is
     // poorly supported, and may result in recursive calls to std::terminate.
+    errno = e; // Restore errno
     siglongjmp(rocblas_sigjmp_buf, sig);
 }
 
@@ -177,7 +164,7 @@ extern "C" void rocblas_test_signal_handler(int sig)
 static void rocblas_test_sigaction()
 {
     struct sigaction act;
-    sigemptyset(&act.sa_mask);
+    sigfillset(&act.sa_mask);
     act.sa_flags   = 0;
     act.sa_handler = rocblas_test_signal_handler;
 
@@ -194,11 +181,8 @@ void catch_signals_and_exceptions_as_failures(const std::function<void()>& test)
     // Save this thread's id, to detect signals in different threads
     rocblas_test_sighandler_tid = pthread_self();
 
-    // Clear the recursive flag
-    is_recursive.clear();
-
     // Set up the return point
-    int sig = sigsetjmp(rocblas_sigjmp_buf, false);
+    int sig = sigsetjmp(rocblas_sigjmp_buf, true);
 
     // If this is a return, indicate the signal received
     if(sig)
