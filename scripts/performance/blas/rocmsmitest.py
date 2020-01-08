7615431679cb7b0f95ed441e6fd30e409be2f940
Eiden Yoshida <eiden.yoshida@amd.com>
2020-01-08 15:43:10 -0700

    Fix rocm-smi calls

--- scripts/performance/blas/rocmsmitest.py
+++ scripts/performance/blas/rocmsmitest.py
@@ -2,7 +2,23 @@ import sys
 sys.path.append('/opt/rocm/bin')
 import rocm_smi as smi
 import argparse
+import io
+from contextlib import redirect_stdout
 
+def parseRocmsmi(func, arg, matchStr):
+    f = io.StringIO()
+    with redirect_stdout(f):
+        if arg is not None:
+            func(arg)
+        else:
+            func()
+    lines = f.getvalue().split('\n')
+    output = ''
+    for line in lines:
+        if matchStr in line:
+            output = line[line.find(matchStr)+len(matchStr):]
+    return output
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d')
@@ -10,28 +26,48 @@ if __name__ == '__main__':
     
     if args.d is not None:
         deviceNum = int(args.d)
-        deviceList = smi.listDevices(True)
+        print("Inputted Device Argument:", deviceNum)
         
-        print("Device List:", deviceList)
+        #get device name from number
+        device = smi.parseDeviceNumber(deviceNum)
         
-        deviceList = deviceList[deviceNum]
+        print("All devices:", smi.listDevices(True))
+        print("AMD devices:", smi.listDevices(False))
         
-        device = deviceList[deviceNum]
-        print("Device", deviceNum, ":", device)
-        
-        print("showAllConciseHw")
-        smi.showAllConciseHw(deviceList)
-        
-        print("showProductName")
-        smi.showProductName(deviceList)
-        
-        print("showClocks")
-        smi.showClocks(deviceList)
-        
-        print("getCurrentClock sclk")
-        sclk = smi.getCurrentClock(device, 'sclk', 'freq')
-        print(sclk)
-        
-        print("getCurrentClock mclk")
-        mclk = smi.getCurrentClock(device, 'mclk', 'freq')
-        print(mclk)
+        if smi.isAmdDevice(device):
+            deviceList = [device]
+            print("\nDevice chosen:", device)
+            
+            print("\nshowAllConciseHw")
+            smi.showAllConciseHw(deviceList)
+            
+            print("\nshowProductName")
+            smi.showProductName(deviceList)
+            
+            print("\nshowClocks")
+            smi.showClocks(deviceList)
+            
+            print("\ngetCurrentClock sclk")
+            sclk = smi.getCurrentClock(device, 'sclk', 'freq')
+            print("sclk:", sclk)
+            
+            print("\ngetCurrentClock mclk")
+            mclk = smi.getCurrentClock(device, 'mclk', 'freq')
+            print("mclk:", mclk)
+            
+            print("\nshowVbiosVersion:")
+            smi.showVbiosVersion(deviceList)
+            vbios = parseRocmsmi(smi.showVbiosVersion, deviceList, 'VBIOS version: ')
+            print("VBIOS:", vbios)
+            
+            print("\nshowId:", vbios)
+            smi.showId(deviceList)
+            deviceId = parseRocmsmi(smi.showId, deviceList, 'GPU ID: ')
+            print("device ID:", deviceId)
+            
+            print("\nshowPerformanceLevel")
+            smi.showPerformanceLevel(deviceList)
+            performanceLevel = parseRocmsmi(smi.showPerformanceLevel, deviceList, 'Performance Level: ')
+            print("Performance Level:", performanceLevel)
+        else:
+            print("Selected device is not an AMD GPU")
