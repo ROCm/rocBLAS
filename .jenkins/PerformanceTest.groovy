55b843b1bc9a28276c04361fdf8b42afbdd8b761
Eiden Yoshida <eiden.yoshida@amd.com>
2020-01-16 13:47:33 -0700

    Fix path to rocblas_common.yaml

--- .jenkins/PerformanceTest.groovy
+++ .jenkins/PerformanceTest.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 // This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
-@Library('rocJenkins@perftest') _
+@Library('rocJenkins') _
 
 // This is file for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
