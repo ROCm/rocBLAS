bdb5e0cf143fcb9910c757a33f30b66338e4e9b8
YvanMokwinski <yvan.mokwinski@gmail.com>
2020-01-15 17:56:08 -0700

    switch not implemented to invalid value

--- library/src/blas2/rocblas_trmv.cpp
+++ library/src/blas2/rocblas_trmv.cpp
@@ -95,7 +95,7 @@ namespace
 
         if(uplo != rocblas_fill_lower && uplo != rocblas_fill_upper)
         {
-            return rocblas_status_not_implemented;
+            return rocblas_status_invalid_value;
         }
 
         if(m < 0 || lda < m || lda < 1 || !incx)
