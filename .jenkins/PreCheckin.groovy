97c0fcf8e6b9596b18c59d281e3d8294775c25fb
Eiden Yoshida <eiden.yoshida@amd.com>
2020-01-31 18:09:25 -0700

    Adding Ubuntu 18 to CI in order to create more packages

--- .jenkins/PreCheckin.groovy
+++ .jenkins/PreCheckin.groovy
@@ -16,10 +16,12 @@ rocBLASCI:
 
     def rocblas = new rocProject('rocBLAS', 'PreCheckin')
     // customize for project
-    rocblas.paths.build_command = './install.sh -lasm_ci -c'
+    rocblas.paths.build_command = './install.sh -c'
+
+    rocblas.timeout.compile = 180
 
     // Define test architectures, optional rocm version argument is available
-    def nodes = new dockerNodes(['ubuntu && gfx900', 'centos7 && gfx906', 'sles && gfx906'], rocblas)
+    def nodes = new dockerNodes(['ubuntu && gfx900', 'ubuntu18 && gfx906', 'centos7 && gfx906', 'sles && gfx906'], rocblas)
 
     boolean formatCheck = true
 
