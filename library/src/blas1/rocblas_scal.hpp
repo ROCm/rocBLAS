bc4cbee25817e76a975451addc1d7ed0fca8569d
amcamd <andrew.chapman@amd.com>
2020-02-08 12:47:16 -0600

    scal changes

--- library/src/blas1/rocblas_scal.hpp
+++ library/src/blas1/rocblas_scal.hpp
@@ -73,3 +73,65 @@ rocblas_status rocblas_scal_template(rocblas_handle handle,
 
     return rocblas_status_success;
 }
+
+// alpha is entry in batched or strided_batched matrix
+template <typename T, typename U, typename V>
+__global__ void rocblas_scal_kernel(rocblas_int    n,
+                                    const U*       alphaa,
+                                    ptrdiff_t      offset_alpha,
+                                    rocblas_int    inc_alpha,
+                                    rocblas_stride stride_alpha,
+                                    V*             xa,
+                                    ptrdiff_t      offsetx,
+                                    rocblas_int    incx,
+                                    rocblas_stride stridex)
+{
+    T*        x     = load_ptr_batch(xa, hipBlockIdx_y, offsetx, stridex);
+    const T*  alpha = load_ptr_batch(alphaa, hipBlockIdx_y, offset_alpha, stride_alpha);
+    ptrdiff_t tid   = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+
+    // bound
+    if(tid < n)
+        x[tid * incx] *= *alpha;
+}
+
+template <rocblas_int NB, typename T, typename U, typename V>
+rocblas_status rocblas_scal_template(rocblas_handle handle,
+                                     rocblas_int    n,
+                                     const U*       alpha,
+                                     rocblas_int    offset_alpha,
+                                     rocblas_int    inc_alpha,
+                                     rocblas_stride stride_alpha,
+                                     V*             x,
+                                     rocblas_int    offsetx,
+                                     rocblas_int    incx,
+                                     rocblas_stride stridex,
+                                     rocblas_int    batch_count)
+{
+    // Quick return if possible. Not Argument error
+    if(n <= 0 || incx <= 0 || batch_count <= 0)
+    {
+        return rocblas_status_success;
+    }
+
+    dim3        blocks((n - 1) / NB + 1, batch_count);
+    dim3        threads(NB);
+    hipStream_t rocblas_stream = handle->rocblas_stream;
+
+    hipLaunchKernelGGL(rocblas_scal_kernel<T>,
+                       blocks,
+                       threads,
+                       0,
+                       rocblas_stream,
+                       n,
+                       alpha,
+                       offset_alpha,
+                       inc_alpha,
+                       stride_alpha,
+                       x,
+                       offsetx,
+                       incx,
+                       stridex);
+
+    return rocblas_status_success;
+}
