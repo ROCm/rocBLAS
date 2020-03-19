a5bc640d34edd69e4e535f5a09686a7cb7d4a6eb
Lee Killough <killough@leekillough.com>
2020-03-19 14:28:30 -0400

    improve documentation
    [ROCm/rocblas commit: 74861899db0fe15c075d7d7438eaf24e2a6f7f20]

--- clients/include/argument_model.hpp
+++ clients/include/argument_model.hpp
@@ -78,7 +78,31 @@ public:
             delim = ",";
         };
 
-        // Apply the arguments, calling print on each one
+        // Args is a parameter pack of type:   rocblas_argument...
+        // The rocblas_argument enum values in Args correspond to the function arguments that
+        // will be printed by rocblas_test or rocblas_bench. For example, the function:
+        //
+        //  rocblas_ddot(rocblas_handle handle,
+        //                                 rocblas_int    n,
+        //                                 const double*  x,
+        //                                 rocblas_int    incx,
+        //                                 const double*  y,
+        //                                 rocblas_int    incy,
+        //                                 double*        result);
+        // will have <Args> = <e_N, e_incx, e_incy>
+        //
+        // print is a lambda defined above this comment block
+        //
+        // arg is an instance of the Arguments struct
+        //
+        // apply is a templated lambda for C++17 and a templated fuctor for C++14
+        //
+        // For rocblas_ddot, the following template specialization of apply will be called:
+        // apply<e_N>(print, arg, T{}), apply<e_incx>(print, arg, T{}),, apply<e_incy>(print, arg, T{})
+        //
+        // apply in turn calls print with a string corresponding to the enum, for example "N" and the value of N
+        //
+
 #if __cplusplus >= 201703L
         // C++17
         (ArgumentsHelper::apply<Args>(print, arg, T{}), ...);
