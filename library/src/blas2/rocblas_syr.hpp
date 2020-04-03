28ffad019e1d2b8a213916b61763a9449c773d35
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2020-04-03 08:24:44 -0600

    Template api (#1056)
    * export complex and _template functions for rocSOLVER

--- library/src/blas2/rocblas_syr.hpp
+++ library/src/blas2/rocblas_syr.hpp
@@ -61,20 +61,20 @@ inline rocblas_status rocblas_syr_arg_check(rocblas_fill   uplo,
 }
 
 template <typename T, typename U, typename V, typename W>
-rocblas_status rocblas_syr_template(rocblas_handle handle,
-                                    rocblas_fill   uplo,
-                                    rocblas_int    n,
-                                    U              alpha,
-                                    rocblas_stride stride_alpha,
-                                    V              x,
-                                    rocblas_int    offsetx,
-                                    rocblas_int    incx,
-                                    rocblas_stride stridex,
-                                    W              A,
-                                    rocblas_int    offseta,
-                                    rocblas_int    lda,
-                                    rocblas_stride strideA,
-                                    rocblas_int    batch_count)
+ROCBLAS_EXPORT_NOINLINE rocblas_status rocblas_syr_template(rocblas_handle handle,
+                                                            rocblas_fill   uplo,
+                                                            rocblas_int    n,
+                                                            U              alpha,
+                                                            rocblas_stride stride_alpha,
+                                                            V              x,
+                                                            rocblas_int    offsetx,
+                                                            rocblas_int    incx,
+                                                            rocblas_stride stridex,
+                                                            W              A,
+                                                            rocblas_int    offseta,
+                                                            rocblas_int    lda,
+                                                            rocblas_stride strideA,
+                                                            rocblas_int    batch_count)
 {
     // Quick return
     if(!n || batch_count == 0)
