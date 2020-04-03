3ac642eccc8d0460dd66cea83d739d69bbe3fe48
Torre Zuk <Torre.Zuk@amd.com>
2020-04-03 08:57:37 -0600

    shared xy and workitem count

--- library/src/blas2/rocblas_ger.hpp
+++ library/src/blas2/rocblas_ger.hpp
@@ -8,6 +8,7 @@
 
 template <rocblas_int DIM_X,
           rocblas_int DIM_Y,
+          rocblas_int WIN,
           bool        CONJ,
           typename T,
           typename U,
@@ -31,7 +32,7 @@ __global__ void ger_kernel(rocblas_int    m,
                            rocblas_int strideA)
 {
     __shared__ T xdata[DIM_X];
-    __shared__ T ydata[DIM_Y];
+    __shared__ T ydata[DIM_Y * WIN];
 
     const T* __restrict__ x = load_ptr_batch(xa, hipBlockIdx_z, shiftx, stridex);
     const T* __restrict__ y = load_ptr_batch(ya, hipBlockIdx_z, shifty, stridey);
@@ -40,23 +41,33 @@ __global__ void ger_kernel(rocblas_int    m,
 
     ptrdiff_t tx = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
     ptrdiff_t ty = hipBlockIdx_y * hipBlockDim_y + hipThreadIdx_y;
+    ty *= WIN;
 
-    if(hipThreadIdx_y == 0 && tx < m)
+    // shared data base index
+    int tyi = hipThreadIdx_y * WIN;
+
+    if(hipThreadIdx_y == 0)
     {
-        xdata[hipThreadIdx_x] = x[tx * incx];
+        xdata[hipThreadIdx_x] = tx < m ? x[tx * incx] : 0;
     }
 
-    if(hipThreadIdx_x == 0 && ty < n)
+    if(hipThreadIdx_x < WIN)
     {
-        ydata[hipThreadIdx_y] = y[ty * incy];
+        ydata[tyi + hipThreadIdx_x] = ty < n ? y[(ty + hipThreadIdx_x) * incy] : 0;
     }
 
     __syncthreads();
 
-    if(tx < m && ty < n)
+    if(tx < m)
     {
-        A[tx + lda * ty] += alpha * xdata[hipThreadIdx_x]
-                            * (CONJ ? conj(ydata[hipThreadIdx_y]) : ydata[hipThreadIdx_y]);
+        T x_value = alpha * xdata[hipThreadIdx_x];
+
+        for(int i = 0; i < WIN; i++)
+        {
+            int yi = ty + i;
+            if(yi < n)
+                A[tx + lda * yi] += x_value * (CONJ ? conj(ydata[tyi + i]) : ydata[tyi + i]);
+        }
     }
 }
 
@@ -121,16 +132,17 @@ ROCBLAS_EXPORT_NOINLINE rocblas_status rocblas_ger_template(rocblas_handle handl
     auto shiftx = incx < 0 ? offsetx - ptrdiff_t(incx) * (m - 1) : offsetx;
     auto shifty = incy < 0 ? offsety - ptrdiff_t(incy) * (n - 1) : offsety;
 
-    static constexpr int DIM_X   = 64;
-    static constexpr int DIM_Y   = 16;
+    static constexpr int DIM_X   = 32;
+    static constexpr int DIM_Y   = 32;
+    static constexpr int WIN     = 8; // work item number of elements to process
     rocblas_int          blocksX = (m - 1) / DIM_X + 1;
-    rocblas_int          blocksY = (n - 1) / DIM_Y + 1;
+    rocblas_int          blocksY = (n - 1) / (DIM_Y * WIN) + 1; // WIN columns/work item
 
     dim3 grid(blocksX, blocksY, batch_count);
     dim3 threads(DIM_X, DIM_Y);
 
     if(handle->pointer_mode == rocblas_pointer_mode_device)
-        hipLaunchKernelGGL((ger_kernel<DIM_X, DIM_Y, CONJ, T>),
+        hipLaunchKernelGGL((ger_kernel<DIM_X, DIM_Y, WIN, CONJ, T>),
                            grid,
                            threads,
                            0,
@@ -152,7 +164,7 @@ ROCBLAS_EXPORT_NOINLINE rocblas_status rocblas_ger_template(rocblas_handle handl
                            lda,
                            strideA);
     else
-        hipLaunchKernelGGL((ger_kernel<DIM_X, DIM_Y, CONJ, T>),
+        hipLaunchKernelGGL((ger_kernel<DIM_X, DIM_Y, WIN, CONJ, T>),
                            grid,
                            threads,
                            0,
