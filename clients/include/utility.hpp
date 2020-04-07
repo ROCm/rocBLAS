5d6d478b39b1240604b5cb04cc427c0e4cec417b
Lee Killough <15950023+leekillough@users.noreply.github.com>
2020-04-07 08:31:38 -0700

    Cleanup macros and #include file ordering (#1111)
    * Cleanup macros and #include file ordering
    
    * Temporarily leave out sleep abort from poisoned list until PR1100 is merged
    
    [ROCm/rocblas commit: ee52404907bbd6084764bd754e003b2555ff3cfb]

--- clients/include/utility.hpp
+++ clients/include/utility.hpp
@@ -8,7 +8,6 @@
 #include "cblas_interface.hpp"
 #include "logging.h"
 #include "rocblas.h"
-#include "rocblas_test.hpp"
 #include "rocblas_vector.hpp"
 #include "utility.h"
 #include <cstdio>
@@ -37,6 +36,10 @@
 #pragma GCC poison cout cerr clog stdout stderr gets puts putchar fputs fprintf printf sprintf    \
     vfprintf vprintf vsprintf perror strerror strtok gmtime ctime asctime localtime tmpnam putenv \
         clearenv fcloseall ecvt fcvt
+#else
+// Suppress warnings about hipMalloc(), hipFree() except in rocblas-test and rocblas-bench
+#undef hipMalloc
+#undef hipFree
 #endif
 
 static constexpr char LIMITED_MEMORY_STRING[]
