8c47e03dd4ff8698b6371624d13978f57888c7b9
Akila Premachandra <akila.premachandra@amd.com>
2020-04-15 16:37:04 -0600

    Set parallel jobs to 2 if using hip-clang in CI

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -8,10 +8,12 @@ def runCompileCommand(platform, project, jobName)
     String compiler = jobName.contains('hipclang') ? 'hipcc' : 'hcc'
     String hipClang = jobName.contains('hipclang') ? '--hip-clang' : ''
     String sles = platform.jenkinsLabel.contains('sles') ? '/usr/bin/sudo --preserve-env' : ''
+    String parallelJobs = jobName.contains('hipclang') ? "export HIPCC_COMPILE_FLAGS_APPEND=-parallel-jobs=2" : ":"
 
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
+                ${parallelJobs}
                 ${sles} LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=/opt/rocm/bin/${compiler} ${project.paths.build_command} ${hipClang}
                 """
     platform.runCommand(this, command)
