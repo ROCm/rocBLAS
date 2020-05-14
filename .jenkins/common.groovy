9f6d028fa61d8d0be0625a3fd07c34078bdd0ed4
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2020-05-13 21:42:21 -0600

    Only enable devtoolset7 on Centos7 CI (#69)
    [ROCm/rocblas commit: a835b2d5e8e3fd76d2ee6bf0b3e424b6f55cac06]

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -8,12 +8,12 @@ def runCompileCommand(platform, project, jobName)
     String compiler = jobName.contains('hipclang') ? 'hipcc' : 'hcc'
     String hipClang = jobName.contains('hipclang') ? '--hip-clang' : '--no-hip-clang'
     String sles = platform.jenkinsLabel.contains('sles') ? '/usr/bin/sudo --preserve-env' : ''
-    String centos = platform.jenkinsLabel.contains('centos') ? 'source scl_source enable devtoolset-7' : ''
+    String centos7 = platform.jenkinsLabel.contains('centos7') ? 'source scl_source enable devtoolset-7' : ''
 
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
-                ${centos}
+                ${centos7}
                 ${sles} CXX=/opt/rocm/bin/${compiler} ${project.paths.build_command} ${hipClang}
                 """
     platform.runCommand(this, command)
