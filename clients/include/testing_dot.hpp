5f03f3513f8e9fcf364156a2cdcf76dce3efee33
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2020-05-29 10:40:47 -0600

    dot bench pointer_mode_device (#102)

--- clients/include/testing_dot.hpp
+++ clients/include/testing_dot.hpp
@@ -156,18 +156,18 @@ void testing_dot(const Arguments& arg)
     {
         int number_cold_calls = arg.cold_iters;
         int number_hot_calls  = arg.iters;
-        CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_host));
+        CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_device));
 
         for(int iter = 0; iter < number_cold_calls; iter++)
         {
-            (rocblas_dot_fn)(handle, N, dx, incx, dy, incy, &rocblas_result_1);
+            (rocblas_dot_fn)(handle, N, dx, incx, dy, incy, d_rocblas_result_2);
         }
 
         gpu_time_used = get_time_us(); // in microseconds
 
         for(int iter = 0; iter < number_hot_calls; iter++)
         {
-            (rocblas_dot_fn)(handle, N, dx, incx, dy, incy, &rocblas_result_1);
+            (rocblas_dot_fn)(handle, N, dx, incx, dy, incy, d_rocblas_result_2);
         }
 
         gpu_time_used = get_time_us() - gpu_time_used;
