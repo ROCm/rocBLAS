d933e0a12a0c771e4abb36e873f4ea80fec75ce4
Lee Killough <killough@leekillough.com>
2020-06-29 21:43:14 -0400

    Log categories of alpha, beta for ROCBLAS_LAYER=4

--- library/src/blas_ex/rocblas_gemm_batched_ex.cpp
+++ library/src/blas_ex/rocblas_gemm_batched_ex.cpp
@@ -40,6 +40,13 @@ try
 
     RETURN_ZERO_DEVICE_MEMORY_SIZE_IF_QUERIED(handle);
 
+    // Copy alpha and beta to host if on device
+    rocblas_union_t alpha_h, beta_h;
+    RETURN_IF_ROCBLAS_ERROR(
+        copy_alpha_beta_to_host_if_device(handle, alpha, beta, alpha_h, beta_h, k, compute_type));
+    auto saved_pointer_mode = handle->push_pointer_mode(rocblas_pointer_mode_host);
+
+    // Perform logging
     auto layer_mode = handle->layer_mode;
     if(layer_mode
        & (rocblas_layer_mode_log_trace | rocblas_layer_mode_log_bench
@@ -57,126 +64,88 @@ try
         auto d_type_string       = rocblas_datatype_string(d_type);
         auto compute_type_string = rocblas_datatype_string(compute_type);
 
-        if(layer_mode & (rocblas_layer_mode_log_trace | rocblas_layer_mode_log_bench))
+        if(layer_mode & rocblas_layer_mode_log_trace)
         {
-            if(handle->pointer_mode == rocblas_pointer_mode_host)
+            rocblas_ostream alphass, betass;
+
+            if(log_trace_alpha_beta_ex(compute_type, alpha, beta, alphass, betass)
+               == rocblas_status_success)
             {
-                if(layer_mode & rocblas_layer_mode_log_trace)
-                {
-                    rocblas_ostream alphass, betass;
-
-                    if(log_trace_alpha_beta_ex(compute_type, alpha, beta, alphass, betass)
-                       == rocblas_status_success)
-                    {
-                        log_trace(handle,
-                                  "rocblas_gemm_batched_ex",
-                                  trans_a,
-                                  trans_b,
-                                  m,
-                                  n,
-                                  k,
-                                  alphass.str(),
-                                  a,
-                                  a_type_string,
-                                  lda,
-                                  b,
-                                  b_type_string,
-                                  ldb,
-                                  betass.str(),
-                                  c,
-                                  c_type_string,
-                                  ldc,
-                                  d,
-                                  d_type_string,
-                                  ldd,
-                                  batch_count,
-                                  compute_type_string,
-                                  algo,
-                                  solution_index,
-                                  flags);
-                    }
-                }
-
-                if(layer_mode & rocblas_layer_mode_log_bench)
-                {
-                    std::string alphas, betas;
-
-                    if(log_bench_alpha_beta_ex(compute_type, alpha, beta, alphas, betas)
-                       == rocblas_status_success)
-                    {
-                        log_bench(handle,
-                                  "./rocblas-bench -f gemm_batched_ex",
-                                  "--transposeA",
-                                  trans_a_letter,
-                                  "--transposeB",
-                                  trans_b_letter,
-                                  "-m",
-                                  m,
-                                  "-n",
-                                  n,
-                                  "-k",
-                                  k,
-                                  alphas,
-                                  "--a_type",
-                                  a_type_string,
-                                  "--lda",
-                                  lda,
-                                  "--b_type",
-                                  b_type_string,
-                                  "--ldb",
-                                  ldb,
-                                  betas,
-                                  "--c_type",
-                                  c_type_string,
-                                  "--ldc",
-                                  "--d_type",
-                                  d_type_string,
-                                  "--ldd",
-                                  ldd,
-                                  "--batch_count",
-                                  batch_count,
-                                  "--compute_type",
-                                  compute_type_string,
-                                  "--algo",
-                                  algo,
-                                  "--solution_index",
-                                  solution_index,
-                                  "--flags",
-                                  flags);
-                    }
-                }
+                log_trace(handle,
+                          "rocblas_gemm_batched_ex",
+                          trans_a,
+                          trans_b,
+                          m,
+                          n,
+                          k,
+                          alphass.str(),
+                          a,
+                          a_type_string,
+                          lda,
+                          b,
+                          b_type_string,
+                          ldb,
+                          betass.str(),
+                          c,
+                          c_type_string,
+                          ldc,
+                          d,
+                          d_type_string,
+                          ldd,
+                          batch_count,
+                          compute_type_string,
+                          algo,
+                          solution_index,
+                          flags);
             }
-            else
+        }
+
+        if(layer_mode & rocblas_layer_mode_log_bench)
+        {
+            std::string alphas, betas;
+
+            if(log_bench_alpha_beta_ex(compute_type, alpha, beta, alphas, betas)
+               == rocblas_status_success)
             {
-                if(layer_mode & rocblas_layer_mode_log_trace)
-                {
-                    log_trace(handle,
-                              "rocblas_gemm_batched_ex",
-                              trans_a,
-                              trans_b,
-                              m,
-                              n,
-                              k,
-                              alpha,
-                              a,
-                              a_type,
-                              lda,
-                              b,
-                              b_type,
-                              ldb,
-                              beta,
-                              c,
-                              c_type,
-                              ldc,
-                              d,
-                              d_type,
-                              ldd,
-                              batch_count,
-                              compute_type,
-                              algo,
-                              solution_index,
-                              flags);
-                }
+                log_bench(handle,
+                          "./rocblas-bench -f gemm_batched_ex",
+                          "--transposeA",
+                          trans_a_letter,
+                          "--transposeB",
+                          trans_b_letter,
+                          "-m",
+                          m,
+                          "-n",
+                          n,
+                          "-k",
+                          k,
+                          alphas,
+                          "--a_type",
+                          a_type_string,
+                          "--lda",
+                          lda,
+                          "--b_type",
+                          b_type_string,
+                          "--ldb",
+                          ldb,
+                          betas,
+                          "--c_type",
+                          c_type_string,
+                          "--ldc",
+                          "--d_type",
+                          d_type_string,
+                          "--ldd",
+                          ldd,
+                          "--batch_count",
+                          batch_count,
+                          "--compute_type",
+                          compute_type_string,
+                          "--algo",
+                          algo,
+                          "--solution_index",
+                          solution_index,
+                          "--flags",
+                          flags);
             }
         }
 
@@ -204,10 +173,14 @@ try
                         n,
                         "K",
                         k,
+                        "alpha",
+                        alpha_beta_value_category(alpha, compute_type),
                         "lda",
                         lda,
                         "ldb",
                         ldb,
+                        "beta",
+                        alpha_beta_value_category(beta, compute_type),
                         "ldc",
                         ldc,
                         "ldd",
