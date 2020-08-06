b4538c1bcb758d94faa2ce1a49ebb152c5eb6685
Lee Killough <killough@leekillough.com>
2020-08-06 18:43:04 -0400

    Simplify futures and promises implementation
    [ROCm/rocblas commit: beda2ad7fcfe7587a331f3b19075fabc65edc694]

--- library/src/rocblas_ostream.cpp
+++ library/src/rocblas_ostream.cpp
@@ -306,8 +306,8 @@ void rocblas_ostream::worker::thread_function()
         // An empty message indicates the closing of the stream
         if(!task.size())
         {
-            // Tell future to wake up after thread exits
-            task.set_value_at_thread_exit();
+            // Tell future to wake up
+            task.set_value();
             break;
         }
 
@@ -319,8 +319,8 @@ void rocblas_ostream::worker::thread_function()
         {
             perror("Error writing log file");
 
-            // Tell future to wake up after thread exits
-            task.set_value_at_thread_exit();
+            // Tell future to wake up
+            task.set_value();
             break;
         }
 
