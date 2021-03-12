d2ec3ef66b9adfe2382737867afb41e68edaaac8
NaveenElumalaiAMD <69491510+NaveenElumalaiAMD@users.noreply.github.com>
2021-03-12 14:01:06 -0700

    Lwpmlse 118 numerical checking axpy_ex (#582)

--- library/src/blas_ex/rocblas_axpy_strided_batched_ex.cpp
+++ library/src/blas_ex/rocblas_axpy_strided_batched_ex.cpp
@@ -146,7 +146,8 @@ namespace
 
         static constexpr rocblas_stride stride_0 = 0;
         static constexpr ptrdiff_t      offset_0 = 0;
-        return rocblas_axpy_ex_template<NB, false>(handle,
+        return rocblas_axpy_ex_template<NB, false>(name,
+                                                   handle,
                                                    n,
                                                    alpha,
                                                    alpha_type,
