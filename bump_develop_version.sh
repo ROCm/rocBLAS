a0833274572a1503c4dbd17befb57af14e92860d
amcamd <andrew.chapman@amd.com>
2021-04-23 11:04:59 -0400

    bump develop version after ROCm 4.3 feature complete

--- bump_develop_version.sh
+++ bump_develop_version.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# This script needs to be edited to bump new master version to new develop for new release.
-# - run this script after running bump_master_version.sh and merging develop into master
-# - run this script in master branch
-# - after running this script merge master into develop
+# This script needs to be edited to bump develop version after feature complete
+# - run this script in develop branch
 
-OLD_TENSILE_HASH="9cbabb07f81e932b9c98bf5ae48fbd7fcef615cf"
-NEW_TENSILE_HASH="057d389dd902eae437576b505204a22daeb4ae1d"
+OLD_ROCBLAS_VERSION="2.39.0"
+NEW_ROCBLAS_VERSION="2.40.0"
 
-sed -i "s/${OLD_TENSILE_HASH}/${NEW_TENSILE_HASH}/g" tensile_tag.txt
-
+sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
