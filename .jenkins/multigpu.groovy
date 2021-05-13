0a8bfececda10a32b393c5248093a699398539d1
Cory Bloor <Cordell.Bloor@amd.com>
2021-05-13 09:34:07 -0600

    Enable ccache on CI (#660)
    This change makes use of ccache the default setting for rocBLAS builds
    on the CI. It can be disabled for PRs using the label 'ci:no-ccache'.
    
    This also removes use of sudo from the SLES build commands, as it is
    no longer necessary.
    
    [ROCm/rocblas commit: 9dc1a29acb6208eaef9cadad6eb6d63a1bc15223]

--- .jenkins/multigpu.groovy
+++ .jenkins/multigpu.groovy
@@ -17,6 +17,7 @@ def runCI =
     // customize for project
     prj.paths.build_command = './install.sh -c'
 
+    prj.defaults.ccache = true
     prj.timeout.compile = 480
 
     // Define test architectures, optional rocm version argument is available
