070f8f5a6176bba8b9c34ab98b6562e1fb4fd581
amcamd <andrew.chapman@amd.com>
2021-06-02 14:37:43 -0400

    syrkx: kernels for diagonal blocks
    [ROCm/rocblas commit: 386b9a3102df9f2146a0ff21706e12b87a85159e]

--- library/src/blas3/rocblas_syrkx_strided_batched.cpp
+++ library/src/blas3/rocblas_syrkx_strided_batched.cpp
@@ -157,30 +157,8 @@ namespace
         if(arg_status != rocblas_status_continue)
             return arg_status;
 
-        static constexpr bool is2K    = false; // syrkx
         static constexpr bool BATCHED = false;
-#if 0
-        return rocblas_syr2k_template<is2K>(handle,
-                                            uplo,
-                                            transA,
-                                            n,
-                                            k,
-                                            alpha,
-                                            A,
-                                            offset_A,
-                                            lda,
-                                            stride_a,
-                                            B,
-                                            offset_B,
-                                            ldb,
-                                            stride_b,
-                                            beta,
-                                            C,
-                                            offset_C,
-                                            ldc,
-                                            stride_c,
-                                            batch_count);
-#endif
+
         return rocblas_internal_syrkx_template<MIN_NB, BATCHED, T>(handle,
                                                                    uplo,
                                                                    transA,
