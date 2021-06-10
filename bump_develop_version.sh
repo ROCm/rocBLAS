a7d093861bf1e54420b53ba1ef1a10d7875a2ed3
amcamd <andrew.chapman@amd.com>
2021-06-10 16:55:51 -0400

    develop branch for ROCm-4.4

--- bump_develop_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script needs to be edited to bump develop version after feature complete
-# - run this script in develop branch
-
-OLD_ROCBLAS_VERSION="2.39.0"
-NEW_ROCBLAS_VERSION="2.40.0"
-
-sed -i "s/${OLD_ROCBLAS_VERSION}/${NEW_ROCBLAS_VERSION}/g" CMakeLists.txt
