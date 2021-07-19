9af47bd0deaad515dca6a6020e2141e8b86d6c76
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2021-07-19 09:01:17 -0600

    linearized index optimization (#788)
    [ROCm/rocblas commit: 7aec310d58f95e0d3a4d6d798998581f71940626]

--- library/src/blas2/rocblas_syr.hpp
+++ library/src/blas2/rocblas_syr.hpp
@@ -8,32 +8,91 @@
 #include "handle.hpp"
 #include "rocblas.h"
 
-template <rocblas_int DIM_X, rocblas_int DIM_Y, typename T, typename U, typename V, typename W>
-ROCBLAS_KERNEL __launch_bounds__(DIM_X* DIM_Y) void rocblas_syr_kernel(rocblas_fill uplo,
-                                                                       rocblas_int  n,
-                                                                       U alpha_device_host,
-                                                                       rocblas_stride stride_alpha,
-                                                                       V              xa,
-                                                                       ptrdiff_t      shiftx,
-                                                                       rocblas_int    incx,
-                                                                       rocblas_stride stridex,
-                                                                       W              Aa,
-                                                                       ptrdiff_t      shiftA,
-                                                                       rocblas_int    lda,
-                                                                       rocblas_stride strideA)
+template <bool UPPER, rocblas_int DIM_X, typename T, typename U, typename V, typename W>
+ROCBLAS_KERNEL __launch_bounds__(DIM_X) void rocblas_syr_kernel_inc1(rocblas_int n,
+                                                                     size_t      area,
+                                                                     U           alpha_device_host,
+                                                                     rocblas_stride stride_alpha,
+                                                                     V              xa,
+                                                                     ptrdiff_t      shiftx,
+                                                                     rocblas_stride stridex,
+                                                                     W              Aa,
+                                                                     ptrdiff_t      shiftA,
+                                                                     rocblas_int    lda,
+                                                                     rocblas_stride strideA)
 {
     auto alpha = load_scalar(alpha_device_host, hipBlockIdx_z, stride_alpha);
     if(!alpha)
         return;
 
-    rocblas_int tx = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-    rocblas_int ty = hipBlockIdx_y * hipBlockDim_y + hipThreadIdx_y;
+    const auto* __restrict__ x = load_ptr_batch(xa, hipBlockIdx_z, shiftx, stridex);
+    T* __restrict__ A          = load_ptr_batch(Aa, hipBlockIdx_z, shiftA, strideA);
+
+    size_t i = size_t(hipBlockIdx_x) * hipBlockDim_x + hipThreadIdx_x; // linear area index
+    if(i >= area)
+        return;
+
+    size_t ri = !UPPER ? area - 1 - i : i;
+
+    // linearized triangle with diagonal to col, row
+    int         k  = (int)((sqrt(8 * ri + 1) - 1) / 2);
+    rocblas_int ty = k;
+    rocblas_int tx = ri - k * (k + 1) / 2;
+
+    if(!UPPER)
+    {
+        int maxIdx = n - 1;
+        tx         = maxIdx - tx;
+        ty         = maxIdx - ty;
+    }
+
+    A[tx + size_t(lda) * ty] += alpha * x[tx] * x[ty];
+
+    // original algorithm run over rectangular space
+    // if(uplo == rocblas_fill_lower ? tx < n && ty <= tx : ty < n && tx <= ty)
+    // A[tx + size_t(lda) * ty] += alpha * x[tx] * x[ty];
+}
+
+template <bool UPPER, rocblas_int DIM_X, typename T, typename U, typename V, typename W>
+ROCBLAS_KERNEL __launch_bounds__(DIM_X) void rocblas_syr_kernel(rocblas_int    n,
+                                                                size_t         area,
+                                                                U              alpha_device_host,
+                                                                rocblas_stride stride_alpha,
+                                                                V              xa,
+                                                                ptrdiff_t      shiftx,
+                                                                rocblas_int    incx,
+                                                                rocblas_stride stridex,
+                                                                W              Aa,
+                                                                ptrdiff_t      shiftA,
+                                                                rocblas_int    lda,
+                                                                rocblas_stride strideA)
+{
+    auto alpha = load_scalar(alpha_device_host, hipBlockIdx_z, stride_alpha);
+    if(!alpha)
+        return;
 
     const auto* __restrict__ x = load_ptr_batch(xa, hipBlockIdx_z, shiftx, stridex);
-    T* A                       = load_ptr_batch(Aa, hipBlockIdx_z, shiftA, strideA);
+    T* __restrict__ A          = load_ptr_batch(Aa, hipBlockIdx_z, shiftA, strideA);
 
-    if(uplo == rocblas_fill_lower ? tx < n && ty <= tx : ty < n && tx <= ty)
-        A[tx + lda * ty] += alpha * x[tx * incx] * x[ty * incx];
+    size_t i = size_t(hipBlockIdx_x) * hipBlockDim_x + hipThreadIdx_x; // linear area index
+    if(i >= area)
+        return;
+
+    size_t ri = !UPPER ? area - 1 - i : i;
+
+    // linearized triangle with diagonal to col, row
+    int         k  = (int)((sqrt(8 * ri + 1) - 1) / 2);
+    rocblas_int ty = k;
+    rocblas_int tx = ri - k * (k + 1) / 2;
+
+    if(!UPPER)
+    {
+        int maxIdx = n - 1;
+        tx         = maxIdx - tx;
+        ty         = maxIdx - ty;
+    }
+
+    A[tx + size_t(lda) * ty] += alpha * x[tx * incx] * x[ty * incx];
 }
 
 template <typename T, typename U, typename V, typename W>
@@ -89,56 +148,175 @@ ROCBLAS_INTERNAL_EXPORT_NOINLINE rocblas_status
 
     hipStream_t rocblas_stream = handle->get_stream();
 
-    static constexpr int GEMV_DIM_X = 128;
-    static constexpr int GEMV_DIM_Y = 8;
-    rocblas_int          blocksX    = (n - 1) / GEMV_DIM_X + 1;
-    rocblas_int          blocksY    = (n - 1) / GEMV_DIM_Y + 1;
+    static constexpr int SYR_DIM_X = 1024;
 
-    dim3 syr_grid(blocksX, blocksY, batch_count);
-    dim3 syr_threads(GEMV_DIM_X, GEMV_DIM_Y);
+    size_t nitems = (size_t)n * (n + 1) / 2;
+
+    rocblas_int blocksX = (nitems - 1) / (SYR_DIM_X) + 1;
+
+    dim3 syr_grid(blocksX, 1, batch_count);
+    dim3 syr_threads(SYR_DIM_X);
 
     // in case of negative inc shift pointer to end of data for negative indexing tid*inc
     auto shiftx = incx < 0 ? offsetx - ptrdiff_t(incx) * (n - 1) : offsetx;
 
     if(rocblas_pointer_mode_device == handle->pointer_mode)
     {
-        hipLaunchKernelGGL((rocblas_syr_kernel<GEMV_DIM_X, GEMV_DIM_Y, T>),
-                           syr_grid,
-                           syr_threads,
-                           0,
-                           rocblas_stream,
-                           uplo,
-                           n,
-                           alpha,
-                           stride_alpha,
-                           x,
-                           shiftx,
-                           incx,
-                           stridex,
-                           A,
-                           offseta,
-                           lda,
-                           strideA);
+        if(uplo == rocblas_fill_upper)
+        {
+            if(incx == 1)
+                hipLaunchKernelGGL((rocblas_syr_kernel_inc1<true, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+            else
+                hipLaunchKernelGGL((rocblas_syr_kernel<true, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   incx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+        }
+        else
+        {
+            if(incx == 1)
+                hipLaunchKernelGGL((rocblas_syr_kernel_inc1<false, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+            else
+                hipLaunchKernelGGL((rocblas_syr_kernel<false, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   incx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+        }
     }
-    else
+    else // host pointer mode
     {
-        hipLaunchKernelGGL((rocblas_syr_kernel<GEMV_DIM_X, GEMV_DIM_Y, T>),
-                           syr_grid,
-                           syr_threads,
-                           0,
-                           rocblas_stream,
-                           uplo,
-                           n,
-                           *alpha,
-                           stride_alpha,
-                           x,
-                           shiftx,
-                           incx,
-                           stridex,
-                           A,
-                           offseta,
-                           lda,
-                           strideA);
+        if(uplo == rocblas_fill_upper)
+        {
+            if(incx == 1)
+                hipLaunchKernelGGL((rocblas_syr_kernel_inc1<true, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   *alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+            else
+                hipLaunchKernelGGL((rocblas_syr_kernel<true, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   *alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   incx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+        }
+        else
+        {
+            if(incx == 1)
+                hipLaunchKernelGGL((rocblas_syr_kernel_inc1<false, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   *alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+            else
+                hipLaunchKernelGGL((rocblas_syr_kernel<false, SYR_DIM_X, T>),
+                                   syr_grid,
+                                   syr_threads,
+                                   0,
+                                   rocblas_stream,
+                                   n,
+                                   nitems,
+                                   *alpha,
+                                   stride_alpha,
+                                   x,
+                                   shiftx,
+                                   incx,
+                                   stridex,
+                                   A,
+                                   offseta,
+                                   lda,
+                                   strideA);
+        }
     }
     return rocblas_status_success;
 }
