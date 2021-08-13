83940457ae462b4a318388e517f3fb08e6e41aeb
amcamd <andrew.chapman@amd.com>
2021-08-13 17:03:44 -0400

    improve code structure for testing_gemm+batched+strided_batched
    [ROCm/rocblas commit: 95991b877ff0521ce043e3a21027bed39ff45c80]

--- clients/include/blas3/testing_gemm_batched.hpp
+++ clients/include/blas3/testing_gemm_batched.hpp
@@ -135,67 +135,51 @@ void testing_gemm_batched(const Arguments& arg)
 
     for(int i = 0; i < batch_count; i++)
     {
-
-        if(arg.initialization == rocblas_initialization::rand_int)
+        if(arg.alpha_isnan<T>())
         {
-            if(arg.alpha_isnan<T>())
-            {
-                rocblas_init_nan<T>(hA[i], A_row, A_col, lda);
-                rocblas_init_nan<T>(hB[i], B_row, B_col, ldb);
-            }
-            else
+            rocblas_init_nan<T>(hA[i], A_row, A_col, lda);
+            rocblas_init_nan<T>(hB[i], B_row, B_col, ldb);
+        }
+        else
+        {
+            if(arg.initialization == rocblas_initialization::rand_int)
             {
                 rocblas_init<T>(hA[i], A_row, A_col, lda);
                 rocblas_init_alternating_sign<T>(hB[i], B_row, B_col, ldb);
             }
-            if(arg.beta_isnan<T>())
-                rocblas_init_nan<T>(hC_1[i], M, N, ldc);
-            else
-                rocblas_init<T>(hC_1[i], M, N, ldc);
-        }
-        else if(arg.initialization == rocblas_initialization::trig_float)
-        {
-            if(arg.alpha_isnan<T>())
-            {
-                rocblas_init_nan<T>(hA[i], A_row, A_col, lda);
-                rocblas_init_nan<T>(hB[i], B_row, B_col, ldb);
-            }
-            else
+            else if(arg.initialization == rocblas_initialization::trig_float)
             {
                 rocblas_init_sin<T>(hA[i], A_row, A_col, lda);
                 rocblas_init_cos<T>(hB[i], B_row, B_col, ldb);
             }
-            if(arg.beta_isnan<T>())
-                rocblas_init_nan<T>(hC_1[i], M, N, ldc);
-            else
-                rocblas_init_sin<T>(hC_1[i], M, N, ldc);
-        }
-        else if(arg.initialization == rocblas_initialization::hpl)
-        {
-            if(arg.alpha_isnan<T>())
-            {
-                rocblas_init_nan<T>(hA[i], A_row, A_col, lda);
-                rocblas_init_nan<T>(hB[i], B_row, B_col, ldb);
-            }
-            else
+            else if(arg.initialization == rocblas_initialization::hpl)
             {
                 rocblas_init_hpl<T>(hA[i], A_row, A_col, lda);
                 rocblas_init_hpl<T>(hB[i], B_row, B_col, ldb);
             }
-            if(arg.beta_isnan<T>())
-                rocblas_init_nan<T>(hC_1[i], M, N, ldc);
             else
-                rocblas_init_hpl<T>(hC_1[i], M, N, ldc);
+            {
+#ifdef GOOGLE_TEST
+                FAIL() << "unknown initialization type";
+                return;
+#else
+                rocblas_cerr << "unknown initialization type" << std::endl;
+                rocblas_abort();
+#endif
+            }
+        }
+        if(arg.beta_isnan<T>())
+        {
+            rocblas_init_nan<T>(hC_1[i], M, N, ldc);
         }
         else
         {
-#ifdef GOOGLE_TEST
-            FAIL() << "unknown initialization type";
-            return;
-#else
-            rocblas_cerr << "unknown initialization type" << std::endl;
-            rocblas_abort();
-#endif
+            if(arg.initialization == rocblas_initialization::rand_int)
+                rocblas_init<T>(hC_1[i], M, N, ldc);
+            else if(arg.initialization == rocblas_initialization::trig_float)
+                rocblas_init_sin<T>(hC_1[i], M, N, ldc);
+            else if(arg.initialization == rocblas_initialization::hpl)
+                rocblas_init_hpl<T>(hC_1[i], M, N, ldc);
         }
     }
 
