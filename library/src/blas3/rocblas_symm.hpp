86bda2adfeb87810748484ed5c961830a14b94d9
amcamd <andrew.chapman@amd.com>
2021-09-07 20:04:02 -0400

    tweak template-proto.py so rocblas-exported-proto.hpp does not change
    [ROCm/rocblas commit: 521df549d0e0614f95109c1c4772f76142cafba9]

--- library/src/blas3/rocblas_symm.hpp
+++ library/src/blas3/rocblas_symm.hpp
@@ -6,11 +6,28 @@
 
 #include "handle.hpp"
 
-/**
-  *  TScal     is always: const T* (either host or device)
-  *  TConstPtr is either: const T* OR const T* const*
-  *  TPtr      is either:       T* OR       T* const*
-  */
+template <typename TScal, typename TConstPtr, typename TPtr>
+rocblas_status rocblas_symm_arg_check(rocblas_handle handle,
+                                      rocblas_side   side,
+                                      rocblas_fill   uplo,
+                                      rocblas_int    m,
+                                      rocblas_int    n,
+                                      TScal          alpha,
+                                      TConstPtr      AP,
+                                      rocblas_int    offsetA,
+                                      rocblas_int    lda,
+                                      rocblas_stride strideA,
+                                      TConstPtr      BP,
+                                      rocblas_int    offsetB,
+                                      rocblas_int    ldb,
+                                      rocblas_stride strideB,
+                                      TScal          beta,
+                                      TPtr           CP,
+                                      rocblas_int    offsetC,
+                                      rocblas_int    ldc,
+                                      rocblas_stride strideC,
+                                      rocblas_int    batch_count);
+
 template <bool HERM, typename TScal, typename TConstPtr, typename TPtr>
 ROCBLAS_INTERNAL_EXPORT_NOINLINE rocblas_status
     rocblas_internal_symm_template(rocblas_handle handle,
@@ -33,25 +50,3 @@ ROCBLAS_INTERNAL_EXPORT_NOINLINE rocblas_status
                                    rocblas_int    ldc,
                                    rocblas_stride strideC,
                                    rocblas_int    batch_count);
-
-template <typename TScal, typename TConstPtr, typename TPtr>
-rocblas_status rocblas_symm_arg_check(rocblas_handle handle,
-                                      rocblas_side   side,
-                                      rocblas_fill   uplo,
-                                      rocblas_int    m,
-                                      rocblas_int    n,
-                                      TScal          alpha,
-                                      TConstPtr      AP,
-                                      rocblas_int    offsetA,
-                                      rocblas_int    lda,
-                                      rocblas_stride strideA,
-                                      TConstPtr      BP,
-                                      rocblas_int    offsetB,
-                                      rocblas_int    ldb,
-                                      rocblas_stride strideB,
-                                      TScal          beta,
-                                      TPtr           CP,
-                                      rocblas_int    offsetC,
-                                      rocblas_int    ldc,
-                                      rocblas_stride strideC,
-                                      rocblas_int    batch_count);
