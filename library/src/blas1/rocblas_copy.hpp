c518c53ffbce5e581437b4933c854461e9e03c4b
Andrew Chapman <andrew.chapman@amd.com>
2021-09-20 10:24:24 -0500

    instantiate scale, swap, copy, axpy, rot, rotm, rotg, rotmg kernels to reduce size of librocblas.so (#859)
    * instantiating copy kernels to reduce size of librocblas.so
    
    * instantiating scal kernels to reduce size of librocblas.so
    
    * instantiating axpy kernels to reduce size of librocblas.so
    
    * instantiating swap kernels to reduce size of librocblas.so
    
    * add comment on manually updating instantiations
    
    * instantiate rot, rotm, rotg, rotmg kernels to reduce the size of librocblas.so
    
    * change from long to int64_t for Windows compatibility
    
    * change to rocblas_handle and rocblas_stride
    
    * for rocSOLVER compatibility go back to const Ta* for alpha in axpy and scal

--- library/src/blas1/rocblas_copy.hpp
+++ library/src/blas1/rocblas_copy.hpp
@@ -7,128 +7,18 @@
 #include "check_numerics_vector.hpp"
 #include "handle.hpp"
 
-template <bool CONJ, typename U, typename V>
-ROCBLAS_KERNEL void copy_kernel(rocblas_int    n,
-                                const U        xa,
-                                ptrdiff_t      shiftx,
-                                rocblas_int    incx,
-                                rocblas_stride stridex,
-                                V              ya,
-                                ptrdiff_t      shifty,
-                                rocblas_int    incy,
-                                rocblas_stride stridey)
-{
-    ptrdiff_t   tid = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-    const auto* x   = load_ptr_batch(xa, hipBlockIdx_y, shiftx, stridex);
-    auto*       y   = load_ptr_batch(ya, hipBlockIdx_y, shifty, stridey);
-    if(tid < n)
-    {
-
-        y[tid * incy] = CONJ ? conj(x[tid * incx]) : x[tid * incx];
-    }
-}
-
-//! @brief Optimized kernel for the floating points.
-//!
-template <rocblas_int NB, typename U, typename V>
-ROCBLAS_KERNEL __launch_bounds__(NB) void scopy_2_kernel(rocblas_int n,
-                                                         const U __restrict xa,
-                                                         ptrdiff_t      shiftx,
-                                                         rocblas_stride stridex,
-                                                         V __restrict ya,
-                                                         ptrdiff_t      shifty,
-                                                         rocblas_stride stridey)
-{
-    ptrdiff_t   tid = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 2;
-    const auto* x   = load_ptr_batch(xa, hipBlockIdx_y, shiftx, stridex);
-    auto*       y   = load_ptr_batch(ya, hipBlockIdx_y, shifty, stridey);
-    if(tid < n - 1)
-    {
-        for(rocblas_int j = 0; j < 2; ++j)
-        {
-            y[tid + j] = x[tid + j];
-        }
-    }
-    if(n % 2 != 0 && tid == n - 1)
-        y[tid] = x[tid];
-}
-
-template <bool CONJ, rocblas_int NB, typename U, typename V>
+template <bool CONJ, rocblas_int NB, typename T, typename U>
 rocblas_status rocblas_copy_template(rocblas_handle handle,
                                      rocblas_int    n,
-                                     U              x,
+                                     T              x,
                                      rocblas_int    offsetx,
                                      rocblas_int    incx,
                                      rocblas_stride stridex,
-                                     V              y,
+                                     U              y,
                                      rocblas_int    offsety,
                                      rocblas_int    incy,
                                      rocblas_stride stridey,
-                                     rocblas_int    batch_count)
-{
-    // Quick return if possible.
-    if(n <= 0 || batch_count <= 0)
-        return rocblas_status_success;
-
-    if(!x || !y)
-        return rocblas_status_invalid_pointer;
-
-    static constexpr bool using_rocblas_float
-        = std::is_same<V, rocblas_float*>{} || std::is_same<V, rocblas_float* const*>{};
-
-    if(!using_rocblas_float || incx != 1 || incy != 1)
-    {
-        // In case of negative inc shift pointer to end of data for negative indexing tid*inc
-        ptrdiff_t shiftx = offsetx - ((incx < 0) ? ptrdiff_t(incx) * (n - 1) : 0);
-        ptrdiff_t shifty = offsety - ((incy < 0) ? ptrdiff_t(incy) * (n - 1) : 0);
-
-        int  blocks = (n - 1) / NB + 1;
-        dim3 grid(blocks, batch_count);
-        dim3 threads(NB);
-
-        hipLaunchKernelGGL(copy_kernel<CONJ>,
-                           grid,
-                           threads,
-                           0,
-                           handle->get_stream(),
-                           n,
-                           x,
-                           shiftx,
-                           incx,
-                           stridex,
-                           y,
-                           shifty,
-                           incy,
-                           stridey);
-    }
-    else
-    {
-        // Kernel function for improving the performance of SCOPY when incx==1 and incy==1
-
-        // In case of negative inc shift pointer to end of data for negative indexing tid*inc
-        ptrdiff_t shiftx = offsetx - 0;
-        ptrdiff_t shifty = offsety - 0;
-
-        int         blocks = 1 + ((n - 1) / (NB * 2));
-        dim3        grid(blocks, batch_count);
-        dim3        threads(NB);
-        hipStream_t scopy_stream = handle->get_stream();
-
-        hipLaunchKernelGGL(scopy_2_kernel<NB>,
-                           grid,
-                           threads,
-                           0,
-                           scopy_stream,
-                           n,
-                           x,
-                           shiftx,
-                           stridex,
-                           y,
-                           shifty,
-                           stridey);
-    }
-    return rocblas_status_success;
-}
+                                     rocblas_int    batch_count);
 
 template <typename T, typename U>
 rocblas_status rocblas_copy_check_numerics(const char*    function_name,
@@ -144,32 +34,4 @@ rocblas_status rocblas_copy_check_numerics(const char*    function_name,
                                            rocblas_stride stride_y,
                                            rocblas_int    batch_count,
                                            const int      check_numerics,
-                                           bool           is_input)
-{
-    rocblas_status check_numerics_status
-        = rocblas_internal_check_numerics_vector_template(function_name,
-                                                          handle,
-                                                          n,
-                                                          x,
-                                                          offset_x,
-                                                          inc_x,
-                                                          stride_x,
-                                                          batch_count,
-                                                          check_numerics,
-                                                          is_input);
-    if(check_numerics_status != rocblas_status_success)
-        return check_numerics_status;
-
-    check_numerics_status = rocblas_internal_check_numerics_vector_template(function_name,
-                                                                            handle,
-                                                                            n,
-                                                                            y,
-                                                                            offset_y,
-                                                                            inc_y,
-                                                                            stride_y,
-                                                                            batch_count,
-                                                                            check_numerics,
-                                                                            is_input);
-
-    return check_numerics_status;
-}
+                                           bool           is_input);
--- library/src/blas1/rocblas_rot.hpp
+++ library/src/blas1/rocblas_rot.hpp
@@ -2,77 +2,24 @@
  * Copyright 2016-2021 Advanced Micro Devices, Inc.
  * ************************************************************************ */
 
-#pragma once
-
 #include "check_numerics_vector.hpp"
 #include "handle.hpp"
 
-template <typename Tex,
-          typename Tx,
-          typename Ty,
-          typename Tc,
-          typename Ts,
-          std::enable_if_t<!is_complex<Ts>, int> = 0>
-__device__ void
-    rot_kernel_calc(rocblas_int n, Tx* x, rocblas_int incx, Ty* y, rocblas_int incy, Tc c, Ts s)
-{
-    ptrdiff_t tid = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-
-    if(tid < n)
-    {
-        auto ix    = tid * incx;
-        auto iy    = tid * incy;
-        Tex  tempx = Tex(c * x[ix]) + Tex(s * y[iy]);
-        Tex  tempy = Tex(c * y[iy]) - Tex((s)*x[ix]);
-        y[iy]      = Ty(tempy);
-        x[ix]      = Tx(tempx);
-    }
-}
-
-template <typename Tex,
-          typename Tx,
-          typename Ty,
-          typename Tc,
-          typename Ts,
-          std::enable_if_t<is_complex<Ts>, int> = 0>
-__device__ void
-    rot_kernel_calc(rocblas_int n, Tx* x, rocblas_int incx, Ty* y, rocblas_int incy, Tc c, Ts s)
-{
-    ptrdiff_t tid = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-
-    if(tid < n)
-    {
-        auto ix    = tid * incx;
-        auto iy    = tid * incy;
-        Tex  tempx = Tex(c * x[ix]) + Tex(s * y[iy]);
-        Tex  tempy = Tex(c * y[iy]) - Tex(conj(s) * x[ix]);
-        y[iy]      = Ty(tempy);
-        x[ix]      = Tx(tempx);
-    }
-}
-
-template <typename Tex, typename Tx, typename Ty, typename Tc, typename Ts>
-ROCBLAS_KERNEL void rot_kernel(rocblas_int    n,
-                               Tx             x_in,
-                               rocblas_int    offset_x,
-                               rocblas_int    incx,
-                               rocblas_stride stride_x,
-                               Ty             y_in,
-                               rocblas_int    offset_y,
-                               rocblas_int    incy,
-                               rocblas_stride stride_y,
-                               Tc             c_in,
-                               rocblas_stride c_stride,
-                               Ts             s_in,
-                               rocblas_stride s_stride)
-{
-    auto c = std::real(load_scalar(c_in, hipBlockIdx_y, c_stride));
-    auto s = load_scalar(s_in, hipBlockIdx_y, s_stride);
-    auto x = load_ptr_batch(x_in, hipBlockIdx_y, offset_x, stride_x);
-    auto y = load_ptr_batch(y_in, hipBlockIdx_y, offset_y, stride_y);
-
-    rot_kernel_calc<Tex>(n, x, incx, y, incy, c, s);
-}
+template <typename T>
+rocblas_status rocblas_rot_check_numerics(const char*    function_name,
+                                          rocblas_handle handle,
+                                          rocblas_int    n,
+                                          T              x,
+                                          rocblas_int    offset_x,
+                                          rocblas_int    inc_x,
+                                          rocblas_stride stride_x,
+                                          T              y,
+                                          rocblas_int    offset_y,
+                                          rocblas_int    inc_y,
+                                          rocblas_stride stride_y,
+                                          rocblas_int    batch_count,
+                                          const int      check_numerics,
+                                          bool           is_input);
 
 template <rocblas_int NB, typename Tex, typename Tx, typename Ty, typename Tc, typename Ts>
 rocblas_status rocblas_rot_template(rocblas_handle handle,
@@ -89,101 +36,4 @@ rocblas_status rocblas_rot_template(rocblas_handle handle,
                                     rocblas_stride c_stride,
                                     Ts*            s,
                                     rocblas_stride s_stride,
-                                    rocblas_int    batch_count)
-{
-    // Quick return if possible
-    if(n <= 0 || batch_count <= 0)
-        return rocblas_status_success;
-
-    auto shiftx = incx < 0 ? offset_x - ptrdiff_t(incx) * (n - 1) : offset_x;
-    auto shifty = incy < 0 ? offset_y - ptrdiff_t(incy) * (n - 1) : offset_y;
-
-    dim3        blocks((n - 1) / NB + 1, batch_count);
-    dim3        threads(NB);
-    hipStream_t rocblas_stream = handle->get_stream();
-
-    if(rocblas_pointer_mode_device == handle->pointer_mode)
-        hipLaunchKernelGGL(rot_kernel<Tex>,
-                           blocks,
-                           threads,
-                           0,
-                           rocblas_stream,
-                           n,
-                           x,
-                           shiftx,
-                           incx,
-                           stride_x,
-                           y,
-                           shifty,
-                           incy,
-                           stride_y,
-                           c,
-                           c_stride,
-                           s,
-                           s_stride);
-    else // c and s are on host
-        hipLaunchKernelGGL(rot_kernel<Tex>,
-                           blocks,
-                           threads,
-                           0,
-                           rocblas_stream,
-                           n,
-                           x,
-                           shiftx,
-                           incx,
-                           stride_x,
-                           y,
-                           shifty,
-                           incy,
-                           stride_y,
-                           *c,
-                           c_stride,
-                           *s,
-                           s_stride);
-
-    return rocblas_status_success;
-}
-
-template <typename T>
-rocblas_status rocblas_rot_check_numerics(const char*    function_name,
-                                          rocblas_handle handle,
-                                          rocblas_int    n,
-                                          T              x,
-                                          rocblas_int    offset_x,
-                                          rocblas_int    inc_x,
-                                          rocblas_stride stride_x,
-                                          T              y,
-                                          rocblas_int    offset_y,
-                                          rocblas_int    inc_y,
-                                          rocblas_stride stride_y,
-                                          rocblas_int    batch_count,
-                                          const int      check_numerics,
-                                          bool           is_input)
-{
-    rocblas_status check_numerics_status
-        = rocblas_internal_check_numerics_vector_template(function_name,
-                                                          handle,
-                                                          n,
-                                                          x,
-                                                          offset_x,
-                                                          inc_x,
-                                                          stride_x,
-                                                          batch_count,
-                                                          check_numerics,
-                                                          is_input);
-    if(check_numerics_status != rocblas_status_success)
-        return check_numerics_status;
-
-    check_numerics_status = rocblas_internal_check_numerics_vector_template(function_name,
-                                                                            handle,
-                                                                            n,
-                                                                            y,
-                                                                            offset_y,
-                                                                            inc_y,
-                                                                            stride_y,
-                                                                            batch_count,
-                                                                            check_numerics,
-                                                                            is_input);
-
-    return check_numerics_status;
-}
+                                    rocblas_int    batch_count);
