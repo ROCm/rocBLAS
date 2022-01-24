f62d0b2cf23170d096f6a7a81a6ba75b24b98330
Ruban Kumar <92320771+rkamd@users.noreply.github.com>
2022-01-24 08:46:20 -0500

    Hotfix for rocBLAS documentation (#1052) (#1053)
    - Fix to support readthedocs format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'User_Guide'
+master_doc = 'index'
 
 # General information about the project.
 project = u'rocBLAS'
@@ -100,7 +100,7 @@ else:
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    html_logo = "rocm_logo.jpg"
+    html_logo = "rocm_logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
