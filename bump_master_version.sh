595fd21c57e72d4bd978ac15e1209b75ed97eb29
amcamd <andrew.chapman@amd.com>
2022-03-21 20:55:39 -0400

    master branch for ROCm-5.2
    [ROCm/rocblas commit: 206101fb84f73d64d5b2a373ff74ba23574b0eb0]

--- bump_master_version.sh
+++ bump_master_version.sh
@@ -6,11 +6,11 @@
 # - after running this script and merging staging into master, run bump_staging_version.sh in master and
 #   merge master into staging
 
-OLD_TENSILE_VERSION="Tensile 4.31.0"
-NEW_TENSILE_VERSION="Tensile 4.32.0"
+OLD_TENSILE_VERSION="Tensile 4.32.1"
+NEW_TENSILE_VERSION="Tensile 4.33.0"
 
-OLD_TENSILE_HASH="ed6862d1ba8dca1dbb0a6a8177dd5a1d271d6f88"
-NEW_TENSILE_HASH="730f511a39859c7e2911bb0056d7ddada0ec8e58"
+OLD_TENSILE_HASH="725b7a9e632cbc79c587d06580611cfe28102f89"
+NEW_TENSILE_HASH="c8bce10c3f9b4ce38f6d9e5ca5ef1ebb8804cc15"
 
 # OLD_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.6.0"
 # NEW_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.7.1"
