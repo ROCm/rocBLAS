2ea5b66aa1957e878151b5e2cdbf86663dde4687
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2022-07-15 16:48:05 -0400

    windows clang closer to linux now (#1264)
    * windows clang closer to linux now so don't need static to avoid duplicate symbols

--- library/src/include/macros.hpp
+++ library/src/include/macros.hpp
@@ -26,24 +26,17 @@
  * Macros
  ******************************************************************************/
 
-#ifdef WIN32
-#define ROCBLAS_KERNEL(lb_) __global__ __launch_bounds__((lb_)) static void
-#define ROCBLAS_KERNEL_NO_BOUNDS __global__ static void
-#else
+// note compiler differences may require variations in definitions
+//#ifdef WIN32
+//#else
+//#endif
+
 #define ROCBLAS_KERNEL(lb_) __global__ __launch_bounds__((lb_)) void
 #define ROCBLAS_KERNEL_NO_BOUNDS __global__ void
-#endif
 
 // A storage-class-specifier other than thread_local shall not be specified in an explicit specialization.
 // ROCBLAS_KERNEL_INSTANTIATE should be used where kernels are instantiated to avoid use of static.
-#ifdef WIN32
-#define ROCBLAS_KERNEL_INSTANTIATE __global__
-#else
-#define ROCBLAS_KERNEL_INSTANTIATE __global__
-#endif
 
-#ifdef WIN32
+#define ROCBLAS_KERNEL_INSTANTIATE __global__
+
 #define ROCBLAS_KERNEL_ILF __device__ __attribute__((always_inline))
-#else
-#define ROCBLAS_KERNEL_ILF __device__ __attribute__((always_inline))
-#endif
