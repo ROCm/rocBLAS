44292bda46f897afa8f0b8767f4e8e608571b69d
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2022-08-17 13:26:41 -0600

    update rmake for linux, minor fixes, cleanup (#1397)
    * update rmake for linux use, minor fixes, clean out unsupported

--- toolchain-linux.cmake
+++ toolchain-linux.cmake
@@ -3,16 +3,31 @@ if (NOT python)
   set(python "python3") # default for linux
 endif()
 
-# if (DEFINED ENV{ROCM_PATH})
-#   set(rocm_bin "$ENV{ROCM_PATH}/hip/bin")
-# else()
-#   set(rocm_bin "/opt/rocm/hip/bin")
-# endif()
+if (DEFINED ENV{ROCM_PATH})
+  set(rocm_bin "$ENV{ROCM_PATH}/hip/bin")
+else()
+  set(rocm_bin "/opt/rocm/hip/bin")
+endif()
 
+# relying on env and path for backward compatibility with external recipes
+if (NOT DEFINED ENV{CXX} AND NOT CMAKE_CXX_COMPILER)
+  set(CMAKE_CXX_COMPILER "hipcc")
+endif()
+
+if (NOT DEFINED ENV{CC} AND NOT CMAKE_C_COMPILER)
+  set(CMAKE_C_COMPILER "hipcc")
+endif()
+
+if (NOT DEFINED ENV{FC} AND NOT CMAKE_Fortran_COMPILER)
+  set(CMAKE_Fortran_COMPILER "gfortran")
+endif()
+
+# relying on path for backward compatibility
 # set(CMAKE_CXX_COMPILER "${rocm_bin}/hipcc")
 # set(CMAKE_C_COMPILER "${rocm_bin}/hipcc")
-# set(python "python3")
 
-# TODO remove, just to speed up slow cmake
-# set(CMAKE_C_COMPILER_WORKS 1)
-# set(CMAKE_CXX_COMPILER_WORKS 1)
+if (CONFIG_NO_COMPILER_CHECKS)
+  set(CMAKE_CXX_COMPILER_WORKS 1)
+  set(CMAKE_C_COMPILER_WORKS 1)
+  set(CMAKE_Fortran_COMPILER_WORKS 1)
+endif()
