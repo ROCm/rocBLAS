f61c36d0d48448e020162f038a6c376ab31d6f32
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2022-11-17 08:35:50 -0700

    allow d/z trtri one less digit of precision (#1528)
    * allow d/z trtri one less digit of precision
    * use N for tolerance when large

--- clients/include/blas3/testing_trtri_batched.hpp
+++ clients/include/blas3/testing_trtri_batched.hpp
@@ -318,7 +318,7 @@ void testing_trtri_batched(const Arguments& arg)
 
         if(arg.unit_check)
         {
-            const double rel_error = get_epsilon<T>() * 1000;
+            const double rel_error = trtri_tolerance<T>(N);
             near_check_general<T>(N, N, lda, hB, hA, batch_count, rel_error);
             near_check_general<T>(N, N, lda, hB, hA_2, batch_count, rel_error);
         }
