82234654c7b989557ceab9d803d94c2d218312dd
Sam Wu <sam.wu2@amd.com>
2023-05-17 10:43:37 -0600

    Documentation update for RTD
    update documentation requirements (#1858)
    
    add links to documentation roadmap (#1876)
    
    Reduce dependabot schedule to weekly and add no ci labels (#1897)
    
    * reduce dependabot schedule to weekly
    
    * add noCI label for dependabot PRs
    
    * add doc team as reviewers for dependabot PRs
    
    Update rocBLAS documentation to new template (#1706)
    
    Remove old docs system config files (#1770)
    
    These files will no longer be relevant with the transition to a new
    docs system in ROCm 5.6.
    
    rocm-docs-core==0.13.4

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import shutil
+
+from rocm_docs import ROCmDocs
+
+docs_core = ROCmDocs("rocBLAS Documentation")
+docs_core.run_doxygen()
+docs_core.setup()
+
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
