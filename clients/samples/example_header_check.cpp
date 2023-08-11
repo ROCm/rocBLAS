4a653a91df879c3c64850bcf91ccbce75c3e9360
Ruban Kumar <92320771+rkamd@users.noreply.github.com>
2023-08-11 10:33:58 -0400

    Sample program to validate C++ header features (#1996)
    [ROCm/rocblas commit: c0601a12c067e267d8de4328aa4bf0198c134249]

---
+++ clients/samples/example_header_check.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************
+ * Copyright (C) 2020-2023 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell cop-
+ * ies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM-
+ * PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
+ * CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ************************************************************************ */
+#include <hip/hip_runtime.h>
+#include <iostream>
+#include <rocblas/rocblas.h>
+
+#ifndef CHECK_HIP_ERROR
+#define CHECK_HIP_ERROR(error)                    \
+    if(error != hipSuccess)                       \
+    {                                             \
+        fprintf(stderr,                           \
+                "hip error: '%s'(%d) at %s:%d\n", \
+                hipGetErrorString(error),         \
+                error,                            \
+                __FILE__,                         \
+                __LINE__);                        \
+        exit(EXIT_FAILURE);                       \
+    }
+#endif
+
+#ifndef CHECK_ROCBLAS_STATUS
+#define CHECK_ROCBLAS_STATUS(status)                  \
+    if(status != rocblas_status_success)              \
+    {                                                 \
+        fprintf(stderr,                               \
+                "rocBLAS error: '%s'(%d) at %s:%d\n", \
+                rocblas_status_to_string(status),     \
+                status,                               \
+                __FILE__,                             \
+                __LINE__);                            \
+        exit(EXIT_FAILURE);                           \
+    }
+#endif
+
+/*!
+ * \brief A simple program to validate if rocBLAS library header files
+ * have correct cpp version gaurds. This program is compiled for C++11, and will cause
+ * compilation error, if any header file does not use appropriate cpp version check gaurd.
+ */
+int main()
+{
+    rocblas_handle handle;
+    rocblas_status rstatus = rocblas_create_handle(&handle);
+    CHECK_ROCBLAS_STATUS(rstatus);
+    rstatus = rocblas_destroy_handle(handle);
+    CHECK_ROCBLAS_STATUS(rstatus);
+    return 0;
+}
