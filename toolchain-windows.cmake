12a359bd9510798383e7a43e7f6942043f4228b8
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2023-08-15 08:04:03 -0600

    avoid repeating compiler flags on windows (#2001)
    * guards toolchain section
    
    [ROCm/rocblas commit: 753ea3c33ecd80bab77bf960551d0ce40c793f2a]

--- toolchain-windows.cmake
+++ toolchain-windows.cmake
@@ -27,19 +27,22 @@ endif()
 #set(CMAKE_CXX_COMPILER_WORKS 1)
 #set(CMAKE_Fortran_COMPILER_WORKS 1)
 
+if (NOT ROCBLAS_TOOLCHAIN_VARS_APPENDED)
+  set(ROCBLAS_TOOLCHAIN_VARS_APPENDED True)
 
-# our usage flags
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DWIN32 -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_WARNINGS -D_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING")
+  # our usage flags
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DWIN32 -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_CRT_SECURE_NO_WARNINGS -D_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING")
 
-# flags for clang direct use
+  # flags for clang direct use
 
-# -Wno-ignored-attributes to avoid warning: __declspec attribute 'dllexport' is not supported [-Wignored-attributes] which is used by msvc compiler
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-ignored-attributes")
+  # -Wno-ignored-attributes to avoid warning: __declspec attribute 'dllexport' is not supported [-Wignored-attributes] which is used by msvc compiler
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-ignored-attributes")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DHIP_CLANG_HCC_COMPAT_MODE=1")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DHIP_CLANG_HCC_COMPAT_MODE=1")
 
-# args also in hipcc.bat
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions -fms-compatibility -D__HIP_ROCclr__=1 -D__HIP_PLATFORM_AMD__=1 ")
+  # args also in hipcc.bat
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions -fms-compatibility -D__HIP_ROCclr__=1 -D__HIP_PLATFORM_AMD__=1")
+endif()
 
 if (DEFINED ENV{OPENBLAS_DIR})
   file(TO_CMAKE_PATH "$ENV{OPENBLAS_DIR}" OPENBLAS_DIR)
