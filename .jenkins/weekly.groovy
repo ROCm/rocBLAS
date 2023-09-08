acad1c9d85d22ebe2e2db2b2c4df8e9d7f2f2c91
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2023-09-07 20:26:55 -0600

    CI: Fix incorrect rocJenkins branch name in weekly job (#2038)

--- .jenkins/weekly.groovy
+++ .jenkins/weekly.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 // This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
-@Library('rocJenkins@eyoshida/statusdebug') _
+@Library('rocJenkins@pong') _
 
 // This is file for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
