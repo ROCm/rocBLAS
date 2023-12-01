26f85f69d3102f1ceb015cd21f50cdb364bcde96
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2023-12-01 13:14:35 -0700

    ILP64 check_numerics_matrix (#2173)
    * ILP64 check_numerics_matrix
    * adds unit tests for general stress testing of check_numerics helpers
    * check_numerics_vector support and testing of negative and zero increments
    
    [ROCm/rocblas commit: 26df3ed3eace9cb3e2c5c704f5e3d9cffc915f4c]

--- library/src/include/check_numerics_matrix.hpp
+++ library/src/include/check_numerics_matrix.hpp
@@ -26,111 +26,6 @@
 #include "handle.hpp"
 #include "rocblas.h"
 
-/**
-  *
-  * rocblas_check_numerics_ge_matrix_kernel(m, n, Aa, offset_a, lda, stride_a, abnormal)
-  *
-  * Info about rocblas_check_numerics_ge_matrix_kernel function:
-  *
-  *    It is the kernel function which checks a matrix for numerical abnormalities such as NaN/zero/Inf/denormal values and updates the rocblas_check_numerics_t structure.
-  *    ge in rocblas_check_numerics_ge_matrix_kernel refers to general.
-  *
-  * Parameters   : m            : number of rows of matrix 'A'
-  *                n            : number of columns of matrix 'A'
-  *                Aa           : Pointer to the matrix which is under consideration for numerical abnormalities
-  *                offset_a     : Offset of matrix 'Aa'
-  *                lda          : specifies the leading dimension of matrix 'Aa'
-  *                stride_a     : Specifies the pointer increment between one matrix 'A_i' and the next one (Aa_i+1) (where (Aa_i) is the i-th instance of the batch)
-  *                abnormal     : Device pointer to the rocblas_check_numerics_t structure
-  *
-  * Return Value : Nothing --
-  *
-**/
-
-template <int DIM_X, int DIM_Y, typename T>
-ROCBLAS_KERNEL(DIM_X* DIM_Y)
-rocblas_check_numerics_ge_matrix_kernel(rocblas_int               m,
-                                        rocblas_int               n,
-                                        T                         Aa,
-                                        rocblas_stride            offset_a,
-                                        int64_t                   lda,
-                                        rocblas_stride            stride_a,
-                                        rocblas_check_numerics_t* abnormal)
-{
-    rocblas_int tx = blockIdx.x * blockDim.x + threadIdx.x;
-    rocblas_int ty = blockIdx.y * blockDim.y + threadIdx.y;
-
-    //Check every element of the A matrix for a NaN/zero/Inf/denormal value
-    if(tx < m && ty < n)
-    {
-        auto* A = load_ptr_batch(Aa, blockIdx.z, offset_a, stride_a);
-
-        int64_t tid   = tx + lda * ty;
-        auto    value = A[tid];
-        if(!abnormal->has_zero && rocblas_iszero(value))
-            abnormal->has_zero = true;
-        if(!abnormal->has_NaN && rocblas_isnan(value))
-            abnormal->has_NaN = true;
-        if(!abnormal->has_Inf && rocblas_isinf(value))
-            abnormal->has_Inf = true;
-        if(!abnormal->has_denorm && rocblas_isdenorm(value))
-            abnormal->has_denorm = true;
-    }
-}
-
-/**
-  *
-  * rocblas_check_numerics_sym_herm_tri_matrix_kernel(is_upper, n, Aa, offset_a, lda, stride_a, abnormal)
-  *
-  * Info about rocblas_check_numerics_sym_herm_tri_matrix_kernel function:
-  *
-  *    It is the kernel function which checks symmetric, hermitian and triangular matrices for numerical abnormalities such as NaN/zero/Inf/denormal values
-  *    and updates the rocblas_check_numerics_t structure.
-  *    sym_herm_tri in rocblas_check_numerics_sym_herm_tri_matrix_kernel refers to symmetric, hermitian and triangular matrices.
-  *
-  * Parameters   : is_upper     : Boolean which is true when the rocblas_fill is rocblas_fill_upper and false when it is rocblas_fill_lower
-  *                n            : number of columns of matrix 'A'
-  *                Aa           : Pointer to the matrix which is under consideration for numerical abnormalities
-  *                offset_a     : Offset of matrix 'Aa'
-  *                lda          : specifies the leading dimension of matrix 'Aa'
-  *                stride_a     : Specifies the pointer increment between one matrix 'A_i' and the next one (Aa_i+1) (where (Aa_i) is the i-th instance of the batch)
-  *                abnormal     : Device pointer to the rocblas_check_numerics_t structure
-  *
-  * Return Value : Nothing --
-  *
-**/
-
-template <int DIM_X, int DIM_Y, typename T>
-ROCBLAS_KERNEL(DIM_X* DIM_Y)
-rocblas_check_numerics_sym_herm_tri_matrix_kernel(bool                      is_upper,
-                                                  rocblas_int               n,
-                                                  T                         Aa,
-                                                  rocblas_stride            offset_a,
-                                                  int64_t                   lda,
-                                                  rocblas_stride            stride_a,
-                                                  rocblas_check_numerics_t* abnormal)
-{
-    rocblas_int tx = blockIdx.x * blockDim.x + threadIdx.x;
-    rocblas_int ty = blockIdx.y * blockDim.y + threadIdx.y;
-
-    //Check every element of the A matrix for a NaN/zero/Inf/denormal value
-    if(is_upper ? ty < n && tx <= ty : tx < n && ty <= tx)
-    {
-        auto* A = load_ptr_batch(Aa, blockIdx.z, offset_a, stride_a);
-
-        int64_t tid   = tx + lda * ty;
-        auto    value = A[tid];
-        if(!abnormal->has_zero && rocblas_iszero(value))
-            abnormal->has_zero = true;
-        if(!abnormal->has_NaN && rocblas_isnan(value))
-            abnormal->has_NaN = true;
-        if(!abnormal->has_Inf && rocblas_isinf(value))
-            abnormal->has_Inf = true;
-        if(!abnormal->has_denorm && rocblas_isdenorm(value))
-            abnormal->has_denorm = true;
-    }
-}
-
 template <typename T>
 ROCBLAS_INTERNAL_EXPORT_NOINLINE rocblas_status
     rocblas_internal_check_numerics_matrix_template(const char*               function_name,
@@ -138,12 +33,12 @@ ROCBLAS_INTERNAL_EXPORT_NOINLINE rocblas_status
                                                     rocblas_operation         trans_a,
                                                     rocblas_fill              uplo,
                                                     rocblas_check_matrix_type matrix_type,
-                                                    rocblas_int               m,
-                                                    rocblas_int               n,
+                                                    int64_t                   m,
+                                                    int64_t                   n,
                                                     T                         A,
                                                     rocblas_stride            offset_a,
                                                     int64_t                   lda,
                                                     rocblas_stride            stride_a,
-                                                    rocblas_int               batch_count,
+                                                    int64_t                   batch_count,
                                                     const int                 check_numerics,
                                                     bool                      is_input);
