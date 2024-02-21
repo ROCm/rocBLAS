dd3b45db1c3677c82d4a82007523f0de0fbda80b
Andrew Chapman <andrew.chapman@amd.com>
2024-02-21 11:42:41 -0600

    Benchmark class contains common code for timing rocBLAS functions (#2356)
    * Benchmark class contains code for timing rocBLAS functions
    
    * Benchmark class improvements
    
    [ROCm/rocblas commit: ab5d81a3d6fc6bf651d504afbb748ba19981b1d9]

--- clients/include/blas1/testing_copy.hpp
+++ clients/include/blas1/testing_copy.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2018-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2018-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,15 +22,42 @@
 
 #pragma once
 
+#include "benchmark.hpp"
 #include "testing_common.hpp"
+
+template <typename T>
+static rocblas_status copy_dispatch(rocblas_handle     handle,
+                                    rocblas_int        N,
+                                    const T*           dx,
+                                    rocblas_int        incx,
+                                    T*                 dy,
+                                    rocblas_int        incy,
+                                    rocblas_client_api client_api)
+{
+    switch(client_api)
+    {
+    case C:
+        return rocblas_copy<T, false>(handle, N, dx, incx, dy, incy);
+    case C_64:
+        return rocblas_copy_64<T, false>(handle, N, dx, incx, dy, incy);
+    case FORTRAN:
+        return rocblas_copy<T, true>(handle, N, dx, incx, dy, incy);
+    case FORTRAN_64:
+        return rocblas_copy_64<T, true>(handle, N, dx, incx, dy, incy);
+    case INTERNAL:
+        rocblas_cerr << "Error: not implemented client_api == INTERNAL   " << std::endl;
+    case INTERNAL_64:
+        rocblas_cerr << "Error: not implemented client_api == INTERNAL_64" << std::endl;
+    default:
+        rocblas_cerr << "Error: not implemented client_api == " << client_api << std::endl;
+    }
+
+    return rocblas_status_invalid_value;
+}
 
 template <typename T>
 void testing_copy_bad_arg(const Arguments& arg)
 {
-    auto rocblas_copy_fn = arg.api == FORTRAN ? rocblas_copy<T, true> : rocblas_copy<T, false>;
-    auto rocblas_copy_fn_64
-        = arg.api == FORTRAN_64 ? rocblas_copy_64<T, true> : rocblas_copy_64<T, false>;
-
     rocblas_local_handle handle{arg};
 
     int64_t N    = 100;
@@ -42,21 +69,19 @@ void testing_copy_bad_arg(const Arguments& arg)
     CHECK_DEVICE_ALLOCATION(dx.memcheck());
     CHECK_DEVICE_ALLOCATION(dy.memcheck());
 
-    DAPI_EXPECT(rocblas_status_invalid_handle, rocblas_copy_fn, (nullptr, N, dx, incx, dy, incy));
+    EXPECT_ROCBLAS_STATUS(rocblas_status_invalid_handle,
+                          copy_dispatch<T>(nullptr, N, dx, incx, dy, incy, arg.api));
 
-    DAPI_EXPECT(
-        rocblas_status_invalid_pointer, rocblas_copy_fn, (handle, N, nullptr, incx, dy, incy));
-    DAPI_EXPECT(
-        rocblas_status_invalid_pointer, rocblas_copy_fn, (handle, N, dx, incx, nullptr, incy));
+    EXPECT_ROCBLAS_STATUS(rocblas_status_invalid_pointer,
+                          copy_dispatch<T>(handle, N, nullptr, incx, dy, incy, arg.api));
+
+    EXPECT_ROCBLAS_STATUS(rocblas_status_invalid_pointer,
+                          copy_dispatch<T>(handle, N, dx, incx, nullptr, incy, arg.api));
 }
 
 template <typename T>
 void testing_copy(const Arguments& arg)
 {
-    auto rocblas_copy_fn = arg.api == FORTRAN ? rocblas_copy<T, true> : rocblas_copy<T, false>;
-    auto rocblas_copy_fn_64
-        = arg.api == FORTRAN_64 ? rocblas_copy_64<T, true> : rocblas_copy_64<T, false>;
-
     int64_t              N    = arg.N;
     int64_t              incx = arg.incx;
     int64_t              incy = arg.incy;
@@ -65,7 +90,7 @@ void testing_copy(const Arguments& arg)
     // Argument sanity check before allocating invalid memory
     if(N <= 0)
     {
-        DAPI_CHECK(rocblas_copy_fn, (handle, N, nullptr, incx, nullptr, incy));
+        CHECK_ROCBLAS_ERROR(copy_dispatch<T>(handle, N, nullptr, incx, nullptr, incy, arg.api));
         return;
     }
 
@@ -87,8 +112,7 @@ void testing_copy(const Arguments& arg)
     rocblas_init_vector(hx, arg, rocblas_client_alpha_sets_nan, true);
     rocblas_init_vector(hy, arg, rocblas_client_alpha_sets_nan, false);
 
-    // copy vector is easy in STL; hy_gold = hx: save a copy in hy_gold which will be output of CPU
-    // BLAS
+    // save a copy in hy_gold which will be output of CPU BLAS
     hy_gold = hy;
 
     // copy data from CPU to device
@@ -102,7 +126,7 @@ void testing_copy(const Arguments& arg)
     {
         handle.pre_test(arg);
         // GPU BLAS
-        DAPI_CHECK(rocblas_copy_fn, (handle, N, dx, incx, dy, incy));
+        CHECK_ROCBLAS_ERROR(copy_dispatch<T>(handle, N, dx, incx, dy, incy, arg.api));
         handle.post_test(arg);
 
         CHECK_HIP_ERROR(hy.transfer_from(dy));
@@ -126,21 +150,16 @@ void testing_copy(const Arguments& arg)
     if(arg.timing)
     {
         double gpu_time_used;
-        int    number_cold_calls = arg.cold_iters;
-        int    total_calls       = number_cold_calls + arg.iters;
 
         hipStream_t stream;
         CHECK_ROCBLAS_ERROR(rocblas_get_stream(handle, &stream));
 
-        for(int iter = 0; iter < total_calls; iter++)
-        {
-            if(iter == number_cold_calls)
-                gpu_time_used = get_time_us_sync(stream);
+        auto lambda_to_benchmark
+            = [&] { copy_dispatch<T>(handle, N, dx, incx, dy, incy, arg.api); };
 
-            DAPI_DISPATCH(rocblas_copy_fn, (handle, N, dx, incx, dy, incy));
-        }
+        Benchmark<decltype(lambda_to_benchmark)> stats(lambda_to_benchmark, stream, arg);
 
-        gpu_time_used = get_time_us_sync(stream) - gpu_time_used;
+        gpu_time_used = stats.timer();
 
         ArgumentModel<e_N, e_incx, e_incy>{}.log_args<T>(rocblas_cout,
                                                          arg,
