4752454981237635d2b9f0dd9ad4c324add6d8ee
Ruban Kumar <92320771+rkamd@users.noreply.github.com>
2024-03-01 09:57:35 -0600

    Run 2 Run validation for syrk and syr2k (#2377)
    [ROCm/rocblas commit: 24505ba19f14c4e4ad380f6f190c5dfdbcd2b1a0]

--- clients/include/blas3/testing_syr2k_strided_batched.hpp
+++ clients/include/blas3/testing_syr2k_strided_batched.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2020-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2020-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -534,6 +534,37 @@ void testing_syr2k_strided_batched(const Arguments& arg)
                                                                 ldc,
                                                                 strideC,
                                                                 batch_count));
+
+            if(arg.repeatability_check)
+            {
+                host_strided_batch_matrix<T> hC_copy(N, N, ldc, strideC, batch_count);
+
+                CHECK_HIP_ERROR(hC.transfer_from(dC));
+                for(int i = 0; i < arg.iters; i++)
+                {
+                    CHECK_HIP_ERROR(dC.transfer_from(hC_gold));
+                    CHECK_ROCBLAS_ERROR(rocblas_syrk_strided_batched_fn(handle,
+                                                                        uplo,
+                                                                        transA,
+                                                                        N,
+                                                                        K,
+                                                                        d_alpha,
+                                                                        dA,
+                                                                        lda,
+                                                                        strideA,
+                                                                        dB,
+                                                                        ldb,
+                                                                        strideB,
+                                                                        d_beta,
+                                                                        dC,
+                                                                        ldc,
+                                                                        strideC,
+                                                                        batch_count));
+                    CHECK_HIP_ERROR(hC_copy.transfer_from(dC));
+                    unit_check_general<T>(N, N, ldc, strideC, hC, hC_copy, batch_count);
+                }
+                return;
+            }
         }
 
         // CPU BLAS
