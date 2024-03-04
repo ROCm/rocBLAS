b03974a3d2b5c7df0aab84ea6ac5ecb4ce7e20ad
Ruban Kumar <92320771+rkamd@users.noreply.github.com>
2024-03-04 13:12:58 -0600

    Run 2 Run validation for symm and hemm (#2381)
    [ROCm/rocblas commit: 5faf332379eb6afed008d7844434cba783daf65d]

--- clients/include/blas3/testing_symm_hemm_batched.hpp
+++ clients/include/blas3/testing_symm_hemm_batched.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2020-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2020-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -384,6 +384,33 @@ void testing_symm_hemm_batched(const Arguments& arg)
                                            dC.ptr_on_device(),
                                            ldc,
                                            batch_count));
+
+            if(arg.repeatability_check)
+            {
+                host_batch_matrix<T> hC_copy(M, N, ldc, batch_count);
+                CHECK_HIP_ERROR(hC.transfer_from(dC));
+                for(int i = 0; i < arg.iters; i++)
+                {
+                    CHECK_HIP_ERROR(dC.transfer_from(hC_gold));
+                    CHECK_ROCBLAS_ERROR(rocblas_fn(handle,
+                                                   side,
+                                                   uplo,
+                                                   M,
+                                                   N,
+                                                   d_alpha,
+                                                   dA.ptr_on_device(),
+                                                   lda,
+                                                   dB.ptr_on_device(),
+                                                   ldb,
+                                                   d_beta,
+                                                   dC.ptr_on_device(),
+                                                   ldc,
+                                                   batch_count));
+                    CHECK_HIP_ERROR(hC_copy.transfer_from(dC));
+                    unit_check_general<T>(M, N, ldc, hC, hC_copy, batch_count);
+                }
+                return;
+            }
         }
 
         // CPU BLAS
