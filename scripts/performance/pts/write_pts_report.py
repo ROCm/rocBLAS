c1b478c89805fd41394358af46174a556a8e4afc
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2024-04-18 09:36:52 -0600

    Updating PTS sizes (#2445)
    [ROCm/rocblas commit: 77804c63255ec0a5ba8d17b71ca7e88052788e70]

--- scripts/performance/pts/write_pts_report.py
+++ scripts/performance/pts/write_pts_report.py
@@ -1,4 +1,4 @@
-"""Copyright (C) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
+"""Copyright (C) 2022-2024 Advanced Micro Devices, Inc. All rights reserved.
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
@@ -284,7 +284,7 @@ def main(args):
 
         saveRocblasBenchResults(rocblasBenchCommand,
                                 filename,
-                                10,
+                                5,
                                 os.path.join(subDirectory, outputName+'_benchmark.csv'),
                                 perf_queries, res_queries)
 
