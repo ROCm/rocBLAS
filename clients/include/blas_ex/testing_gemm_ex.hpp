181b4ab3cbca45e9677d618102678152eda1429d
amcamd <andrew.chapman@amd.com>
2024-04-21 14:58:42 -0500

    standardize gemm_ex test code using rotating buffer only for timing
    [ROCm/rocblas commit: d50a5b1cd39aed3e0c82c13c9cbbcf583d1ef4bd]

--- clients/include/blas_ex/testing_gemm_ex.hpp
+++ clients/include/blas_ex/testing_gemm_ex.hpp
@@ -316,47 +316,6 @@ void testing_gemm_ex(const Arguments& arg)
         d_type = arg.c_type;
     }
 
-    // Information on flush_memory_size and flush_batch_count
-    // - To time gemm_ex it is called number_hot_calls times.
-    // - if the size of dA, dB, dC, dD are small enough they will be cached
-    //   and reused number_hot_calls-1 times.
-    // - This "hot-cache" timing will give higher performance than if the
-    //   cache is flushed
-    // - arg.flush_batch_count or arg.flush_memory_size can be used to avoid caching of dA, dB, dC, dD
-    // - if arg.flush_memory_size is specified, then flush_batch_count is calculated
-    // - only one of arg.flush_memory_size or arg.flush_batch_count can be used, not both
-    // - Note that this is only used in timing code, not in testing code.
-    // - The method is as outlined in
-    //   "Achieving accurate and context-sensitive timing for code optimization" by Whaley and Castaldo.
-    // - In the number_hot_calls timing loop it cycles through the arg.flush_batch_count copies
-    //   of dA, dB, dC, dD, and if flush_memory_size is large enough they will be evicted
-    //   from cache before they are reused.
-    // - The individual matrices are aligned on the same byte boundaries provided by hipMalloc.
-    rocblas_stride stride_a = size_t(lda) * A_col;
-    rocblas_stride stride_b = size_t(ldb) * B_col;
-    rocblas_stride stride_c = size_t(ldc) * N;
-    rocblas_stride stride_d = arg.outofplace ? size_t(ldd) * N : 0;
-
-    rocblas_stride aligned_stride_a = align_stride<Ti>(stride_a);
-    rocblas_stride aligned_stride_b = align_stride<Ti>(stride_b);
-    rocblas_stride aligned_stride_c = align_stride<To>(stride_c);
-    rocblas_stride aligned_stride_d = align_stride<To>(stride_d);
-
-    size_t flush_batch_count = 1;
-    if(arg.timing)
-    {
-        size_t a_size = M * K * sizeof(Ti);
-        size_t b_size = K * N * sizeof(Ti);
-        size_t c_size = M * N * sizeof(To);
-        //      exclude d_size from cached_size calculation because
-        //      - for arg.outofplace == false : D == C
-        //      - for arg.outofplace == true  : D is write only
-        size_t a_b_c_cached_size = a_size + b_size + c_size;
-
-        flush_batch_count = calculate_flush_batch_count(
-            arg.flush_batch_count, arg.flush_memory_size, a_b_c_cached_size);
-    }
-
     // Allocate host memory
     host_matrix<Ti> hA(A_row, A_col, lda);
     host_matrix<Ti> hB(B_row, B_col, ldb);
@@ -368,24 +327,10 @@ void testing_gemm_ex(const Arguments& arg)
     CHECK_HIP_ERROR(hC.memcheck());
 
     // Allocate device memory
-    device_strided_batch_matrix<Ti> dA(A_row, A_col, lda, aligned_stride_a, flush_batch_count);
-    device_strided_batch_matrix<Ti> dB(B_row, B_col, ldb, aligned_stride_b, flush_batch_count);
-    device_strided_batch_matrix<To> dC(M, N, ldc, aligned_stride_c, flush_batch_count);
-    // if C!=D, allocate C and D normally
-    // if C==D, allocate C big enough for the larger of C and D; D points to C
-    device_strided_batch_matrix<To> dD_alloc
-        = (arg.outofplace) ? device_strided_batch_matrix<To>(M, N, ldd, aligned_stride_d, 1)
-                           : device_strided_batch_matrix<To>(0, 1, 1, 1, 1);
-    device_strided_batch_matrix<To>& dD = (arg.outofplace) ? dD_alloc : dC;
-
     device_vector<Tc> d_alpha_Tc(1);
     device_vector<Tc> d_beta_Tc(1);
 
     // Check device memory allocation
-    CHECK_DEVICE_ALLOCATION(dA.memcheck());
-    CHECK_DEVICE_ALLOCATION(dB.memcheck());
-    CHECK_DEVICE_ALLOCATION(dC.memcheck());
-    CHECK_DEVICE_ALLOCATION(dD_alloc.memcheck());
     CHECK_DEVICE_ALLOCATION(d_alpha_Tc.memcheck());
     CHECK_DEVICE_ALLOCATION(d_beta_Tc.memcheck());
 
@@ -400,13 +345,34 @@ void testing_gemm_ex(const Arguments& arg)
     rocblas_init_matrix<To, true>(
         hC, arg, rocblas_client_beta_sets_nan, rocblas_client_general_matrix);
 
-    // copy data from CPU to device
-    CHECK_HIP_ERROR(dA.broadcast_one_matrix_from(hA));
-    CHECK_HIP_ERROR(dB.broadcast_one_matrix_from(hB));
-    CHECK_HIP_ERROR(dC.broadcast_one_matrix_from(hC));
-
     if(arg.unit_check || arg.norm_check)
     {
+        // Allocate device memory
+        device_matrix<Ti> dA(A_row, A_col, lda);
+        device_matrix<Ti> dB(B_row, B_col, ldb);
+
+        // if C!=D, allocate C and D normally
+        // if C==D, allocate C big enough for the larger of C and D; D points to C
+        device_matrix<To> dC(M, N, ldc);
+        device_matrix<To> dD
+            = (arg.outofplace) ? device_matrix<To>(M, N, ldd) : device_matrix<To>(0, 1, 1);
+        device_matrix<To>& dDref = (arg.outofplace) ? dD : dC;
+        device_vector<Tc>  d_alpha_Tc(1);
+        device_vector<Tc>  d_beta_Tc(1);
+
+        // Check device memory allocation
+        CHECK_DEVICE_ALLOCATION(dA.memcheck());
+        CHECK_DEVICE_ALLOCATION(dB.memcheck());
+        CHECK_DEVICE_ALLOCATION(dC.memcheck());
+        CHECK_DEVICE_ALLOCATION(dD.memcheck());
+        CHECK_DEVICE_ALLOCATION(d_alpha_Tc.memcheck());
+        CHECK_DEVICE_ALLOCATION(d_beta_Tc.memcheck());
+
+        // copy data from CPU to device
+        CHECK_HIP_ERROR(dA.transfer_from(hA));
+        CHECK_HIP_ERROR(dB.transfer_from(hB));
+        CHECK_HIP_ERROR(dC.transfer_from(hC));
+
         using To_hpa = std::conditional_t<std::is_same_v<To, rocblas_bfloat16>, float, To>;
 
         host_matrix<To>     hD_1(M, N, ldd);
@@ -424,15 +390,15 @@ void testing_gemm_ex(const Arguments& arg)
             handle.pre_test(arg);
             // clang-format off
             CHECK_ROCBLAS_ERROR(rocblas_gemm_ex_fn(handle, transA, transB, M, N, K, &h_alpha_Tc,
-                                               dA[0], arg.a_type, lda,
-                                               dB[0], arg.b_type, ldb, &h_beta_Tc,
-                                               dC[0], arg.c_type, ldc,
-                                               dD[0],     d_type, ldd,
+                                               dA, arg.a_type, lda,
+                                               dB, arg.b_type, ldb, &h_beta_Tc,
+                                               dC, arg.c_type, ldc,
+                                               dDref,  d_type, ldd,
                                                arg.compute_type, algo, solution_index, flags));
             // clang-format on
             handle.post_test(arg);
             // copy output from device to CPU
-            CHECK_HIP_ERROR(hD_1.transfer_one_matrix_from(dD));
+            CHECK_HIP_ERROR(hD_1.transfer_from(dDref));
         }
 
         if(arg.pointer_mode_device)
@@ -440,20 +406,20 @@ void testing_gemm_ex(const Arguments& arg)
             // ROCBLAS rocblas_pointer_mode_device
             CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_device));
 
-            CHECK_HIP_ERROR(dC.broadcast_one_matrix_from(hC));
+            CHECK_HIP_ERROR(dC.transfer_from(hC));
 
             CHECK_HIP_ERROR(hipMemcpy(d_alpha_Tc, &h_alpha_Tc, sizeof(Tc), hipMemcpyHostToDevice));
             CHECK_HIP_ERROR(hipMemcpy(d_beta_Tc, &h_beta_Tc, sizeof(Tc), hipMemcpyHostToDevice));
             // clang-format off
             CHECK_ROCBLAS_ERROR(rocblas_gemm_ex_fn(handle, transA, transB, M, N, K, d_alpha_Tc,
-                                               dA[0], arg.a_type, lda,
-                                               dB[0], arg.b_type, ldb, d_beta_Tc,
-                                               dC[0], arg.c_type, ldc,
-                                               dD[0],     d_type, ldd,
+                                               dA, arg.a_type, lda,
+                                               dB, arg.b_type, ldb, d_beta_Tc,
+                                               dC, arg.c_type, ldc,
+                                               dDref,  d_type, ldd,
                                                arg.compute_type, algo, solution_index, flags));
             // clang-format on
 
-            CHECK_HIP_ERROR(hD_2.transfer_one_matrix_from(dD));
+            CHECK_HIP_ERROR(hD_2.transfer_from(dDref));
 
             if(arg.repeatability_check)
             {
@@ -462,17 +428,17 @@ void testing_gemm_ex(const Arguments& arg)
 
                 for(int i = 0; i < arg.iters; i++)
                 {
-                    CHECK_HIP_ERROR(dC.broadcast_one_matrix_from(hC));
+                    CHECK_HIP_ERROR(dC.transfer_from(hC));
                     // clang-format off
                     CHECK_ROCBLAS_ERROR(rocblas_gemm_ex_fn(handle, transA, transB, M, N, K, d_alpha_Tc,
-                                               dA[0], arg.a_type, lda,
-                                               dB[0], arg.b_type, ldb, d_beta_Tc,
-                                               dC[0], arg.c_type, ldc,
-                                               dD[0],     d_type, ldd,
+                                               dA, arg.a_type, lda,
+                                               dB, arg.b_type, ldb, d_beta_Tc,
+                                               dC, arg.c_type, ldc,
+                                               dDref,  d_type, ldd,
                                                arg.compute_type, algo, solution_index, flags));
                     // clang-format on
                     // fetch device mode GPU results
-                    CHECK_HIP_ERROR(hD_copy.transfer_one_matrix_from(dD));
+                    CHECK_HIP_ERROR(hD_copy.transfer_from(dDref));
 
                     unit_check_general<To>(M, N, ldd, hD_2, hD_copy);
                 }
@@ -579,6 +545,66 @@ void testing_gemm_ex(const Arguments& arg)
     {
         int number_cold_calls = arg.cold_iters;
         int number_hot_calls  = arg.iters;
+
+        // Information on flush_memory_size and flush_batch_count
+        // - To time gemm_ex it is called number_hot_calls times.
+        // - if the size of dA, dB, dC, dD are small enough they will be cached
+        //   and reused number_hot_calls-1 times.
+        // - This "hot-cache" timing will give higher performance than if the
+        //   cache is flushed
+        // - arg.flush_batch_count or arg.flush_memory_size can be used to avoid caching of dA, dB, dC, dD
+        // - if arg.flush_memory_size is specified, then flush_batch_count is calculated
+        // - only one of arg.flush_memory_size or arg.flush_batch_count can be used, not both
+        // - Note that this is only used in timing code, not in testing code.
+        // - The method is as outlined in
+        //   "Achieving accurate and context-sensitive timing for code optimization" by Whaley and Castaldo.
+        // - In the number_hot_calls timing loop it cycles through the arg.flush_batch_count copies
+        //   of dA, dB, dC, dD, and if flush_memory_size is large enough they will be evicted
+        //   from cache before they are reused.
+        // - The individual matrices are aligned on the same byte boundaries provided by hipMalloc.
+
+        rocblas_stride stride_a = size_t(lda) * A_col;
+        rocblas_stride stride_b = size_t(ldb) * B_col;
+        rocblas_stride stride_c = size_t(ldc) * N;
+        rocblas_stride stride_d = arg.outofplace ? size_t(ldd) * N : 0;
+
+        rocblas_stride aligned_stride_a = align_stride<Ti>(stride_a);
+        rocblas_stride aligned_stride_b = align_stride<Ti>(stride_b);
+        rocblas_stride aligned_stride_c = align_stride<To>(stride_c);
+        rocblas_stride aligned_stride_d = align_stride<To>(stride_d);
+
+        size_t a_size = M * K * sizeof(Ti);
+        size_t b_size = K * N * sizeof(Ti);
+        size_t c_size = M * N * sizeof(To);
+        //      exclude d_size from cached_size calculation because
+        //      - for arg.outofplace == false : D == C
+        //      - for arg.outofplace == true  : D is write only
+        size_t a_b_c_cached_size = a_size + b_size + c_size;
+
+        size_t flush_batch_count = calculate_flush_batch_count(
+            arg.flush_batch_count, arg.flush_memory_size, a_b_c_cached_size);
+
+        // Allocate device memory
+        device_strided_batch_matrix<Ti> dA(A_row, A_col, lda, aligned_stride_a, flush_batch_count);
+        device_strided_batch_matrix<Ti> dB(B_row, B_col, ldb, aligned_stride_b, flush_batch_count);
+        device_strided_batch_matrix<To> dC(M, N, ldc, aligned_stride_c, flush_batch_count);
+        // if C!=D, allocate C and D normally
+        // if C==D, allocate C big enough for the larger of C and D; D points to C
+        device_strided_batch_matrix<To> dD_alloc
+            = (arg.outofplace) ? device_strided_batch_matrix<To>(M, N, ldd, aligned_stride_d, 1)
+                               : device_strided_batch_matrix<To>(0, 1, 1, 1, 1);
+        device_strided_batch_matrix<To>& dD = (arg.outofplace) ? dD_alloc : dC;
+
+        // Check device memory allocation
+        CHECK_DEVICE_ALLOCATION(dA.memcheck());
+        CHECK_DEVICE_ALLOCATION(dB.memcheck());
+        CHECK_DEVICE_ALLOCATION(dC.memcheck());
+        CHECK_DEVICE_ALLOCATION(dD_alloc.memcheck());
+
+        // copy data from CPU to device
+        CHECK_HIP_ERROR(dA.broadcast_one_matrix_from(hA));
+        CHECK_HIP_ERROR(dB.broadcast_one_matrix_from(hB));
+        CHECK_HIP_ERROR(dC.broadcast_one_matrix_from(hC));
 
         CHECK_ROCBLAS_ERROR(rocblas_set_pointer_mode(handle, rocblas_pointer_mode_host));
 
