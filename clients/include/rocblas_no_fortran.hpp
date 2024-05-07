042c26eea65893cab2088a988932767f4fe9b4c1
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2024-05-07 15:59:18 -0600

    Fix no-fortran build with gemm_ex_64, and remove unused file (#2505)
    Fix no-fortran build with gemm_ex_64, and remove unused file.

--- clients/include/rocblas_no_fortran.hpp
+++ clients/include/rocblas_no_fortran.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2021-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2021-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -79,6 +79,10 @@
 #define rocblas_gemmt_fortran rocblas_gemmt
 #define rocblas_gemmt_batched_fortran rocblas_gemmt_batched
 #define rocblas_gemmt_strided_batched_fortran rocblas_gemmt_strided_batched
+
+#define rocblas_gemm_ex_64_fortran rocblas_gemm_ex_64
+#define rocblas_gemm_batched_ex_64_fortran rocblas_gemm_batched_ex_64
+#define rocblas_gemm_strided_batched_ex_64_fortran rocblas_gemm_strided_batched_ex_64
 
 #define rocblas_geam_ex_fortran rocblas_geam_ex
 
