f9be7ba2022f61fabb8b250a28f6a16bc68de5ba
NaveenElumalaiAMD <69491510+NaveenElumalaiAMD@users.noreply.github.com>
2024-05-26 20:10:10 -0600

    Add ILP64 syr2k and syrkx (#2504)
    * Add ILP64 syrkx and syr2k
    
    * minor changes
    
    * remove clang-format in instantiations
    
    * Address Torre's comments
    
    * Change to hpl initialization
    
    * Commented out the 64 bit syrkx test
    
    * syrkx hpl init
    
    * Comment 64-bit tests which leads to alarm time out
    
    * Clang-format fix
    
    * Add template parameters missed during rebase
    
    * Add 64-bit leading dimension tests
    
    * gemm changes
    
    * clang-format fix
    
    * use gemm_internal_64
    
    * preparation for merge
    
    [ROCm/rocblas commit: c71d2fc96830f6ce3a57695878db300db61ebba9]

--- library/src/src64/blas3/rocblas_syr2k_64.cpp
+++ library/src/src64/blas3/rocblas_syr2k_64.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2016-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,6 +19,8 @@
  * CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * ************************************************************************ */
-#include "rocblas_syrkx.hpp"
-#include "rocblas_syrkx_imp.hpp"
-INST_SYRKX_C_API(rocblas_int);
+
+#include "blas3/rocblas_syr2k_imp.hpp"
+#include "rocblas_syr2k_her2k_64.hpp"
+
+INST_SYR2K_C_API(int64_t);
