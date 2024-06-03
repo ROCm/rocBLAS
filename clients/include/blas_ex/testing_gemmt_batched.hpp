d39e68a6a80331724291c7be1084c26972d5a10f
amcamd <andrew.chapman@amd.com>
2024-06-03 17:11:15 -0500

    gemmt ilp64 address PR requests

--- clients/include/blas_ex/testing_gemmt_batched.hpp
+++ clients/include/blas_ex/testing_gemmt_batched.hpp
@@ -30,8 +30,8 @@ void testing_gemmt_batched_bad_arg(const Arguments& arg)
     auto rocblas_gemmt_batched_fn
         = arg.api == FORTRAN ? rocblas_gemmt_batched<T, true> : rocblas_gemmt_batched<T, false>;
 
-    auto rocblas_gemmt_batched_fn_64
-        = arg.api == FORTRAN ? rocblas_gemmt_batched<T, true> : rocblas_gemmt_batched<T, false>;
+    auto rocblas_gemmt_batched_fn_64 = arg.api == FORTRAN_64 ? rocblas_gemmt_batched_64<T, true>
+                                                             : rocblas_gemmt_batched_64<T, false>;
 
     for(auto pointer_mode : {rocblas_pointer_mode_host, rocblas_pointer_mode_device})
     {
@@ -387,8 +387,8 @@ void testing_gemmt_batched(const Arguments& arg)
     auto rocblas_gemmt_batched_fn
         = arg.api == FORTRAN ? rocblas_gemmt_batched<T, true> : rocblas_gemmt_batched<T, false>;
 
-    auto rocblas_gemmt_batched_fn_64 = arg.api == FORTRAN ? rocblas_gemmt_batched_64<T, true>
-                                                          : rocblas_gemmt_batched_64<T, false>;
+    auto rocblas_gemmt_batched_fn_64 = arg.api == FORTRAN_64 ? rocblas_gemmt_batched_64<T, true>
+                                                             : rocblas_gemmt_batched_64<T, false>;
 
     rocblas_local_handle handle{arg};
     rocblas_fill         uplo        = char2rocblas_fill(arg.uplo);
