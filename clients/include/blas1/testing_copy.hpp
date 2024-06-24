9ddf668859ca5a091472981a039385e0e9d86341
amcamd <andrew.chapman@amd.com>
2024-06-24 16:27:01 -0500

    add rotating buffer MALL flush to Benchmark class
    [ROCm/rocblas commit: 103063163b8dc5b9bf60320e463a2217038dd8c9]

--- clients/include/blas1/testing_copy.hpp
+++ clients/include/blas1/testing_copy.hpp
@@ -26,38 +26,12 @@
 #include "testing_common.hpp"
 
 template <typename T>
-static rocblas_status copy_dispatch(rocblas_handle     handle,
-                                    int64_t            N,
-                                    const T*           dx,
-                                    int64_t            incx,
-                                    T*                 dy,
-                                    int64_t            incy,
-                                    rocblas_client_api client_api)
-{
-    switch(client_api)
-    {
-    case C:
-        return rocblas_copy<T, false>(handle, N, dx, incx, dy, incy);
-    case C_64:
-        return rocblas_copy_64<T, false>(handle, N, dx, incx, dy, incy);
-    case FORTRAN:
-        return rocblas_copy<T, true>(handle, N, dx, incx, dy, incy);
-    case FORTRAN_64:
-        return rocblas_copy_64<T, true>(handle, N, dx, incx, dy, incy);
-    case INTERNAL:
-        rocblas_cerr << "Error: not implemented client_api == INTERNAL   " << std::endl;
-    case INTERNAL_64:
-        rocblas_cerr << "Error: not implemented client_api == INTERNAL_64" << std::endl;
-    default:
-        rocblas_cerr << "Error: not implemented client_api == " << client_api << std::endl;
-    }
-
-    return rocblas_status_not_implemented;
-}
-
-template <typename T>
 void testing_copy_bad_arg(const Arguments& arg)
 {
+    auto rocblas_copy_fn = arg.api == FORTRAN ? rocblas_copy<T, true> : rocblas_copy<T, false>;
+    auto rocblas_copy_fn_64
+        = arg.api == FORTRAN_64 ? rocblas_copy_64<T, true> : rocblas_copy_64<T, false>;
+
     rocblas_local_handle handle{arg};
 
     int64_t N    = 100;
@@ -69,28 +43,31 @@ void testing_copy_bad_arg(const Arguments& arg)
     CHECK_DEVICE_ALLOCATION(dx.memcheck());
     CHECK_DEVICE_ALLOCATION(dy.memcheck());
 
-    EXPECT_ROCBLAS_STATUS(rocblas_status_invalid_handle,
-                          copy_dispatch<T>(nullptr, N, dx, incx, dy, incy, arg.api));
+    DAPI_EXPECT(rocblas_status_invalid_handle, rocblas_copy_fn, (nullptr, N, dx, incx, dy, incy));
 
-    EXPECT_ROCBLAS_STATUS(rocblas_status_invalid_pointer,
-                          copy_dispatch<T>(handle, N, nullptr, incx, dy, incy, arg.api));
-
-    EXPECT_ROCBLAS_STATUS(rocblas_status_invalid_pointer,
-                          copy_dispatch<T>(handle, N, dx, incx, nullptr, incy, arg.api));
+    DAPI_EXPECT(
+        rocblas_status_invalid_pointer, rocblas_copy_fn, (handle, N, nullptr, incx, dy, incy));
+    DAPI_EXPECT(
+        rocblas_status_invalid_pointer, rocblas_copy_fn, (handle, N, dx, incx, nullptr, incy));
 }
 
 template <typename T>
 void testing_copy(const Arguments& arg)
 {
+    auto rocblas_copy_fn = arg.api == FORTRAN ? rocblas_copy<T, true> : rocblas_copy<T, false>;
+    auto rocblas_copy_fn_64
+        = arg.api == FORTRAN_64 ? rocblas_copy_64<T, true> : rocblas_copy_64<T, false>;
+
     int64_t              N    = arg.N;
     int64_t              incx = arg.incx;
     int64_t              incy = arg.incy;
+    bool                 HMM  = arg.HMM;
     rocblas_local_handle handle{arg};
 
     // Argument sanity check before allocating invalid memory
     if(N <= 0)
     {
-        CHECK_ROCBLAS_ERROR(copy_dispatch<T>(handle, N, nullptr, incx, nullptr, incy, arg.api));
+        DAPI_CHECK(rocblas_copy_fn, (handle, N, nullptr, incx, nullptr, incy));
         return;
     }
 
@@ -100,14 +77,6 @@ void testing_copy(const Arguments& arg)
     host_vector<T> hy(N, incy);
     host_vector<T> hy_gold(N, incy);
 
-    // Allocate device memory
-    device_vector<T> dx(N, incx);
-    device_vector<T> dy(N, incy);
-
-    // Check device memory allocation
-    CHECK_DEVICE_ALLOCATION(dx.memcheck());
-    CHECK_DEVICE_ALLOCATION(dy.memcheck());
-
     // Initialize data on host memory
     rocblas_init_vector(hx, arg, rocblas_client_alpha_sets_nan, true);
     rocblas_init_vector(hy, arg, rocblas_client_alpha_sets_nan, false);
@@ -115,18 +84,26 @@ void testing_copy(const Arguments& arg)
     // save a copy in hy_gold which will be output of CPU BLAS
     hy_gold = hy;
 
-    // copy data from CPU to device
-    CHECK_HIP_ERROR(dx.transfer_from(hx));
-    CHECK_HIP_ERROR(dy.transfer_from(hy));
-
     double cpu_time_used;
     double rocblas_error = 0.0;
 
     if(arg.unit_check || arg.norm_check)
     {
+        // Allocate device memory
+        device_vector<T> dx(N, incx, HMM);
+        device_vector<T> dy(N, incy, HMM);
+
+        // Check device memory allocation
+        CHECK_DEVICE_ALLOCATION(dx.memcheck());
+        CHECK_DEVICE_ALLOCATION(dy.memcheck());
+
+        // copy data from CPU to device
+        CHECK_HIP_ERROR(dx.transfer_from(hx));
+        CHECK_HIP_ERROR(dy.transfer_from(hy));
+
         handle.pre_test(arg);
         // GPU BLAS
-        CHECK_ROCBLAS_ERROR(copy_dispatch<T>(handle, N, dx, incx, dy, incy, arg.api));
+        DAPI_CHECK(rocblas_copy_fn, (handle, N, dx, incx, dy, incy));
         handle.post_test(arg);
 
         CHECK_HIP_ERROR(hy.transfer_from(dy));
@@ -149,17 +126,43 @@ void testing_copy(const Arguments& arg)
 
     if(arg.timing)
     {
+        size_t aligned_stride_x = align_stride<T>(size_t(N) * (incx >= 0 ? incx : -incx));
+        size_t aligned_stride_y = align_stride<T>(size_t(N) * (incy >= 0 ? incy : -incy));
+
+        size_t x_size          = N * incx * sizeof(T);
+        size_t y_size          = N * incy * sizeof(T);
+        size_t x_y_cached_size = x_size + y_size;
+
+        size_t flush_batch_count = calculate_flush_batch_count(
+            arg.flush_batch_count, arg.flush_memory_size, x_y_cached_size);
+
+        // allocate device rotating buffer arrays
+        device_strided_batch_vector<T> dx_rot_buff(
+            N, incx, aligned_stride_x, flush_batch_count, HMM);
+        device_strided_batch_vector<T> dy_rot_buff(
+            N, incy, aligned_stride_y, flush_batch_count, HMM);
+
+        CHECK_DEVICE_ALLOCATION(dx_rot_buff.memcheck());
+        CHECK_DEVICE_ALLOCATION(dy_rot_buff.memcheck());
+
+        CHECK_HIP_ERROR(dx_rot_buff.broadcast_one_vector_from(hx));
+        CHECK_HIP_ERROR(dy_rot_buff.broadcast_one_vector_from(hy));
+
         double gpu_time_used;
 
         hipStream_t stream;
         CHECK_ROCBLAS_ERROR(rocblas_get_stream(handle, &stream));
 
-        auto lambda_to_benchmark
-            = [&] { copy_dispatch<T>(handle, N, dx, incx, dy, incy, arg.api); };
+        auto lambda_to_benchmark = [&](int flush_index) {
+            DAPI_DISPATCH(
+                rocblas_copy_fn,
+                (handle, N, dx_rot_buff[flush_index], incx, dy_rot_buff[flush_index], incy));
+        };
 
-        Benchmark<decltype(lambda_to_benchmark)> stats(lambda_to_benchmark, stream, arg);
+        Benchmark<decltype(lambda_to_benchmark)> benchmark_copy(
+            lambda_to_benchmark, stream, arg, flush_batch_count);
 
-        gpu_time_used = stats.timer();
+        gpu_time_used = benchmark_copy.timer();
 
         ArgumentModel<e_N, e_incx, e_incy>{}.log_args<T>(rocblas_cout,
                                                          arg,
