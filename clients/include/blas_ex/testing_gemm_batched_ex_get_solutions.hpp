7cde1b743ce05df2e956b33612ad2b047898f864
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2024-12-19 12:06:28 -0700

    remove invalid tests (#2805)
    * implement hipblaslt support for flag check_solution_index
    * remove some solution constraint tests when building with hipblaslt
    
    [ROCm/rocblas commit: 452691f1d72ea988f5aba2c24685d183c4d25b3a]

--- clients/include/blas_ex/testing_gemm_batched_ex_get_solutions.hpp
+++ clients/include/blas_ex/testing_gemm_batched_ex_get_solutions.hpp
@@ -171,9 +171,11 @@ void testing_gemm_batched_ex_get_solutions(const Arguments& arg)
             max = sol;
     }
 
+#ifndef BUILD_WITH_HIPBLASLT
     EXPECT_ROCBLAS_STATUS(
         rocblas_gemm_batched_exM(GEMM_B_EX_ARGS, max + 1, rocblas_gemm_flags_none),
         rocblas_status_invalid_value);
+#endif
 
     // Testing get solutions by type - should be superset of solutions that solve problem
     rocblas_int size_type;
@@ -194,6 +196,7 @@ void testing_gemm_batched_ex_get_solutions(const Arguments& arg)
                                                                       ary_type.data(),
                                                                       &size_type));
 
+#ifndef BUILD_WITH_HIPBLASLT
     std::vector<rocblas_int> valid_ary(ary.begin(), ary.begin() + size); // Trim off junk values
     std::sort(ary_type.begin(), ary_type.end());
     std::sort(valid_ary.begin(), valid_ary.end());
@@ -201,4 +204,5 @@ void testing_gemm_batched_ex_get_solutions(const Arguments& arg)
     bool ary_is_subset
         = std::includes(ary_type.begin(), ary_type.end(), valid_ary.begin(), valid_ary.end());
     EXPECT_TRUE(ary_is_subset);
+#endif
 }
