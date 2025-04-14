e3251f96316cb09e66af49adb382cca40044f51e
Ruban Kumar <ruban.kumar@amd.com>
2025-04-14 11:12:33 -0400

    fix: update logic

--- library/src/blas3/Tensile/gemm_templates.cpp
+++ library/src/blas3/Tensile/gemm_templates.cpp
@@ -135,7 +135,7 @@ rocblas_status rocblas_internal_gemm(rocblas_handle    handle,
     // Return the current status if a solution was not found
     // by other libraries(hipBLASLt or Tensile) or if the data is not both complex.
     // Otherwise, fall through to rocblas source GEMM.
-    if(status != rocblas_status_not_implemented && !rocblas_is_complex<TScal>)
+    if(status != rocblas_status_not_implemented || !rocblas_is_complex<TScal>)
     {
         return status;
     }
