9f45a9d6eca3fb490793f5dfd0396fca3fd3e5f2
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-06-10 22:32:35 +0000

    fix(next-cmake): change sanitizer arg to 'visibility'

--- next-cmake/cmake/rocblas_target_configure_sanitizers.cmake
+++ next-cmake/cmake/rocblas_target_configure_sanitizers.cmake
@@ -1,15 +1,15 @@
 # Copyright Advanced Micro Devices, Inc., or its affiliates.
 # SPDX-License-Identifier: MIT
 
-function(rocblas_target_configure_sanitizers rocblas_target linkage)
+function(rocblas_target_configure_sanitizers rocblas_target visibility)
     # Add asan flags to target
     target_compile_options(${rocblas_target}
-        ${linkage}
+        ${visibility}
             -fsanitize=address
             -shared-libasan
     )
     target_link_options(${rocblas_target}
-        ${linkage}
+        ${visibility}
             -fsanitize=address
             -shared-libasan
             -fuse-ld=lld
