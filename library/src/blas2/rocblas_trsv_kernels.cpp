0a56308699c296b15a317f9d23ae61a14a264919
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2025-06-27 11:36:53 -0600

    fix explicit specialization cannot have a storage class warnings (#1658)
    * our clang still requires repeated static token for specialization to avoid duplicate symbols but inline should avoid these too in a more C++17 manner, may reduce assembly too
    
    
    [ROCm/rocblas commit: 3c4c868585cb50dc80fa9ef9d2deb693f8323975]

--- library/src/blas2/rocblas_trsv_kernels.cpp
+++ library/src/blas2/rocblas_trsv_kernels.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2016-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2016-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -191,10 +191,10 @@ void ROCBLAS_KERNEL_ILF rocblas_trsv_transpose(const rocblas_int n,
 }
 
 template <rocblas_int n>
-static constexpr bool equals_two = false;
+inline constexpr bool equals_two = false;
 
 template <>
-ROCBLAS_CLANG_STATIC constexpr bool equals_two<2> = true;
+inline constexpr bool equals_two<2> = true;
 
 // Invert a 2x2 triangular section of A
 template <typename T,
